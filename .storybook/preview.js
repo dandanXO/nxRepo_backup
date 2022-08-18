a836bdc479df890a48951d60be10ec3ebae4685b
fronteric <fronteric@1.1>
2022-08-18 11:20:38 +0800

    feature(storybook/addon): setting preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,36 +2,8 @@ const customViewports = {
   xs: {
     name: "XS",
     styles: {
-      width: "480px",
-      height: "600px",
-    },
-  },
-  s: {
-    name: "S",
-    styles: {
-      width: "640px",
-      height: "801px",
-    },
-  },
-  m: {
-    name: "M",
-    styles: {
-      width: "960px",
-      height: "700px",
-    },
-  },
-  l: {
-    name: "L",
-    styles: {
-      width: "1200px",
-      height: "800px",
-    },
-  },
-  xl: {
-    name: "XL",
-    styles: {
-      width: "1400px",
-      height: "900px",
+      width: "360px",
+      height: "640px",
     },
   },
 };
