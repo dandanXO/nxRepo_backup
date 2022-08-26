324a32351d9dbaa3fe2e2c739eebb1dd3b929b28
fronteric <fronteric@1.1>
2022-08-26 16:55:15 +0800

    feature: add flag to avoid request again

--- src/pages/BindBankAccountPage/PureBindBankAccountPage.tsx
+++ src/pages/BindBankAccountPage/PureBindBankAccountPage.tsx
@@ -182,7 +182,7 @@ export const PureBindBankAccountPage = (
             })
             .finally(() => {
                 setIsFormPending(false);
-            })
+            });
     }, [
         ifscData.data,
         bankcardNoData.data,
