cbb99fee63176191a4ff4595c44e238d7ca0ec10
fronteric <fronteric@1.1>
2022-09-12 15:44:02 +0800

    feature(demo): add demo page

--- apps/admin/pages/_app.tsx
+++ apps/admin/pages/_app.tsx
@@ -2,6 +2,10 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
 
+// [style is broken on build but works on dev env](https://github.com/SolidZORO/next-plugin-antd-less/issues/103)
+// import 'antd/dist/antd.less';
+require('antd/dist/antd.less');
+
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
