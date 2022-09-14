800edcf20c9caf1bc0e7ec98b94b54f3b60dcd68
fronteric <fronteric@1.1>
2022-09-14 12:09:43 +0800

    feat(admin): product

---
+++ apps/admin/pages/product/index.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+import Table from "./table";
+
+import { PageContainer, ProCard } from '@ant-design/pro-components';
+
+export default () => {
+  const [domLoaded, setDomLoaded] = useState(false);
+
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
+
+  return domLoaded ? (
+    <div>
+      <PageContainer
+        // loading
+        header={{
+          // title: '页面标题',
+          ghost: true,
+          breadcrumb: {
+            routes: [
+              {
+                path: '',
+                breadcrumbName: '首頁',
+              },
+              {
+                path: '',
+                breadcrumbName: '產品管理',
+              },
+              {
+                path: '',
+                breadcrumbName: '產品管理',
+              },
+            ],
+          },
+        }}
+      >
+        <Table/>
+      </PageContainer>
+
+    </div>
+  ): null;
+};
