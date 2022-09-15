9dff35d53dad36e2938ad33e894fd74709380b66
fronteric <fronteric@1.1>
2022-09-15 17:09:49 +0800

    feature: product management - add

--- apps/admin/pages/product/ProductModal/RateSettingSection.tsx
+++ apps/admin/pages/product/ProductModal/RateSettingSection.tsx
@@ -1,10 +1,27 @@
-import {Divider, Form, Input} from "antd";
+import {Divider, Form, Input, Typography, Row, Col} from "antd";
+const { Paragraph, Text } = Typography;
+
 import React from "react";
 
 const RateSettingSection = () => {
   return (
     <React.Fragment>
       <Divider orientation="left">費率設定</Divider>
+
+      <Paragraph style={{ margin: "0 0 0 100px" }}>
+          <span>
+            填寫規則：
+          </span>
+        <ul>
+          <li>
+            所有費率至多填寫至<Text strong>小數點後第一位</Text>，例如：20 或 20.5
+          </li>
+          <li>
+            <Text strong>前置利息+後置利息</Text>不得超過100%
+          </li>
+        </ul>
+      </Paragraph>
+
 
       <Form.Item name="amountRange" label="前置利息" rules={[{required: true}]}>
         <Form.Item style={{display: 'inline-block', width: '180px', margin: '0 8px 0 0'}}>
