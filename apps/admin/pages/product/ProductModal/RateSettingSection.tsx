444ca8b515a1d66ce98630156c188733f27219a9
fronteric <fronteric@1.1>
2022-09-16 13:16:51 +0800

    feature(product management): Validate RateSettingSection

--- apps/admin/pages/product/ProductModal/RateSettingSection.tsx
+++ apps/admin/pages/product/ProductModal/RateSettingSection.tsx
@@ -4,11 +4,11 @@ const { Paragraph, Text } = Typography;
 import React from "react";
 import {MinusCircleOutlined, PlusOutlined} from "@ant-design/icons";
 import {formItemRule}from "./rules";
+import {NumberValidator} from "./validator";
 const RateSettingSection = () => {
   return (
     <React.Fragment>
       <Divider orientation="left">費率設定</Divider>
-
       <Paragraph style={{ margin: "0 0 0 100px" }}>
           <span>
             填寫規則：
@@ -24,36 +24,96 @@ const RateSettingSection = () => {
       </Paragraph>
 
 
-      <Form.Item name="preInterestRate" label="前置利息" rules={[{required: true}]}>
-        <Form.Item name="preInterestRate" style={{display: 'inline-block', width: '180px', margin: '0 8px 0 0'}}>
+      <Form.Item name="preInterestRate" label="前置利息" required>
+        <Form.Item name="preInterestRate" style={{display: 'inline-block', width: '180px', margin: '0 8px 0 0'}}
+                   rules={[
+                     {
+                       transform: (value) => Number(value),
+                       validator: async (_, value) =>NumberValidator(_, value)({
+                         min: 1,
+                         minMessage: "请输入前置利息",
+                         max: 100,
+                         maxMessage: "请填写1-100间数字"
+                       })
+                     },
+                   ]}
+        >
           <Input allowClear placeholder="填寫 0 - 100 間數字"/>
         </Form.Item>
         <Form.Item style={{display: 'inline-block', marginBottom: 0}}>%</Form.Item>
       </Form.Item>
 
-      <Form.Item name="postInterestRate" label="後置利息" rules={[{required: true}]}>
-        <Form.Item name="postInterestRate" style={{display: 'inline-block', width: '180px', margin: '0 8px 0 0'}}>
+      <Form.Item name="postInterestRate" label="後置利息" required>
+        <Form.Item name="postInterestRate" style={{display: 'inline-block', width: '180px', margin: '0 8px 0 0'}}
+                   rules={[
+                     {
+                       transform: (value) => Number(value),
+                       validator: async (_, value) =>NumberValidator(_, value)({
+                         min: 1,
+                         minMessage: "请输入後置利息",
+                         max: 100,
+                         maxMessage: "请填写1-100间数字"
+                       })
+                     },
+                   ]}
+        >
           <Input allowClear placeholder="填寫 0 - 100 間數字"/>
         </Form.Item>
         <Form.Item style={{display: 'inline-block', marginBottom: 0}}>%</Form.Item>
       </Form.Item>
 
-      <Form.Item name="dailyRate" label="日利息" tooltip="以借款金額計" rules={[{required: true}]}>
-        <Form.Item name="dailyRate" style={{display: 'inline-block', width: '180px', margin: '0 8px 0 0'}}>
+      <Form.Item name="dailyRate" label="日利息" tooltip="以借款金額計" required>
+        <Form.Item name="dailyRate" style={{display: 'inline-block', width: '180px', margin: '0 8px 0 0'}}
+                   rules={[
+                     {
+                       transform: (value) => Number(value),
+                       validator: async (_, value) =>NumberValidator(_, value)({
+                         min: 1,
+                         minMessage: "请输入日利息",
+                         max: 36,
+                         maxMessage: "请填写1-36间数字"
+                       })
+                     },
+                   ]}
+        >
           <Input allowClear placeholder="填寫 1-36 間數字"/>
         </Form.Item>
         <Form.Item style={{display: 'inline-block', marginBottom: 0}}>%</Form.Item>
       </Form.Item>
 
-      <Form.Item name="extensionRate" label="展期利率" rules={[{required: true}]}>
-        <Form.Item name="extensionRate" style={{display: 'inline-block', width: '180px', margin: '0 8px 0 0'}}>
+      <Form.Item name="extensionRate" label="展期利率" required>
+        <Form.Item name="extensionRate" style={{display: 'inline-block', width: '180px', margin: '0 8px 0 0'}}
+                   rules={[
+                     {
+                       transform: (value) => Number(value),
+                       validator: async (_, value) =>NumberValidator(_, value)({
+                         min: 1,
+                         minMessage: "请输入展期利率",
+                         max: 100,
+                         maxMessage: "请填写1-100间数字"
+                       })
+                     },
+                   ]}
+        >
           <Input allowClear placeholder="填寫 0 - 100 間數字"/>
         </Form.Item>
         <Form.Item style={{display: 'inline-block', marginBottom: 0}}>%</Form.Item>
       </Form.Item>
 
-      <Form.Item name="overdueRate" label="逾期費率" rules={[{required: true}]}>
-        <Form.Item name="overdueRate" style={{display: 'inline-block', width: '180px', margin: '0 8px 0 0'}}>
+      <Form.Item name="overdueRate" label="逾期費率" required>
+        <Form.Item name="overdueRate" style={{display: 'inline-block', width: '180px', margin: '0 8px 0 0'}}
+                   rules={[
+                     {
+                       transform: (value) => Number(value),
+                       validator: async (_, value) =>NumberValidator(_, value)({
+                         min: 1,
+                         minMessage: "请输入逾期費率",
+                         max: 100,
+                         maxMessage: "请填写1-100间数字"
+                       })
+                     },
+                   ]}
+        >
           <Input allowClear placeholder="填寫 0 - 100 間數字"/>
         </Form.Item>
         <Form.Item style={{display: 'inline-block', marginBottom: 0}}>%</Form.Item>
@@ -79,21 +139,51 @@ const RateSettingSection = () => {
                   <Form.Item
                     {...restField}
                     name={[name, 'num']}
-                    rules={formItemRule('起始期数')}
+                    rules={[
+                      {
+                        transform: (value) => Number(value),
+                        validator: async (_, value) =>NumberValidator(_, value)({
+                          min: 1,
+                          minMessage: "请输入起始期数",
+                        })
+                      },
+                    ]}
                   >
                     <Input placeholder="起始期数" />
                   </Form.Item>
                   <Form.Item
                     {...restField}
                     name={[name, 'preInterest']}
-                    rules={formItemRule('前置利息')}
+                    required
+                    rules={[
+                      {
+                        transform: (value) => Number(value),
+                        validator: async (_, value) =>NumberValidator(_, value)({
+                          min: 1,
+                          minMessage: "请输入前置利息",
+                          max: 100,
+                          maxMessage: "请填写1-100间数字"
+                        })
+                      },
+                    ]}
                   >
                     <Input placeholder="前置利息" suffix={"%"}/>
                   </Form.Item>
                   <Form.Item
                     {...restField}
                     name={[name, 'postInterest']}
-                    rules={formItemRule('后置利息')}
+                    required
+                    rules={[
+                      {
+                        transform: (value) => Number(value),
+                        validator: async (_, value) =>NumberValidator(_, value)({
+                          min: 1,
+                          minMessage: "请输入后置利息",
+                          max: 100,
+                          maxMessage: "请填写1-100间数字"
+                        })
+                      },
+                    ]}
                   >
                     <Input placeholder="后置利息" suffix={"%"}/>
                   </Form.Item>
