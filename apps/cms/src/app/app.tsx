087a11650ac9c70f87afeca47c2370fda74aae6b
fronteric <fronteric@1.1>
2022-09-22 16:26:51 +0800

    feat: qiankun with react cms 18

--- apps/cms/src/app/app.tsx
+++ apps/cms/src/app/app.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, Link } from 'react-router-dom';
+import {Route, Routes, Link, BrowserRouter} from 'react-router-dom';
 import MerchantPage from "./pages/MerchantPage"
 import ProductPage from "./pages/ProductPage";
 import IndexPage from "./pages/IndexPage";
@@ -8,20 +8,23 @@ import {appStore} from "./store";
 export function App() {
   return (
     <Provider store={appStore}>
-      <Routes>
-        <Route
-          path="/"
-          element={<IndexPage/>}
-        />
-        <Route
-          path="/merchant"
-          element={<MerchantPage/>}
-        />
-        <Route
-          path="/product"
-          element={<ProductPage/>}
-        />
-      </Routes>
+      <BrowserRouter basename={window["__POWERED_BY_QIANKUN__"] ? '/app-react-history' : '/child/react-history/'}>
+        <Routes>
+          <Route
+            path="/"
+            element={<IndexPage/>}
+          />
+          <Route
+            path="/merchant"
+            element={<MerchantPage/>}
+          />
+          <Route
+            path="/product"
+            element={<ProductPage/>}
+          />
+        </Routes>
+        <div> CMS !!</div>
+      </BrowserRouter>
     </Provider>
   );
 }
--- apps/cms/src/main.tsx
+++ apps/cms/src/main.tsx
@@ -1,17 +1,67 @@
-import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
 import App from './app/app';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  // NOTICE: [react使用antd警告:Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance](https://blog.csdn.net/qq_40314318/article/details/105209873)
-  // <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  // </StrictMode>
-);
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   // NOTICE: [react使用antd警告:Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance](https://blog.csdn.net/qq_40314318/article/details/105209873)
+//   // <StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   // </StrictMode>
+// );
+
+
+// NOTICE: [React 18 用 createRoot 替换 render](https://juejin.cn/post/6992435557456412709)
+
+let root
+
+function render(props) {
+  // NOTICE: 17
+  const { container } = props;
+  // ReactDOM.render(<App />, container ? container.querySelector('#root') : document.querySelector('#root'));
+
+
+  // const newContainer = document.getElementById('app');
+  // const root = ReactDOM.createRoot(newContainer);
+  // root.render(<App />);
+
+  const newContainer = container ? container.querySelector('#root') : document.querySelector('#root')
+  root = ReactDOM.createRoot(newContainer);
+  root.render(<App />);
+
+}
+
+function storeTest(props) {
+  props.onGlobalStateChange((value, prev) => console.log(`[onGlobalStateChange - ${props.name}]:`, value, prev), true);
+  props.setGlobalState({
+    ignore: props.name,
+    user: {
+      name: props.name,
+    },
+  });
+}
+
+if (!window["__POWERED_BY_QIANKUN__"]) {
+  render({});
+}
+
+export async function bootstrap() {
+  console.log('[react16] react app bootstraped');
+}
+
+export async function mount(props) {
+  console.log('[react16] props from main framework', props);
+  storeTest(props);
+  render(props);
+}
+
+// [Updates to Client Rendering APIs](https://zh-hant.reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis)
+export async function unmount(props) {
+  // NOTICE: 17
+  // const { container } = props;
+  // ReactDOM.unmountComponentAtNode(container ? container.querySelector('#root') : document.querySelector('#root'));
+  root.unmount();
+}
