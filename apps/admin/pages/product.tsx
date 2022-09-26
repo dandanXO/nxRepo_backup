e73c91882356c85277639ef9d8c161b939a3729a
fronteric <fronteric@1.1>
2022-09-26 17:14:45 +0800

    refactor: product login

--- apps/admin/pages/product.tsx
+++ apps/admin/pages/product.tsx
@@ -5,9 +5,12 @@ import {PageContainer} from '@ant-design/pro-components';
 import {useProductFormModal} from "../modules/product/hooks/useProductFormModal";
 import {ProductModal} from "../modules/product/components/ProductModal";
 import ProductForm from "../modules/product/components/ProductForm";
+import useAutoLogin from "../modules/shared/hooks/useAutoLogin";
 
 
 const Product=() => {
+  // useAutoLogin();
+
   const [domLoaded, setDomLoaded] = useState(false);
   useEffect(() => {
     setDomLoaded(true);
@@ -16,9 +19,8 @@ const Product=() => {
   const {
     productModalData, setProductModalData,
     form, handleCloseModal, merchantList,
-    uploadFiles, onFinish, setCustomAntFormFieldError,
-    customAntFormFieldError, productFormData,
-    setTriggerFetchTableList,
+    onFinish, setCustomAntFormFieldError,
+    customAntFormFieldError,
     triggerGetList, productListData,
     onAutoFinishedForm,
     onFormSubmit
@@ -28,47 +30,48 @@ const Product=() => {
     // formRef,
   });
 
+  useEffect(() => {
+    triggerGetList(null);
+  });
+
   return domLoaded ? (
-    <div>
-      <PageContainer
-        // loading
-        header={{
-          // title: '页面标题',
-          ghost: true,
-          breadcrumb: {
-            routes: [
-              {
-                path: '',
-                breadcrumbName: '首页',
-              },
-              {
-                path: '',
-                breadcrumbName: '产品管理 ',
-              },
-              {
-                path: '',
-                breadcrumbName: '产品管理',
-              },
-            ],
-          },
-        }}
-      >
-        <ProductTable triggerGetList={triggerGetList} productListData={productListData} setProductModalData={setProductModalData} />
+    <PageContainer
+      // loading
+      header={{
+        // title: '页面标题',
+        ghost: true,
+        breadcrumb: {
+          routes: [
+            {
+              path: '',
+              breadcrumbName: '首页',
+            },
+            {
+              path: '',
+              breadcrumbName: '产品管理 ',
+            },
+            {
+              path: '',
+              breadcrumbName: '产品管理',
+            },
+          ],
+        },
+      }}
+    >
+      <ProductTable triggerGetList={triggerGetList} productListData={productListData} setProductModalData={setProductModalData} />
 
-        {productModalData.show && (
-          <ProductModal
-            show={productModalData.show}
-            handleCloseModal={handleCloseModal}
-            onMockFinish={onAutoFinishedForm}
-            isEdit={productModalData.isEdit}
-            onOk={onFormSubmit}
-          >
-            <ProductForm productModalData={productModalData} onFinish={onFinish} form={form} merchantList={merchantList} customAntFormFieldError={customAntFormFieldError} setCustomAntFormFieldError={setCustomAntFormFieldError} show={productModalData.show}/>
-          </ProductModal>
-        )}
-      </PageContainer>
-
-    </div>
+      {productModalData.show && (
+        <ProductModal
+          show={productModalData.show}
+          handleCloseModal={handleCloseModal}
+          onMockFinish={onAutoFinishedForm}
+          isEdit={productModalData.isEdit}
+          onOk={onFormSubmit}
+        >
+          <ProductForm productModalData={productModalData} onFinish={onFinish} form={form} merchantList={merchantList} customAntFormFieldError={customAntFormFieldError} setCustomAntFormFieldError={setCustomAntFormFieldError} show={productModalData.show}/>
+        </ProductModal>
+      )}
+    </PageContainer>
   ): null;
 };
 export default Product;
