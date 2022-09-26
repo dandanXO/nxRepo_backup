aa1305b232bcc2c7a2389623abc434aab5dbe0f1
fronteric <fronteric@1.1>
2022-09-26 16:55:17 +0800

    refactor: product page

---
+++ apps/admin/component/product/ProductForm.tsx
@@ -0,0 +1,139 @@
+import {CustomAntFormFieldError, ProductFormModal} from "./useProductFormModal";
+import {GetAvailableMerchantResponse} from "../../types/getAvailbaleMerchant";
+import React, {useCallback} from "react";
+import {Form} from "antd";
+import BaseSettingSection from "./useProductFormModal/components/BaseSettingSection";
+import ProductSettingSection from "./useProductFormModal/components/ProductSettingSection";
+import LoanSettingSection from "./useProductFormModal/components/LoanSettingSection";
+import RateSettingSection from "./useProductFormModal/components/RateSettingSection";
+import {UploadSettingSection} from "./useProductFormModal/components/UploadSettingSection";
+
+
+interface ProductFormProps {
+  productModalData: ProductFormModal;
+  onFinish: (value: any) => void;
+  form: any;
+  merchantList: GetAvailableMerchantResponse;
+  customAntFormFieldError: CustomAntFormFieldError;
+  setCustomAntFormFieldError: React.Dispatch<React.SetStateAction<CustomAntFormFieldError>>;
+  show: boolean;
+}
+
+const ProductForm = (props: ProductFormProps) => {
+  const { productModalData, onFinish, form, merchantList, customAntFormFieldError, setCustomAntFormFieldError } = props;
+
+  const layout = {
+    labelCol: { span: 5 },
+    wrapperCol: { span: 18 },
+  };
+
+  const setLogo = useCallback((url: string) => {
+    form.setFieldsValue({
+      logo: url
+    });
+  }, [form])
+
+  const setBackgroundImg = useCallback((url: string) => {
+    form.setFieldsValue({
+      backgroundImg: url
+    });
+  }, [form])
+
+  return (
+    <Form
+      // ref={props.formRef}
+      {...layout} form={form} name="control-hooks" onFinish={onFinish}
+      onFieldsChange={(changedFields, allFields) =>{
+        // console.log("changedFields", changedFields)
+        // console.log("allFields", allFields)
+        const preInterestRate = allFields.filter(field => field.name.toString() === "preInterestRate")[0]?.value ?? undefined;
+        const postInterestRate = allFields.filter(field => field.name.toString() === "postInterestRate")[0]?.value ?? undefined;
+        let isValidate = true;
+        if(Number(preInterestRate)> 100 || Number(preInterestRate) < 0) {
+          setCustomAntFormFieldError({
+            ...customAntFormFieldError,
+            preInterestRate: {
+              validateStatus: "error",
+              help: "请填写0-100间数字",
+            },
+          })
+          isValidate = false;
+        } else if(Number(preInterestRate) >= 0 && Number(preInterestRate)<= 100){
+          setCustomAntFormFieldError({
+            ...customAntFormFieldError,
+            preInterestRate: {
+              validateStatus: "",
+              help: "",
+            },
+          })
+        }
+        if(Number(postInterestRate)> 100 || Number(postInterestRate) < 0) {
+          setCustomAntFormFieldError({
+            ...customAntFormFieldError,
+            postInterestRate: {
+              validateStatus: "error",
+              help: "请填写0-100间数字",
+            },
+          })
+          isValidate = false;
+        } else if(Number(postInterestRate) >= 0 && Number(postInterestRate)<= 100){
+          setCustomAntFormFieldError({
+            ...customAntFormFieldError,
+            postInterestRate: {
+              validateStatus: "",
+              help: "",
+            },
+          })
+        }
+        if(isValidate) {
+          if(Number(preInterestRate) + Number(postInterestRate) > 100) {
+            setCustomAntFormFieldError({
+              ...customAntFormFieldError,
+              preInterestRate: {
+                validateStatus: "error",
+                help: "前置利息＋后置利息不得超过100%",
+              },
+              postInterestRate: {
+                validateStatus: "error",
+                help: "前置利息＋后置利息不得超过100%",
+              }
+            })
+          } else {
+            setCustomAntFormFieldError({
+              ...customAntFormFieldError,
+              preInterestRate: {
+                validateStatus:"",
+                help: "",
+              },
+              postInterestRate: {
+                validateStatus:"",
+                help: "",
+              }
+            })
+          }
+        }
+      }}
+      initialValues={{
+        // NOTICE: [antd: Form.Item] `defaultValue` will not work on controlled Field. You should use `initialValues`
+        approveTimeUnit: "mins",
+        extensible: false,
+        top: false,
+        enabled: true,
+        templateType: 1,
+        productInterestRatePairs: [{
+          num: "",
+          postInterest: "",
+          preInterest: "",
+        }]
+      }}
+    >
+      <BaseSettingSection merchantList={merchantList} isEdit={productModalData.isEdit}/>
+      <ProductSettingSection setLogo={setLogo} setBackgroundImg={setBackgroundImg}/>
+      <LoanSettingSection/>
+      <RateSettingSection form={form} customAntFormFieldError={customAntFormFieldError}/>
+      <UploadSettingSection/>
+    </Form>
+  )
+}
+
+export default ProductForm;
---
+++ apps/admin/pages/product.tsx
@@ -0,0 +1,74 @@
+import React, {useCallback, useEffect, useState} from 'react';
+import moment from "moment"
+import ProductTable from "../component/product/ProductTable";
+import {PageContainer} from '@ant-design/pro-components';
+import {useProductFormModal} from "../component/product/useProductFormModal";
+import {ProductModal} from "../component/product/ProductModal";
+import ProductForm from "../component/product/ProductForm";
+
+
+const Product=() => {
+  const [domLoaded, setDomLoaded] = useState(false);
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
+
+  const {
+    productModalData, setProductModalData,
+    form, handleCloseModal, merchantList,
+    uploadFiles, onFinish, setCustomAntFormFieldError,
+    customAntFormFieldError, productFormData,
+    setTriggerFetchTableList,
+    triggerGetList, productListData,
+    onAutoFinishedForm,
+    onFormSubmit
+  } = useProductFormModal({
+    show: false,
+    isEdit: false,
+    // formRef,
+  });
+
+  return domLoaded ? (
+    <div>
+      <PageContainer
+        // loading
+        header={{
+          // title: '页面标题',
+          ghost: true,
+          breadcrumb: {
+            routes: [
+              {
+                path: '',
+                breadcrumbName: '首页',
+              },
+              {
+                path: '',
+                breadcrumbName: '产品管理 ',
+              },
+              {
+                path: '',
+                breadcrumbName: '产品管理',
+              },
+            ],
+          },
+        }}
+      >
+        <ProductTable triggerGetList={triggerGetList} productListData={productListData} setProductModalData={setProductModalData} />
+
+        {productModalData.show && (
+          <ProductModal
+            show={productModalData.show}
+            handleCloseModal={handleCloseModal}
+            onMockFinish={onAutoFinishedForm}
+            isEdit={productModalData.isEdit}
+            onOk={onFormSubmit}
+          >
+            <ProductForm productModalData={productModalData} onFinish={onFinish} form={form} merchantList={merchantList} customAntFormFieldError={customAntFormFieldError} setCustomAntFormFieldError={setCustomAntFormFieldError} show={productModalData.show}/>
+          </ProductModal>
+        )}
+      </PageContainer>
+
+    </div>
+  ): null;
+};
+export default Product;
