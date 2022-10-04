9a35e4292074eeac0ad514422992331a0ee51460
Emma <imgenu@gmail.com>
2022-10-04 16:21:32 +0800

    fix(mobile):ProductAdModalListPage import HotSvgIcon text wrong

--- apps/mobile/src/app/pages/ProductAdModalListPage/index.tsx
+++ apps/mobile/src/app/pages/ProductAdModalListPage/index.tsx
@@ -2,7 +2,7 @@ import { Page } from "@frontend/mobile/shared/ui";
 import React, { useEffect } from "react";
 import styled from "styled-components";
 import { useGetLoanRecommendQuery } from "../../api";
-import HotSvgIcon from "./HotSVGIcon";
+import HotSvgIcon from "./HotSvgIcon";
 
 const ProductAdStyled = styled.div`
     display: flex;
