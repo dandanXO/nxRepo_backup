20e077e2c2b368d62a3d982425de62a74c27b036
dev003 <dev003>
2022-10-12 14:57:35 +0800

    增加產品名稱 & APP名稱

--- packages/dlh-web/src/pages/extensionPeriodLoanManage/ExtensionPeriodRecord/DetailModal/DetailModal.js
+++ packages/dlh-web/src/pages/extensionPeriodLoanManage/ExtensionPeriodRecord/DetailModal/DetailModal.js
@@ -43,6 +43,8 @@ class DetailModal extends Component{
             { title: props.intl.formatMessage({id : "page.search.list.order.no"}), dataIndex: 'orderNo', key: 'orderNo' },
             { title: props.intl.formatMessage({id : "page.search.list.name"}), dataIndex: 'userTrueName', key: 'userTrueName' },
             { title: props.intl.formatMessage({id : "page.search.list.mobile"}), dataIndex: 'userPhone', key: 'userPhone' },
+            { title: props.intl.formatMessage({id : "page.search.list.product.name"}), dataIndex: 'productName', key: 'productName' },
+            { title: props.intl.formatMessage({id : "page.table.appName"}), dataIndex: 'appName', key: 'appName' },
             {
                 title: props.intl.formatMessage({id : "page.search.list.appication.time"}),
                 dataIndex: 'addTime',
