66569ba8aed555362539a2668384996a60a6958b
Emma <imgenu@gmail.com>
2022-10-19 10:36:13 +0800

    feat(cms-webpack4):add api postUserReview

--- packages/cms-webpack4/src/modules/user/api/UserReviewApi.ts
+++ packages/cms-webpack4/src/modules/user/api/UserReviewApi.ts
@@ -1,6 +1,7 @@
 import { API } from "../../../api";
 import { GetUerReviewListProps,GetUserReviewListRequestQuerystring } from "./types/userReviewTypes/getUserReviewList";
 import { UserId } from "../../../types/UserId";
+import { PostUserReviewErrorReponse,PostUserReviewRequestQuerystring } from "./types/userReviewTypes/postUserReview";
 
 const UserReviewApi = API.injectEndpoints({
     overrideExisting: false,
@@ -13,10 +14,17 @@ const UserReviewApi = API.injectEndpoints({
                 method: "get",
             }),
         }),
-      
+        // NOTE: POST /hs​/admin​/user-review​/final 终审
+        postUserReview: builder.mutation<PostUserReviewErrorReponse, PostUserReviewRequestQuerystring>({
+            query: (requestBody: PostUserReviewRequestQuerystring) => ({
+                url: `/user-review/final`,
+                method: "post",
+                data: requestBody,
+            }),
+        }),
     })
 })
 export const {
     useLazyGetUserReviewListQuery,
- 
+    usePostUserReviewMutation
 } = UserReviewApi;
---
+++ packages/cms-webpack4/src/modules/user/api/types/userReviewTypes/postUserReview.ts
@@ -0,0 +1,14 @@
+export interface PostUserReviewRequestQuerystring {
+    reason?: string;    //原因
+
+    status?: 0 | 1 | 2; // 審核結果，0:拒絕 1:通過 2:黑單
+
+    userIds?: number[]  // 用戶ID們
+}
+
+export type PostUserReviewErrorReponse = UserReviewError[]
+
+export interface UserReviewError {
+    errorMessage?; string;
+    userId?: number
+}
