d08aff641662f67bc87487806dd49e7a80ef152d
fronteric <fronteric@1.1>
2022-10-21 15:27:49 +0800

    feat: APIADMIN-22 修改 modal

--- packages/cms-webpack4/src/modules/shared/components/AdminDeleteModal.tsx
+++ packages/cms-webpack4/src/modules/shared/components/AdminDeleteModal.tsx
@@ -1,6 +1,7 @@
 import {Modal} from "antd/es";
 import React from "react";
-
+import {InfoCircleOutlined} from "@ant-design/icons";
+import "./AdminDeleteModal.less";
 export interface AdminDeleteModalProps {
     open: boolean;
     onOk?: () => void;
@@ -10,8 +11,8 @@ export interface AdminDeleteModalProps {
 
 export const AdminDeleteModal = (props: AdminDeleteModalProps) => {
     return (
-        <Modal open={props.open} closable={false} width={416} maskClosable={false} okText={"确定"} cancelText={"取消"} onOk={props.onOk} onCancel={props.onCancel}>
-            是否要删除?
+        <Modal wrapClassName="admin-delete-modal" open={props.open} closable={false} width={416} maskClosable={false} okText={"确定"} cancelText={"取消"} onOk={props.onOk} onCancel={props.onCancel}>
+            <InfoCircleOutlined style={{ color: "#FAAD14", marginRight: 16 }}/>确认要删除此笔数据吗?
         </Modal>
     )
 }
