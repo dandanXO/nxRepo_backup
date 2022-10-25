d606acbe740c3d8cea77de9e28ad9d60fbeb93e0
fronteric <fronteric@1.1>
2022-10-25 17:46:53 +0800

    feat: 麵包屑調整

--- packages/cms-webpack4/src/modules/shared/itemRender.tsx
+++ packages/cms-webpack4/src/modules/shared/itemRender.tsx
@@ -5,7 +5,12 @@ import {Link} from "react-router-dom";
 
 export const itemRender = (route: Route, params: any, routes: Route[], paths: string[]): React.ReactNode => {
     if(route.path === "/") {
-        return <span style={{ cursor: "pointer" }} onClick={() => location.href = "/#/index"}>{route.breadcrumbName}</span>
+        if(!window["__POWERED_BY_QIANKUN__"]) {
+            return <Link to={"/"}>{route.breadcrumbName}</Link>
+        } else {
+            return <span style={{ cursor: "pointer" }} onClick={() => location.href = "/#/index"}>{route.breadcrumbName}</span>
+        }
+
     } else if(route.path === null) {
         return <span>{route.breadcrumbName}</span>
     } else {
