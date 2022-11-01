191c1e8af825faabf9772ef1c855267683d9d712
Emma <imgenu@gmail.com>
2022-11-01 16:16:00 +0800

    feat:APIADMIN-104_參數配置組件 - 輸入框

---
+++ packages/cms-webpack4/src/modules/system/components/pages/ConfigManagePage/components/ConfigInput.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Button, Form, InputNumber, Modal, Radio, Space,Tag,Input, } from 'antd';
+import { ConfigList } from "../../../../api/types/configManageTypes/getConfigList";
+import { CheckOutlined,CloseOutlined  } from '@ant-design/icons';
+const { TextArea } = Input;
+type ConfigInputProps=ConfigList&{
+    saveValue:any;
+    inputKey:string;
+}
+
+function ConfigInput(props:ConfigInputProps){
+    const {value,inputType,channelId,saveValue,inputKey}=props;
+
+    const [form] = Form.useForm();
+    const [isEdit,setIsEdit]=useState(false);
+    
+    const handleOnChange=(e)=>{
+        setIsEdit(true);
+    }
+
+    const handleReset=()=>{
+        form.setFieldsValue({[inputKey]:value});
+        setIsEdit(false);
+
+    }
+    const onFinish=(value)=>{
+        saveValue(inputKey, channelId, Object.values(value))
+
+    }
+    return (
+        <Form style={{ display: 'flex' }} form={form} onFinish={onFinish} initialValues={{ [inputKey]: value }}>
+            <Space>
+                {isEdit && <Space>
+                    <Button htmlType="submit" icon={<CheckOutlined />} type="primary" size="small" />
+                    <Button onClick={handleReset} icon={<CloseOutlined />} type="ghost" size="small" />
+                </Space>}
+                <Form.Item style={{ marginBottom: '0' ,width:'200px'}} rules={[{ required: true, message: '不能為空' }]} label="" name={inputKey} >
+                    {inputType === 'textarea' ?
+                        <TextArea  onChange={handleOnChange}/> :
+                        <Input onChange={handleOnChange}/>}
+                </Form.Item>
+            </Space>
+        </Form>
+    )
+}
+
+export default ConfigInput;
