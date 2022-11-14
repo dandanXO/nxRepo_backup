80f0bf52ac7023a3b88d2afd5ba9e6ba526f9802
fronteric <fronteric@1.1>
2022-11-14 15:41:01 +0800

    feat(mobile): bank bind

--- apps/mobile/src/app/api/GetBindCardDropList.ts
+++ apps/mobile/src/app/api/GetBindCardDropList.ts
@@ -1,9 +1,15 @@
 export interface GetBindCardDropListResponse {
   availableWalletVendors: WalletVendor[];
   showBankOption: boolean;
+  availableBanks: BankVendor[];
 }
 
 export interface WalletVendor {
   code:	string;
   displayName:	string;
+}
+
+export interface BankVendor {
+  "bankCode": string;
+  "bankName": string;
 }
