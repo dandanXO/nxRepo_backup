78a9e5fa1d34fb8a965936a02fad0d5a5cc71a03
fronteric <fronteric@1.1>
2022-11-22 11:30:47 +0800

    feat: API-374 替換孟加拉語系

--- apps/mobile/src/app/components/pages/UploadedPaymentReceiptPage/i18n/translations.ts
+++ apps/mobile/src/app/components/pages/UploadedPaymentReceiptPage/i18n/translations.ts
@@ -9,7 +9,9 @@ export const i18nUploadedPaymentReceiptPage: i18nComponent = {
       "Done": "Done",
     },
     bn_BD: {
-
+      "Upload payment receipt": "পেমেন্টের রসিদ আপলোড করো",
+      "Thank you. Your receipt has been uploaded successfully.": "ধন্যবাদ. তোমার রসিদ সফলভাবে আপলোড হয়েছে",
+      "Done": "সম্পন্ন হয়েছে",
     }
   }
 }
