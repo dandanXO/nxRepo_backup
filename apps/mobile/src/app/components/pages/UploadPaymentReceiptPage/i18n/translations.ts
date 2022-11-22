bb6d1e9317c2ae892aa437e44343114233015c22
fronteric <fronteric@1.1>
2022-11-22 11:29:12 +0800

    feat: API-376 替換孟加拉語系

--- apps/mobile/src/app/components/pages/UploadPaymentReceiptPage/i18n/translations.ts
+++ apps/mobile/src/app/components/pages/UploadPaymentReceiptPage/i18n/translations.ts
@@ -14,7 +14,13 @@ export const i18nUploadPaymentReceiptPage: i18nComponent = {
       // PakistanUploadPaymentReceiptPage
     },
     bn_BD: {
-
+      // Common
+      "Upload your repayment receipt(optional)": "তোমার ঋণ পরিশোধের রসিদ আপলোড কর (ঐচ্ছিক)",
+      "Upload from Photo Album": "তোমার ঋণ পরিশোধের রসিদ আপলোড কর (ঐচ্ছিক)",
+      "Confirm": "নিশ্চিত করুন",
+      // IndiaUploadPaymentReceiptPage
+      "UTR": "UTR",
+      // PakistanUploadPaymentReceiptPage
     }
   }
 }
