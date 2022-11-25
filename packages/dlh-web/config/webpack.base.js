99c45ffe994de334b5c69f8bc2aee73f3a32dc9a
fronteric <fronteric@1.1>
2022-11-25 14:11:13 +0800

    feat(dlh-web): 根據時區顯示時間

--- packages/dlh-web/config/webpack.base.js
+++ packages/dlh-web/config/webpack.base.js
@@ -6,6 +6,8 @@ const config = require('./index');
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 const argv = yargs(hideBin(process.argv)).argv
+
+
 module.exports = {
     resolve: {
         extensions: ['.js', '.jsx', '.json'],
