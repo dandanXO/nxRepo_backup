36a6aac5803ac4b09a6161213c30f4a7a8feff97
Emma <imgenu@gmail.com>
2022-11-28 12:48:21 +0800

    feat(cms):add Copy component

---
+++ packages/cms-webpack4/src/modules/shared/components/CopyText.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { Tooltip, message} from 'antd';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+
+// acturalCopy 實際複製的可以與顯示的內容不同，沒給就是預設text 
+function CopyText({ text, acturalCopy = text}) {
+
+    const handleCopy = () => {
+        message.success('复制成功');
+    }
+    return (
+        <div style={{cursor:'pointer'}}> 
+            {text &&
+                <CopyToClipboard text={acturalCopy} onCopy={handleCopy}> 
+                    <Tooltip title={text} >
+                        <span>{text}</span>
+                    </Tooltip>
+                </CopyToClipboard>
+            }
+        </div>
+    )
+}
+
+export default CopyText;
