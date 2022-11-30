71f2454182a987eef8d76af80387d7a97c6538e9
fronteric <fronteric@1.1>
2022-11-30 12:58:23 +0800

    feat(mobile): API-512 [前端] 印 api 上傳還款證明頁_僅填寫UTR資料送出後會失敗

--- apps/mobile/src/main.tsx
+++ apps/mobile/src/main.tsx
@@ -2,10 +2,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
-import {environment} from "./environments/environment";
-
-console.log("environment.production", environment.production)
-console.log("environment.country", environment.country)
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
