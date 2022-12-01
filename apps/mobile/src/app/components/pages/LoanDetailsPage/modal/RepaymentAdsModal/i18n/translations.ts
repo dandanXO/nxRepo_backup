f5c624602df615ce61c954816f91a51aa8364085
fronteric <fronteric@1.1>
2022-12-01 15:45:23 +0800

    feat(mobile): API-454 [多國] H5 付款頁 - 付款方式增加廣告轉化率行銷步驟

--- apps/mobile/src/app/components/pages/LoanDetailsPage/modal/RepaymentAdsModal/i18n/translations.ts
+++ apps/mobile/src/app/components/pages/LoanDetailsPage/modal/RepaymentAdsModal/i18n/translations.ts
@@ -1,7 +1,7 @@
 import {i18nComponent} from "../../../../../../i18n/i18nComponent";
 
 export const i18nRepaymentAdsModal: i18nComponent = {
-  namespace: "i18nRepaymentAdsModal",
+  namespace: "pages/modals/i18nRepaymentAdsModal",
   translation: {
     en_US: {
       // NOTE: Long Title
@@ -25,20 +25,20 @@ export const i18nRepaymentAdsModal: i18nComponent = {
     },
     bn_BD: {
       // NOTE: Long Title
-      "Available for higher amount": "Available for higher amount",
-      "Available for lower interest rates": "Available for lower interest rates",
+      "Available for higher amount": "বেশি পরিমাণে অর্থ পাওয়ার জন্য উপলব্ধ",
+      "Available for lower interest rates": "কম পরিমাণ মুনাফার জন্য উপলব্ধ",
 
       // NOTE: Short title
-      "Good news for you…": "Good news for you…",
-      "Just finish within 24 hours…": "Just finish within 24 hours…",
-      "Don’t rush to leave, here’s a time-limited offer": "Don’t rush to leave, here’s a time-limited offer",
-      "only today": "only today",
-      "special offer": "special offer",
-      "this week only": "this week only",
+      "Good news for you…": "তোমার জন্য সুসংবাদ...",
+      "Just finish within 24 hours…": "শুধুমাত্র ২৪ ঘন্টার মধ্যে শেষ করো...",
+      "Don’t rush to leave, here’s a time-limited offer": "চলে যাওয়ার জন্য তাড়াহুড়ো করো না, এখানে একটি সীমিত সময়ের জন্য অফার রয়েছে ",
+      "only today": "শুধুমাত্র আজকে",
+      "special offer": "বিশেষ অফার",
+      "this week only": "শুধুমাত্র এই সপ্তাহ",
 
       // NOTE: other
-      "Limited Time Offer": "Limited Time Offer",
-      "Countdown": "Countdown",
+      "Limited Time Offer": "অফারটি সীমিত সময়ের",
+      "Countdown": "গণনা",
       "Repayment": "পরিশোধের",
       "Repay and Apply Again": "পরিশোধ করুন এবং আবার আবেদন করুন",
     }
