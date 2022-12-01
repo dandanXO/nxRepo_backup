5595f703dbda6b8999c47402f143c41dcaf39a5c
fronteric <fronteric@1.1>
2022-12-01 11:20:51 +0800

    feat(cms): i18n

--- packages/cms-webpack4/src/index/i18n.ts
+++ packages/cms-webpack4/src/index/i18n.ts
@@ -1,5 +1,7 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
+import {environment} from "../../../../apps/mobile/src/environments/environment";
+import {AllLanguage} from "../../../../apps/mobile/src/environments/config/languages/IAllLanguage";
 
 i18next
     .use(initReactI18next) // passes i18n down to react-i18next
@@ -9,6 +11,17 @@ i18next
         // (tip move them in a JSON file and import them,
         // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
         resources: {
+            zh_cn: {
+                common: {
+                    "Welcome to CMS": "歡迎 CMS !!!",
+                },
+                "page1": {
+                    "Welcome": "Page1 - 歡迎 Page1 !!!",
+                },
+                "page2": {
+                    "Welcome": "Page2 - 歡迎 Page2 !!!",
+                },
+            },
             en_US: {
                 common: {
                     "Welcome to CMS": "common Welcome to CMS !!!",
@@ -21,8 +34,8 @@ i18next
                 },
             }
         },
-        lng: "en_US", // if you're using a language detector, do not define the lng option
-        fallbackLng: "en_US",
+        lng: "zh_cn", // if you're using a language detector, do not define the lng option
+        fallbackLng: "zh_cn",
 
         interpolation: {
             escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
@@ -37,3 +50,22 @@ i18next
         console.log("i18next-callback:", t('Welcome', {ns: "page2"}));
     });
 
+// NOTE: HOW to change language
+// i18next
+//     .changeLanguage("zh_cn")
+//     .then((t) => {
+//         console.log("changeLanguage:", "zh_cn");
+//     })
+//     .catch((error) => {
+//         console.log("changeLanguage:", "zh_cn");
+//         console.log("changeLanguage - error:", error);
+//     })
+// i18next
+//     .changeLanguage("en_US")
+//     .then((t) => {
+//         console.log("changeLanguage:", "en_US");
+//     })
+//     .catch((error) => {
+//         console.log("changeLanguage:", "en_US");
+//         console.log("changeLanguage - error:", error);
+//     })
