e3aedcd7ccdb8d1b3e1a766fc00061616f89ad7e
fronteric <fronteric@1.1>
2022-12-02 17:26:58 +0800

    feat(mobile&cms): API-464, API-465 H5 廣告列表, 後台廣告列表

--- apps/mobile/src/app/components/pages/ActivityAdListPage/components/AdTemplate2.tsx
+++ apps/mobile/src/app/components/pages/ActivityAdListPage/components/AdTemplate2.tsx
@@ -2,11 +2,20 @@ import {AdTemplateCard} from "../index";
 import {AdContainer} from "./AdContainer";
 import styled from "styled-components";
 
+const makeStyleEllipsisOverflowText = () => {
+  return `
+        // NOTICE:
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+    `;
+}
+
 export interface AdTemplate2 {
-    type?: "adTemplate2",
-    brandCard: AdTemplate2BrandCard;
-    topCard: AdTemplate2Card;
-    bottomCard: AdTemplate2Card;
+  type?: "adTemplate2",
+  brandCard: AdTemplate2BrandCard;
+  topCard: AdTemplate2Card;
+  bottomCard: AdTemplate2Card;
 }
 
 export interface AdTemplate2BrandCard extends AdTemplateCard{
@@ -38,14 +47,20 @@ const Title1 = styled.div`
   font-size: 12px;
   font-weight: 500;
   color: #845e3a;
+  // NOTICE:
+  ${makeStyleEllipsisOverflowText()};
 `
 const Title2 = styled.div`
   font-size: 16px;
   font-weight: 500;
   color: #845e3a;
+  // NOTICE:
+  ${makeStyleEllipsisOverflowText()};
 `
 const Price = styled.div`
   margin-bottom: 12px;
+  // NOTICE:
+  ${makeStyleEllipsisOverflowText()};
 `;
 const PriceUnit = styled.span`
   font-size: 14px;
@@ -68,15 +83,18 @@ const Button = styled.div<{bgColor: string}>`
   padding: 2px 16px;
   color: #fff;
   font-size: 12px;
-  background-color: ${props => props.bgColor || "#ef7e3a\n" +
-    "\n"};
+  background-color: ${props => props.bgColor || "#ef7e3a"};
+  // NOTICE:
+  ${makeStyleEllipsisOverflowText()};
 `
+
 const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   height: 130px;
   flex: 1 1;
+  overflow: hidden;
 `;
 const Card = styled.div<{bgColor: string}>`
   box-sizing: border-box;
@@ -89,12 +107,15 @@ const Title = styled.div<{color: string}>`
   font-weight: 500;
   color: ${props => props.color || "#469c7d"};
   margin-bottom: 4px;
+  // NOTICE:
+  ${makeStyleEllipsisOverflowText()};
 `;
+
 interface IAdTemplate2 {
-    data?: AdTemplate2;
+  data?: AdTemplate2;
 }
 export const AdTemplate2 = (props: IAdTemplate2) => {
-    console.log("props", props)
+  // console.log("props", props)
   return (
     <AdTemplate2Container>
       <BrandCard>
