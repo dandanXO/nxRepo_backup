b39a50b1c6aab7b7e8e3c8caf3c08b4a198f6b79
fronteric <fronteric@1.1>
2022-12-14 20:32:58 +0800

    feat: APIADMIN-265 [商戶管理] #4 當日催收

--- packages/dlh-web/src/pages/todayLoanManage/TodayOrderDistribute/SearchList/SearchList.js
+++ packages/dlh-web/src/pages/todayLoanManage/TodayOrderDistribute/SearchList/SearchList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Form, DatePicker, Row, Col, Button, Select, Input } from 'antd';
+import {FormattedMessage, injectIntl} from "react-intl";
 const { Option } = Select;
 
 const { RangePicker } = DatePicker;
@@ -21,41 +22,64 @@ class SearchList extends Component{
         const { form: { getFieldsValue }, handleSearch } = this.props;
         handleSearch(getFieldsValue());
     }
+    renderMerchants = () => {
+      const { allMerchants } = this.props;
+      if(!allMerchants) return;
+      const ele = allMerchants.map(item => <Option key={item.merchantId} value={item.merchantId} >{item.name}</Option>);
+      return [<Option value={''} key={''}><FormattedMessage id="page.search.list.no.restrict" /></Option>].concat(ele);
+    }
+
+
     render() {
-        const { form: { getFieldDecorator  }  } = this.props;
+        const { form: { getFieldDecorator  }, intl, isSuperAdmin  } = this.props;
         return (
             <div>
                 <Form onSubmit={this.submit}>
-                    {/* <Row gutter={40}>
-                        <Col span={8}>
-                            <Form.Item {...formItemLayout} label={"逾期时间"}>
-                                {
-                                    getFieldDecorator('time', {
-                                        initialValue: []
-                                    })(
-                                        <RangePicker placeholder={['请选择', '请选择']}/>
-                                    )
-                                }
-                            </Form.Item>
-                        </Col>
-                        <Col span={8}>
-                            <Form.Item {...formItemLayout} label={"手机号"}>
-                                {
-                                    getFieldDecorator('userPhone', {
-                                        initialValue: ''
-                                    })(
-                                        <Input placeholder={'请输入手机号'}/>
-                                    )
-                                }
-                            </Form.Item>
-                        </Col>
+                     <Row gutter={40}>
+                        {/*<Col span={8}>*/}
+                        {/*    <Form.Item {...formItemLayout} label={"逾期时间"}>*/}
+                        {/*        {*/}
+                        {/*            getFieldDecorator('time', {*/}
+                        {/*                initialValue: []*/}
+                        {/*            })(*/}
+                        {/*                <RangePicker placeholder={['请选择', '请选择']}/>*/}
+                        {/*            )*/}
+                        {/*        }*/}
+                        {/*    </Form.Item>*/}
+                        {/*</Col>*/}
+                        {/*<Col span={8}>*/}
+                        {/*    <Form.Item {...formItemLayout} label={"手机号"}>*/}
+                        {/*        {*/}
+                        {/*            getFieldDecorator('userPhone', {*/}
+                        {/*                initialValue: ''*/}
+                        {/*            })(*/}
+                        {/*                <Input placeholder={'请输入手机号'}/>*/}
+                        {/*            )*/}
+                        {/*        }*/}
+                        {/*    </Form.Item>*/}
+                        {/*</Col>*/}
+                         {isSuperAdmin && (
+                           <Col lg={12} xl={8}>
+                             <Form.Item {...formItemLayout} label={intl.formatMessage({id : "page.search.list.merchantName"})}>
+                               {
+                                 getFieldDecorator('merchantId', {
+                                   initialValue: ''
+                                 })(
+                                   <Select>
+                                     {this.renderMerchants()}
+                                   </Select>
+                                 )
+                               }
+                             </Form.Item>
+                           </Col>
+                         )}
                         <Col span={8}>
                             <Form.Item style={{textAlign:'right'}}>
                                 <Button type={'primary'} htmlType={'submit'}>查询</Button>
                             </Form.Item>
-                        </Col> 
+                        </Col>
                     </Row>
-                        */}
+
                 </Form>
             </div>
 
@@ -71,4 +95,4 @@ SearchList.defaultProps = {
 };
 
 
-export default Form.create()(SearchList);
+export default Form.create()(injectIntl(SearchList));
--- packages/dlh-web/src/pages/todayLoanManage/TodayOrderDistribute/TodayOrderDistribute.js
+++ packages/dlh-web/src/pages/todayLoanManage/TodayOrderDistribute/TodayOrderDistribute.js
@@ -9,45 +9,57 @@ import SearchList from './SearchList/SearchList';
 import {convertMoneyFormat} from "utils";
 import PropTypes from 'prop-types';
 import {FormattedMessage, injectIntl} from "react-intl";
+import {getAllMerchants, getIsSuperAdmin} from "../../../utils";
 
 
 class TodayOrderDistribute extends Component {
-    columns = [
-        {title: <FormattedMessage id="page.search.list.order.no"/>, dataIndex: 'orderNo', key: 'orderNo'},
-        {title: <FormattedMessage id="page.search.list.name"/>, dataIndex: 'userTrueName', key: 'userTrueName'},
-        {title: <FormattedMessage id="page.search.list.mobile"/>, dataIndex: 'userPhone', key: 'userPhone'},
-        {title: <FormattedMessage id="page.search.list.product.name"/>, dataIndex: 'productName', key: 'productName' },
-        {title: <FormattedMessage id="page.table.appName"/>, dataIndex: 'appName', key: 'appName' },
-        {
-            title: <FormattedMessage id="page.table.loan2"/>,
-            dataIndex: 'deviceMoney',
-            key: 'deviceMoney',
-            render(text, record) {
-                return convertMoneyFormat(text);
-            }
-        },
-        // { title: '展期次数', dataIndex: 'lengNum', key: 'lengNum' },
-        {
-            title: <FormattedMessage id="page.search.list.expiration.time"/>,
-            dataIndex: 'expireTime',
-            key: 'expireTime',
-            render(text) {
-                return moment(Number(text) * 1000).format('YYYY-MM-DD HH:mm:ss');
-            }
-        }
-        // ,
-        // { title: '催收组', dataIndex: 'departmentName', key: 'departmentName' }
-        // { title: '上次催收人', dataIndex: 'lastPerson', key: 'lastPerson' }
-    ]
-
     constructor(props) {
         super(props);
+        const isSuperAdmin = getIsSuperAdmin();
+        const allMerchants = getAllMerchants();
         this.state = {
+            isSuperAdmin,
+            allMerchants,
             pageSize: 10,
             // selectedRowKeys: []
         };
         //存储搜索参数
         this.searchPrams = {};
+        this.columns = [
+          {title: <FormattedMessage id="page.search.list.order.no"/>, dataIndex: 'orderNo', key: 'orderNo'},
+          {title: <FormattedMessage id="page.search.list.name"/>, dataIndex: 'userTrueName', key: 'userTrueName'},
+          {title: <FormattedMessage id="page.search.list.mobile"/>, dataIndex: 'userPhone', key: 'userPhone'},
+          {title: <FormattedMessage id="page.search.list.product.name"/>, dataIndex: 'productName', key: 'productName' },
+          {title: <FormattedMessage id="page.table.appName"/>, dataIndex: 'appName', key: 'appName' },
+          {
+            title: <FormattedMessage id="page.table.loan2"/>,
+            dataIndex: 'deviceMoney',
+            key: 'deviceMoney',
+            render(text, record) {
+              return convertMoneyFormat(text);
+            }
+          },
+          // { title: '展期次数', dataIndex: 'lengNum', key: 'lengNum' },
+          {
+            title: <FormattedMessage id="page.search.list.expiration.time"/>,
+            dataIndex: 'expireTime',
+            key: 'expireTime',
+            render(text) {
+              return moment(Number(text) * 1000).format('YYYY-MM-DD HH:mm:ss');
+            }
+          }
+          // ,
+          // { title: '催收组', dataIndex: 'departmentName', key: 'departmentName' }
+          // { title: '上次催收人', dataIndex: 'lastPerson', key: 'lastPerson' }
+        ]
+
+        if(isSuperAdmin) {
+          this.columns.unshift({
+            title: props.intl.formatMessage({id: "page.search.list.merchantName"}),
+            dataIndex: 'merchantName',
+            key: 'merchantName'
+          })
+        }
     }
 
     //选择订单
@@ -86,7 +98,7 @@ class TodayOrderDistribute extends Component {
         const {distributeOrder, getTableData, personType} = this.props;
 
         //let key = personType === 'group' ? 'departmentId' : 'collectorId';
-   
+
         //todo 分配订单回调？ type 1 = 订单分配 , type2 = 重新分配 ( 逾期的沒有type )
         distributeOrder({type: 1, disIds: selectKeys.join(','), personIds: obj.join(',')}, () => {
             getTableData({pageSize: pageSize, pageNum: 1, collectorId: null, ...this.searchPrams});
@@ -101,7 +113,8 @@ class TodayOrderDistribute extends Component {
         const params = {
             startTime: isArr ? time[0].format('YYYY-MM-DD HH:mm:ss') : '',
             endTime: isArr ? time[1].format('YYYY-MM-DD HH:mm:ss') : '',
-            userPhone: obj['userPhone']
+            userPhone: obj['userPhone'],
+            merchantId: obj['merchantId'],
         };
         this.searchPrams = params;
         getTableData({...params, pageSize: pageSize, pageNum: 1, collectorId: null});
@@ -177,7 +190,7 @@ class TodayOrderDistribute extends Component {
 
         return (
             <div>
-                <SearchList handleSearch={this.handleSearch}/>
+                <SearchList handleSearch={this.handleSearch} isSuperAdmin={this.state.isSuperAdmin} allMerchants={this.state.allMerchants}/>
                 <div>
                     <Button type={'primary'} onClick={this.distributeOrder}><FormattedMessage id="windowPage.distribute.order"/></Button>
                     <Button type={'primary'} onClick={this.handleAll}><FormattedMessage id="button.todayOrder.all"/></Button>
