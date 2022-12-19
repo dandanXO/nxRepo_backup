9d7f4d17e323fff2363abb3952ff6362e32b710e
fronteric <fronteric@1.1>
2022-12-19 15:28:32 +0800

    feat: APIADMIN-47 [APP參數配置] for QA&維運設定

--- packages/cms-webpack4/src/modules/product/components/pages/AppManagePage/AppManageForm.tsx
+++ packages/cms-webpack4/src/modules/product/components/pages/AppManagePage/AppManageForm.tsx
@@ -6,15 +6,20 @@ const { Text } = Typography;
 const { Option } = Select;
 import {AdminForm, AdminFormProps} from "../../../../shared/components/AdminForm";
 import {Switch} from "antd";
+import {AppConfiguration} from "../../../service/appManage/domain/AppConfiguration";
+import "./style.less";
 
 type AppManageFormProps = {
     isEdit: boolean;
     id?: number;
 } & AdminFormProps & {
-
+    idCardOcrList: string[];
+    liveDetectList: string[];
+    taxCardOcrList: string[];
 };
 
 export const AppManageForm = (props: AppManageFormProps) => {
+    const hasPartnershipUrl = Form.useWatch('showPartnership', props.form) ? false : true;
     return (
         <AdminForm
             form={props.form}
@@ -22,7 +27,7 @@ export const AppManageForm = (props: AppManageFormProps) => {
             onFieldsChange={props.onFieldsChange}
             onFinish={props.onFinish}
         >
-            <Form.Item label="APP名稱" name={"name"} rules={[{ required: true }]}
+            <Form.Item label="APP名稱" name={"appName"} rules={[{ required: true }]}
                 extra={"设定后即无法直接修改，需请求技术支持，送出前请务必再次确认。"}
             >
                 <Input placeholder={"APP名稱"}/>
@@ -34,67 +39,60 @@ export const AppManageForm = (props: AppManageFormProps) => {
                 <Input placeholder={"PackageID"} disabled={props.isEdit}/>
             </Form.Item>
 
-            <Collapse defaultActiveKey={['1']} ghost>
+            <Collapse defaultActiveKey={['1', '2']} ghost className={"base-setting"}>
                 <Panel header="基本配置" key="1">
-                    <Form.Item>
+                    <div>
 
-                    </Form.Item>
-
-                    <Form.Item>
-                        <Form.Item name="top" label="是否需要先登入" valuePropName="checked">
+                        <Form.Item name="showNbfc" label="NBFC开关" valuePropName="checked" colon={false}>
                             <Switch checkedChildren="是" unCheckedChildren="否"/>
                         </Form.Item>
-                    </Form.Item>
 
-
-
-                    <Form.Item name="top" label="是否强制跳转到KYC流程" valuePropName="checked">
-                        <Switch checkedChildren="是" unCheckedChildren="否"/>
-                    </Form.Item>
-
-                    <Form.Item name="top" label="是否显示受权页面" valuePropName="checked">
-                        <Switch checkedChildren="是" unCheckedChildren="否"/>
-                    </Form.Item>
-
-                    <Form.Item name="top" label="是否显示条款页面" valuePropName="checked">
-                        <Switch checkedChildren="是" unCheckedChildren="否"/>
-                    </Form.Item>
-
-                    <Input.Group compact>
-
-                        <Form.Item name="top" valuePropName="checked">
+                        <Form.Item name="loginFirst" label="是否需要先登入" valuePropName="checked" colon={false}>
                             <Switch checkedChildren="是" unCheckedChildren="否"/>
                         </Form.Item>
-                        <Form.Item name="top" label="显示合作伙伴开关" valuePropName="checked">
+
+                        <Form.Item name="kycFirst" label="是否强制跳转到KYC流程" valuePropName="checked" colon={false}>
+                            <Switch checkedChildren="是" unCheckedChildren="否"/>
                         </Form.Item>
-                        <Form.Item name={"packageId"}>
-                            <Input placeholder={"合作伙伴H5 URL"} disabled={props.isEdit}/>
+
+                        <Form.Item name="showPermission" label="是否显示受权页面" valuePropName="checked" colon={false}>
+                            <Switch checkedChildren="是" unCheckedChildren="否"/>
                         </Form.Item>
-                    </Input.Group>
+
+                        <Form.Item name="showTermAndCondition" label="是否显示条款页面" valuePropName="checked" colon={false}>
+                            <Switch checkedChildren="是" unCheckedChildren="否"/>
+                        </Form.Item>
+
+                        <Input.Group>
+                            <Form.Item name="showPartnership" label="显示合作伙伴开关" valuePropName="checked" style={{marginRight: 20}} colon={false}>
+                                <Switch checkedChildren="是" unCheckedChildren="否"/>
+                            </Form.Item>
+
+                            <Form.Item name={"partnershipUrl"} style={{ width: 400}}>
+                                <Input placeholder={"合作伙伴H5 URL"} disabled={hasPartnershipUrl}/>
+                            </Form.Item>
+                        </Input.Group>
+
+                    </div>
 
                 </Panel>
+
                 <Panel header="检测服务配置" key="2">
-                    <Form.Item label="税卡" name={"name"}>
+                    <Form.Item label="税卡" name={"taxCardOcr"}>
                         <Select>
-                            <Select.Option value="mins">分钟</Select.Option>
-                            <Select.Option value="hours">小时</Select.Option>
-                            {/*{props?.merchantList?.map((i) => <Option key={i.merchantId} value={i.merchantId}>{i.name}</Option>)}*/}
+                            {props.taxCardOcrList.map((item, i) => <Option key={i} value={item}>{item}</Option>)}
                         </Select>
                     </Form.Item>
 
-                    <Form.Item label="实名卡" name={"name"}>
+                    <Form.Item label="实名卡" name={"idCardOcr"}>
                         <Select>
-                            <Select.Option value="mins">分钟</Select.Option>
-                            <Select.Option value="hours">小时</Select.Option>
-                            {/*{props?.merchantList?.map((i) => <Option key={i.merchantId} value={i.merchantId}>{i.name}</Option>)}*/}
+                            {props.idCardOcrList.map((item, i) => <Option key={i} value={item}>{item}</Option>)}
                         </Select>
                     </Form.Item>
 
-                    <Form.Item label="人脸检测服务" name={"name"}>
+                    <Form.Item label="人脸检测服务" name={"liveDetect"}>
                         <Select>
-                            <Select.Option value="mins">分钟</Select.Option>
-                            <Select.Option value="hours">小时</Select.Option>
-                            {/*{props?.merchantList?.map((i) => <Option key={i.merchantId} value={i.merchantId}>{i.name}</Option>)}*/}
+                            {props.liveDetectList.map((item, i) => <Option key={i} value={item}>{item}</Option>)}
                         </Select>
                     </Form.Item>
 
