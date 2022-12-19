33ac25e222ed8ef9ab0f5d6ec95de7ff8486faef
fronteric <fronteric@1.1>
2022-12-19 10:43:49 +0800

    refactor: google qrcode

--- packages/dlh-web/src/pages/login/Login/api/index.js
+++ packages/dlh-web/src/pages/login/Login/api/index.js
@@ -1,4 +1,22 @@
 import { axios } from 'utils';
+
+// NOTE: '/hs/admin/auth/login'
+// Response
+// {
+//   "code":200,
+//   "data":{
+//       "passGoogleAuth":false,
+//       "role":"MERCHANT_ADMIN",
+//       "merchantId":5,
+//       "appName":"",
+//       "loginIp":"36.228.214.152",
+//       "googleAuthFlag":true,
+//       "operatorId":35,
+//       "phoneNo":"12341234",
+//       "token":"2815c854ebd445d8ac0eba809187cc6a"
+//   }
+// }
+
 const api = {
     loginVerifyCodeData: '/hs/admin/auth/sendVerifyCode',
     login: '/hs/admin/auth/login',
