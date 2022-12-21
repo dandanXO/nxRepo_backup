d3deb492910081d338c32a0656df63fb539a3189
fronteric <fronteric@1.1>
2022-12-21 15:40:34 +0800

    feat: add Sentry

--- apps/mobile/src/main.tsx
+++ apps/mobile/src/main.tsx
@@ -1,3 +1,4 @@
+import "./sentry";
 // NOTE: [TypeScript style guide](https://ts.dev/style/#identifiers)
 import React from "react";
 import ReactDOM from "react-dom/client";
---
+++ apps/mobile/src/sentry.ts
@@ -0,0 +1,14 @@
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+import {environment} from "./environments/environment";
+
+
+Sentry.init({
+  dsn: "https://24e0adc52fe84c69812ad7f500b771d8@o4504354754985984.ingest.sentry.io/4504365838434304",
+  integrations: [new BrowserTracing()],
+  environment: `${environment.countryName}`,
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
