ffd4caff04772e880a642d015803f3411ef41896
Emma <imgenu@gmail.com>
2023-01-09 14:28:21 +0800

    feat(cms):APIADMIN-371_[還款補單管理] 還款明細UTR查询功能

---
+++ packages/cms-webpack4/src/modules/financial/api/PayReceiptApi.ts
@@ -0,0 +1,29 @@
+import { API } from "../../../api";
+import { GetPayReceiptListRequestQuerystring,GetPayReceiptListProps } from "./types/PayReceiptTypes/getPayReceiptList";
+import { PostPayRecieptConfirmRequest } from "./types/PayReceiptTypes/postPayReceiptConfirm";
+
+const PayReceiptApi = API.injectEndpoints({
+    overrideExisting: false,
+    endpoints: (builder) => ({
+        // NOTE: GET /hs​/admin​/pay-receipt​/list 查詢還款憑證列表
+        getPayReceiptList: builder.query<GetPayReceiptListProps, GetPayReceiptListRequestQuerystring>({
+            query: (requestBody: GetPayReceiptListRequestQuerystring) => ({
+                url: `/pay-receipt/list`,
+                params: requestBody,
+                method: "get",
+            }),
+        }),
+        // NOTE: POST /hs/admin/pay-receipt/confirm 确认还款明细
+         postPayReceiptConfirm: builder.mutation<null, PostPayRecieptConfirmRequest>({
+            query: (requestBody: PostPayRecieptConfirmRequest) => ({
+                url: `/pay-receipt/confirm`,
+                method: "post",
+                data: requestBody,
+            }),
+        }),
+    })
+})
+export const {
+    useLazyGetPayReceiptListQuery,
+    usePostPayReceiptConfirmMutation
+} = PayReceiptApi;
