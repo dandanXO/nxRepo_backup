87a8142f7f67d48ba14097c6a21545a21ba483a6
fronteric <fronteric@1.1>
2023-02-15 17:35:53 +0800

    feat(FRONTEND-43): 新客風控回款率

--- packages/cms-webpack4/src/modules/product/components/pages/ProductPage/index.tsx
+++ packages/cms-webpack4/src/modules/product/components/pages/ProductPage/index.tsx
@@ -60,7 +60,7 @@ export const ProductPage = () => {
         },
       }}
     >
-      <ProductTable triggerToRefreshList={triggerGetList} productListData={productListData} setProductModalData={setProductModalData} />
+      <ProductTable triggerGetList={triggerGetList} productListData={productListData} setProductModalData={setProductModalData} />
 
       {productModalData.show && (
         <ProductModal
