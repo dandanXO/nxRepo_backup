1bac041b0a0d14123c0f2d15d3a7cafad3367570
Emma <imgenu@gmail.com>
2023-02-17 10:55:13 +0800

    feat(cms):FRONTEND-63_[系統管理] 登入帳號管理

--- packages/cms-webpack4/src/modules/system/components/pages/LoginAccountManage/LoginAccountManageTable.tsx
+++ packages/cms-webpack4/src/modules/system/components/pages/LoginAccountManage/LoginAccountManageTable.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import type { ProColumns } from '@ant-design/pro-components';
 import { ProTable } from '@ant-design/pro-components';
-import { Button, Form, Input, Modal, Radio, Space, List } from 'antd';
+import { Button, Form, Input, Modal, Radio, Space, List ,AutoComplete} from 'antd';
 import { LoginAccountList, GetLoginAccountListRequestQuery } from '../../../api/types/LoginAccountManageTypes/getLoginAccountList';
 import { usePostLogoutMutation, useLazyGetLoginAccountListQuery } from '../../../api/LoginAccountManageApi';
 import { ProColumnsOperationConstant } from "../../../../shared/components/common/ProColumnsOperationConstant";
 import { getIsSuperAdmin } from '../../../../shared/storage/getUserInfo';
-import useGetMerchantEnum from '../../../../shared/hooks/common/useGetMerchantEnum';
+import useGetMerchantEnum from '../../../../shared/hooks/useGetMerchantEnum';
 
 const LoginAccountManageTable = () => {
 
@@ -39,13 +39,13 @@ const LoginAccountManageTable = () => {
         setSelectedRow(selectedRowKeys);
     };
 
-    const onExpandChange=(expandRowKeys)=>{
+    const onExpandChange = (expandRowKeys) => {
         setExpandRow(expandRowKeys)
     }
 
     useEffect(() => {
         triggerGetList(searchList);
-    }, [searchList,postLogoutIsSuccess])
+    }, [searchList, postLogoutIsSuccess])
 
     useEffect(() => {
         if (isSuperAdmin) {
@@ -67,35 +67,48 @@ const LoginAccountManageTable = () => {
     }
 
 
+    const handleLogout = (confirmType, record) => {
+        const confirmText = confirmType === 'ip' ? `确认要踢出此IP位址吗？` : `确认要踢出此用户吗？`;
+
+        modal.confirm({
+            content: confirmText,
+            onOk() {
+                postLogout({ tokens: confirmType === 'ip' ? [record.token] : record.tokens })
+                    .unwrap().then(() => onSelectChange(selectedRow.filter(i => i !== record.accountNumber)));
+            }
+        });
+    }
+
+
     const columns: ProColumns<LoginAccountList>[] = [
         {
             title: '操作',
             valueType: 'option',
             key: 'option',
-            render: (text, record, _, action) => [<a key="editable" onClick={() => handleLogoutAll()} >踢出</a>],
+            render: (text, record, _, action) => [<a key="editable" onClick={() => handleLogout('account', record)} >踢出</a>],
             width: '50px',
         },
-        { title: '手机号/登入帐号', dataIndex: 'accountNumber', key: 'accountNumber', initialValue: "" },
+        { title: '手机号/登入帐号', dataIndex: 'accountNumber', key: 'accountNumber', initialValue: "", render: (text) => <CopyText text={text} /> },
         { title: 'IP位址', dataIndex: 'ip', key: 'ip', initialValue: "", hideInTable: true },
         { title: '登入地区', dataIndex: 'loginLocation', key: 'loginLocation', initialValue: "", hideInTable: true },
         { title: '最后活跃时间', dataIndex: 'lastActiveTime', key: 'lastActiveTime', initialValue: "", hideInSearch: true },
         {
-            title: '最后活跃时间', dataIndex: 'lastActiveTimeRange', key: 'lastActiveTimeRange', valueType: 'dateRange',
-            fieldProps: { placeholder: ['开始时间', '结束时间'] }, hideInTable: true, initialValue: ""
+            title: '最后活跃时间', dataIndex: 'lastActiveTimeRange', key: 'lastActiveTimeRange', valueType: 'dateTimeRange',
+            fieldProps: { placeholder: ['开始时间', '结束时间'], minuteStep: 15, secondStep: 10 }, hideInTable: true, initialValue: ""
         },
         {
-            title: '上次登入时间', dataIndex: 'lastLoginTimeRange', key: 'lastLoginTimeRange', valueType: 'dateRange',
-            fieldProps: { placeholder: ['开始时间', '结束时间'] }, hideInTable: true, initialValue: ""
+            title: '上次登入时间', dataIndex: 'lastLoginTimeRange', key: 'lastLoginTimeRange',  valueType: 'dateTimeRange',
+            fieldProps: { placeholder: ['开始时间', '结束时间'], minuteStep: 15, secondStep: 10 }, hideInTable: true, initialValue: ""
         },
 
     ]
+
     if (isSuperAdmin) {
         columns.splice(1, 0, {
-            title: '商户名', dataIndex: 'merchantName', key: 'merchantName', hideInSearch: true, width: ProColumnsOperationConstant.width["2"]
+            title: '商户名', dataIndex: 'merchantName', key: 'merchantName', hideInSearch: true, width: ProColumnsOperationConstant.width["2"], render: (text) => <CopyText text={text} />
         }, {
-            title: '商户名', dataIndex: 'merchantId', key: 'merchantId', hideInTable: true, valueEnum: merchantListEnum, valueType: 'select', initialValue: '',
+            title: '商户名', dataIndex: 'merchantId', key: 'merchantId', hideInTable: true, initialValue: '', valueType: 'select', valueEnum: merchantListEnum, fieldProps: { showSearch: true }
         })
-
     }
 
     const expandedRowRender = (data) => {
@@ -103,15 +116,15 @@ const LoginAccountManageTable = () => {
             <ProTable
                 columns={[
                     { width: `${50 + 32}px`, }, // checkbox 47px + 操作 50px
-                    { title: 'IP位址', dataIndex: 'ip', key: 'ip' },
-                    { title: '登入地区', dataIndex: 'loginLocation', key: 'loginLocation' },
+                    { title: 'IP位址', dataIndex: 'ip', key: 'ip', render: (text) => <CopyText text={text} /> },
+                    { title: '登入地区', dataIndex: 'loginLocation', key: 'loginLocation', render: (text) => <CopyText text={text} /> },
                     { title: '最后活跃时间', dataIndex: 'lastActiveTime', key: 'lastActiveTime' },
                     { title: '上次登入时间', dataIndex: 'lastLoginTime', key: 'lastLoginTime' },
                     {
                         title: '操作',
                         valueType: 'option',
                         key: 'option',
-                        render: (text, record, _, action) => [<a key="editable" onClick={() => postLogout({ tokens: [record.token] })} >踢出</a>],
+                        render: (text, record, _, action) => [<a key="editable" onClick={() => handleLogout('ip', record)} >踢出</a>],
                         width: ProColumnsOperationConstant.width["1"],
                     },
                 ]}
@@ -168,12 +181,13 @@ const LoginAccountManageTable = () => {
                             onClick={() => {
                                 // @ts-ignore
                                 const { merchantId = '', accountNumber, ip, loginLocation, lastActiveTimeRange, lastLoginTimeRange } = form.getFieldValue();
+                                console.log(lastLoginTimeRange)
                                 setSearchList({
                                     merchantId, accountNumber, ip, loginLocation,
-                                    lastActiveEndTime: lastActiveTimeRange[1] ? lastActiveTimeRange[1].format('YYYY-MM-DD 23:59:59') : '',
-                                    lastActiveStartTime: lastActiveTimeRange[0] ? lastActiveTimeRange[0].format('YYYY-MM-DD 00:00:00') : '',
-                                    lastLoginEndTime: lastLoginTimeRange[1] ? lastLoginTimeRange[1].format('YYYY-MM-DD 23:59:59') : '',
-                                    lastLoginStartTime: lastLoginTimeRange[0] ? lastLoginTimeRange[0].format('YYYY-MM-DD 00:00:00') : '',
+                                    lastActiveEndTime: lastActiveTimeRange[1] ? lastActiveTimeRange[1].format('YYYY-MM-DD HH:mm:ss') : '',
+                                    lastActiveStartTime: lastActiveTimeRange[0] ? lastActiveTimeRange[0].format('YYYY-MM-DD HH:mm:ss') : '',
+                                    lastLoginEndTime: lastLoginTimeRange[1] ? lastLoginTimeRange[1].format('YYYY-MM-DD HH:mm:ss') : '',
+                                    lastLoginStartTime: lastLoginTimeRange[0] ? lastLoginTimeRange[0].format('YYYY-MM-DD HH:mm:ss') : '',
                                 });
                                 onSelectChange([]);
                                 form.submit();
