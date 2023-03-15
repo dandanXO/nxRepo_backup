f8ffc5cc338747c97745021186c74e7ca2fb34e3
fronteric <fronteric@1.1>
2023-03-15 14:49:48 +0800

    refactor: Code Split by routes

--- apps/mobile/src/app/router/index.tsx
+++ apps/mobile/src/app/router/index.tsx
@@ -1,6 +1,15 @@
-import React, {Suspense} from "react";
+import React, {ReactComponentElement, Suspense} from "react";
 import {BrowserRouter, Route, Routes, useLocation} from "react-router-dom";
+import loadable from '@loadable/component'
+// import {I18nRepaymentStepsModal} from "../components/pages/LoanDetailsPage/modal/RepaymentStepsModal";
 
+
+// NOTE: JS way1
+// import(/* webpackChunkName: "chunk-test" */ "../test").then((module) => {
+//   module.default.test();
+// })
+
+// NOTE: Component - way1
 // import IndexPage from "../components/pages/IndexPage";
 // import LoanDetailsPage, {STATE_REPAYMENT_STEPS} from "../components/pages/LoanDetailsPage";
 // import ExtendDetailsPage from "../components/pages/ExtendDetailsPage";
@@ -12,30 +21,86 @@ import {BrowserRouter, Route, Routes, useLocation} from "react-router-dom";
 // import ActivityAdListPage, {DemoActivityAdListPage} from "../components/pages/ActivityAdListPage";
 // import NewsSectionPage from "../components/pages/NewsSectionPage";
 
-// import {I18nRepaymentStepsModal} from "../components/pages/LoanDetailsPage/modal/RepaymentStepsModal";
 
-
-// NOTE:
-// https://stackoverflow.com/questions/10302179/hyphen-underscore-or-camelcase-as-word-delimiter-in-uris
-// https://developers.google.com/search/docs/advanced/guidelines/url-structure?hl=en&visit_id=637961283238394064-158551757&rd=1
-export const AppRouter = () => {
-    return (
-        <BrowserRouter>
-          <Pages/>
-        </BrowserRouter>
-    );
-};
-
+// NOTE: Component - way2
 // NOTICE: TS1323: Dynamic imports are only supported when the '--module' flag is set to 'es2020', 'es2022', 'esnext', 'commonjs', 'amd', 'system', 'umd', 'node16', or 'nodenext'.
-const LazyIndexPage = React.lazy(() => import(/* webpackChunkName: "chunk-index" */ "../components/pages/IndexPage"))
-const LazyLoanDetailsPage = React.lazy(() => import(/* webpackChunkName: "chunk-loan-details" */ "../components/pages/LoanDetailsPage"))
-const LazyExtendDetailsPage = React.lazy(() => import(/* webpackChunkName: "chunk-extend-details" */ "../components/pages/ExtendDetailsPage"))
-const LazyBindBankAccountPage = React.lazy(() => import(/* webpackChunkName: "chunk-bind-bank-account" */ "../components/pages/BindBankAccountPage"))
-const LazyUploadPaymentReceiptPage = React.lazy(() => import(/* webpackChunkName: "chunk-upload-payment-receipt" */ "../components/pages/UploadPaymentReceiptPage"))
-const LazyUploadedPaymentReceiptPage = React.lazy(() => import(/* webpackChunkName: "chunk-uploaded-payment-receipt" */ "../components/pages/UploadedPaymentReceiptPage"))
-const LazyProductAdModalListPage = React.lazy(() => import(/* webpackChunkName: "chunk-product-ad-modal-list" */ "../components/pages/ProductAdModalListPage"))
-const LazyActivityAdListPage = React.lazy(() => import(/* webpackChunkName: "chunk-activity-ad-modal-list" */ "../components/pages/ActivityAdListPage"))
-const LazyNewsSectionPage = React.lazy(() => import(/* webpackChunkName: "chunk-news-section" */ "../components/pages/NewsSectionPage"))
+// const LazyIndexPage = React.lazy(() => import(/* webpackChunkName: "chunk-index" */ "../components/pages/IndexPage"))
+// const LazyLoanDetailsPage = React.lazy(() => import(/* webpackChunkName: "chunk-loan-details" */ "../components/pages/LoanDetailsPage"))
+// const LazyExtendDetailsPage = React.lazy(() => import(/* webpackChunkName: "chunk-extend-details" */ "../components/pages/ExtendDetailsPage"))
+// const LazyBindBankAccountPage = React.lazy(() => import(/* webpackChunkName: "chunk-bind-bank-account" */ "../components/pages/BindBankAccountPage"))
+// const LazyUploadPaymentReceiptPage = React.lazy(() => import(/* webpackChunkName: "chunk-upload-payment-receipt" */ "../components/pages/UploadPaymentReceiptPage"))
+// const LazyUploadedPaymentReceiptPage = React.lazy(() => import(/* webpackChunkName: "chunk-uploaded-payment-receipt" */ "../components/pages/UploadedPaymentReceiptPage"))
+// const LazyProductAdModalListPage = React.lazy(() => import(/* webpackChunkName: "chunk-product-ad-modal-list" */ "../components/pages/ProductAdModalListPage"))
+// const LazyActivityAdListPage = React.lazy(() => import(/* webpackChunkName: "chunk-activity-ad-modal-list" */ "../components/pages/ActivityAdListPage"))
+// const LazyNewsSectionPage = React.lazy(() => import(/* webpackChunkName: "chunk-news-section" */ "../components/pages/NewsSectionPage"))
+
+// NOTE: Component - way3 = Error
+// let LazyIndexPage: any;
+// import(/* webpackChunkName: "chunk-index" */ "../components/pages/IndexPage").then((module) => {
+//   LazyIndexPage = module.default;
+// })
+//
+// let LazyLoanDetailsPage: any;
+// import(/* webpackChunkName: "chunk-loan-details" */ "../components/pages/LoanDetailsPage").then((module) => {
+//   LazyLoanDetailsPage = module.default;
+// })
+//
+// let LazyExtendDetailsPage: any;
+// import(/* webpackChunkName: "chunk-extend-details" */ "../components/pages/ExtendDetailsPage").then((module) => {
+//   LazyExtendDetailsPage = module.default;
+// })
+//
+// let LazyBindBankAccountPage: any;
+// import(/* webpackChunkName: "chunk-bind-bank-account" */ "../components/pages/BindBankAccountPage").then((module) => {
+//   LazyBindBankAccountPage = module.default;
+// })
+//
+// let LazyUploadPaymentReceiptPage: any;
+// import(/* webpackChunkName: "chunk-upload-payment-receipt" */ "../components/pages/UploadPaymentReceiptPage").then((module) => {
+//   LazyUploadPaymentReceiptPage = module.default;
+// })
+//
+// let LazyUploadedPaymentReceiptPage: any;
+// import(/* webpackChunkName: "chunk-uploaded-payment-receipt" */ "../components/pages/UploadedPaymentReceiptPage").then((module) => {
+//   LazyUploadedPaymentReceiptPage = module.default;
+// })
+//
+// let LazyProductAdModalListPage: any;
+// import(/* webpackChunkName: "chunk-product-ad-modal-list" */ "../components/pages/ProductAdModalListPage").then((module) => {
+//   LazyProductAdModalListPage = module.default;
+// })
+//
+// let LazyActivityAdListPage: any;
+// import(/* webpackChunkName: "chunk-activity-ad-modal-list" */ "../components/pages/ActivityAdListPage").then((module) => {
+//   LazyActivityAdListPage = module.default;
+// })
+//
+// let LazyNewsSectionPage: any;
+// import(/* webpackChunkName: "chunk-news-section" */ "../components/pages/NewsSectionPage").then((module) => {
+//   LazyNewsSectionPage = module.default;
+// })
+
+// NOTE: Component - way4
+const LazyIndexPage = loadable(() => import(/* webpackChunkName: "chunk-index" */ "../components/pages/IndexPage"))
+const LazyLoanDetailsPage = loadable(() => import(/* webpackChunkName: "chunk-loan-details" */ "../components/pages/LoanDetailsPage"))
+const LazyExtendDetailsPage = loadable(() => import(/* webpackChunkName: "chunk-extend-details" */ "../components/pages/ExtendDetailsPage"))
+const LazyBindBankAccountPage = loadable(() => import(/* webpackChunkName: "chunk-bind-bank-account" */ "../components/pages/BindBankAccountPage"))
+const LazyUploadPaymentReceiptPage = loadable(() => import(/* webpackChunkName: "chunk-upload-payment-receipt" */ "../components/pages/UploadPaymentReceiptPage"))
+const LazyUploadedPaymentReceiptPage = loadable(() => import(/* webpackChunkName: "chunk-uploaded-payment-receipt" */ "../components/pages/UploadedPaymentReceiptPage"))
+const LazyProductAdModalListPage = loadable(() => import(/* webpackChunkName: "chunk-product-ad-modal-list" */ "../components/pages/ProductAdModalListPage"))
+const LazyActivityAdListPage = loadable(() => import(/* webpackChunkName: "chunk-activity-ad-modal-list" */ "../components/pages/ActivityAdListPage"))
+const LazyNewsSectionPage = loadable(() => import(/* webpackChunkName: "chunk-news-section" */ "../components/pages/NewsSectionPage"))
+
+LazyIndexPage.preload();
+LazyLoanDetailsPage.preload();
+LazyExtendDetailsPage.preload();
+LazyBindBankAccountPage.preload();
+LazyUploadPaymentReceiptPage.preload();
+LazyUploadedPaymentReceiptPage.preload();
+LazyProductAdModalListPage.preload();
+LazyActivityAdListPage.preload();
+LazyNewsSectionPage.preload();
+
 
 // NOTICE:
 // Consider adding an error boundary to your tree to customize error handling behavior.
@@ -44,6 +109,18 @@ const LazyNewsSectionPage = React.lazy(() => import(/* webpackChunkName: "chunk-
 // NOTICE:
 // react-dom.development.js:19055 Uncaught Error: A component suspended while responding to synchronous input.
 // This will cause the UI to be replaced with a loading indicator. To fix, updates that suspend should be wrapped with startTransition.
+
+
+// NOTE:
+// https://stackoverflow.com/questions/10302179/hyphen-underscore-or-camelcase-as-word-delimiter-in-uris
+// https://developers.google.com/search/docs/advanced/guidelines/url-structure?hl=en&visit_id=637961283238394064-158551757&rd=1
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Pages/>
+    </BrowserRouter>
+  );
+};
 
 const Pages = () => {
   // const location = useLocation();
