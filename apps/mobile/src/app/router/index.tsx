2a6b555bde3df43507d7cf50d2731436fda29205
fronteric <fronteric@1.1>
2023-03-15 12:52:49 +0800

    refactor: Code Split by routes

--- apps/mobile/src/app/router/index.tsx
+++ apps/mobile/src/app/router/index.tsx
@@ -1,16 +1,19 @@
+import React, {Suspense} from "react";
 import {BrowserRouter, Route, Routes, useLocation} from "react-router-dom";
-import IndexPage from "../components/pages/IndexPage";
-import LoanDetailsPage, {STATE_REPAYMENT_STEPS} from "../components/pages/LoanDetailsPage";
-import ExtendDetailsPage from "../components/pages/ExtendDetailsPage";
-import UploadPaymentReceiptPage from "../components/pages/UploadPaymentReceiptPage";
-import BindBankAccountPage from "../components/pages/BindBankAccountPage";
-import UploadedPaymentReceiptPage from "../components/pages/UploadedPaymentReceiptPage";
-import OldProductAdModalListPage from "../components/pages/OldProductAdModalListPage";
-import ProductAdModalListPage from "../components/pages/ProductAdModalListPage";
-import {ActivityAdListPage, DemoActivityAdListPage} from "../components/pages/ActivityAdListPage";
-import {I18nRepaymentStepsModal} from "../components/pages/LoanDetailsPage/modal/RepaymentStepsModal";
-import React from "react";
-import {NewsSectionPage} from "../components/pages/NewsSectionPage";
+
+// import IndexPage from "../components/pages/IndexPage";
+// import LoanDetailsPage, {STATE_REPAYMENT_STEPS} from "../components/pages/LoanDetailsPage";
+// import ExtendDetailsPage from "../components/pages/ExtendDetailsPage";
+// import BindBankAccountPage from "../components/pages/BindBankAccountPage";
+// import UploadPaymentReceiptPage from "../components/pages/UploadPaymentReceiptPage";
+// import UploadedPaymentReceiptPage from "../components/pages/UploadedPaymentReceiptPage";
+// import OldProductAdModalListPage from "../components/pages/OldProductAdModalListPage";
+// import ProductAdModalListPage from "../components/pages/ProductAdModalListPage";
+// import ActivityAdListPage, {DemoActivityAdListPage} from "../components/pages/ActivityAdListPage";
+// import NewsSectionPage from "../components/pages/NewsSectionPage";
+
+// import {I18nRepaymentStepsModal} from "../components/pages/LoanDetailsPage/modal/RepaymentStepsModal";
+
 
 // NOTE:
 // https://stackoverflow.com/questions/10302179/hyphen-underscore-or-camelcase-as-word-delimiter-in-uris
@@ -23,6 +26,25 @@ export const AppRouter = () => {
     );
 };
 
+// NOTICE: TS1323: Dynamic imports are only supported when the '--module' flag is set to 'es2020', 'es2022', 'esnext', 'commonjs', 'amd', 'system', 'umd', 'node16', or 'nodenext'.
+const LazyIndexPage = React.lazy(() => import(/* webpackChunkName: "chunk-index" */ "../components/pages/IndexPage"))
+const LazyLoanDetailsPage = React.lazy(() => import(/* webpackChunkName: "chunk-loan-details" */ "../components/pages/LoanDetailsPage"))
+const LazyExtendDetailsPage = React.lazy(() => import(/* webpackChunkName: "chunk-extend-details" */ "../components/pages/ExtendDetailsPage"))
+const LazyBindBankAccountPage = React.lazy(() => import(/* webpackChunkName: "chunk-bind-bank-account" */ "../components/pages/BindBankAccountPage"))
+const LazyUploadPaymentReceiptPage = React.lazy(() => import(/* webpackChunkName: "chunk-upload-payment-receipt" */ "../components/pages/UploadPaymentReceiptPage"))
+const LazyUploadedPaymentReceiptPage = React.lazy(() => import(/* webpackChunkName: "chunk-uploaded-payment-receipt" */ "../components/pages/UploadedPaymentReceiptPage"))
+const LazyProductAdModalListPage = React.lazy(() => import(/* webpackChunkName: "chunk-product-ad-modal-list" */ "../components/pages/ProductAdModalListPage"))
+const LazyActivityAdListPage = React.lazy(() => import(/* webpackChunkName: "chunk-activity-ad-modal-list" */ "../components/pages/ActivityAdListPage"))
+const LazyNewsSectionPage = React.lazy(() => import(/* webpackChunkName: "chunk-news-section" */ "../components/pages/NewsSectionPage"))
+
+// NOTICE:
+// Consider adding an error boundary to your tree to customize error handling behavior.
+//   Visit https://reactjs.org/link/error-boundaries to learn more about error boundaries.
+
+// NOTICE:
+// react-dom.development.js:19055 Uncaught Error: A component suspended while responding to synchronous input.
+// This will cause the UI to be replaced with a loading indicator. To fix, updates that suspend should be wrapped with startTransition.
+
 const Pages = () => {
   // const location = useLocation();
   // const showRepaymentStepsModal = location.state && location.state.name === STATE_REPAYMENT_STEPS;
@@ -30,23 +52,28 @@ const Pages = () => {
   // const state = location.state as { name?: Location };
 
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        <Route path="/" element={<IndexPage />} />
+        <Route path="/" element={<LazyIndexPage />} />
         {/* url: /loan-details?token=xxxxxxxx&orderNo=xxxxxxxx */}
-        <Route path="/loan-details" element={<LoanDetailsPage />} />
+        <Route path="/loan-details" element={<LazyLoanDetailsPage />} />
+
         {/* url: /extend-details?token=xxxxxxxx&orderNo=xxxxxxxx */}
-        <Route path="/extend-details" element={<ExtendDetailsPage />} />
-        <Route path="/repayment-modal-advertisement" element={<div />} />
-        <Route path="/bank-bind" element={<BindBankAccountPage />} />
-        <Route path="/upload-payment-receipt" element={<UploadPaymentReceiptPage />}/>
-        <Route path="/uploaded-payment-receipt" element={<UploadedPaymentReceiptPage />}/>
-        <Route path="/product-ad-modal-list-old" element={<OldProductAdModalListPage />}/>
-        <Route path="/product-ad-modal-list" element={<ProductAdModalListPage />}/>
+        <Route path="/extend-details" element={<LazyExtendDetailsPage />} />
+
+        {/*<Route path="/repayment-modal-advertisement" element={<div />} />*/}
+
+        <Route path="/bank-bind" element={<LazyBindBankAccountPage />} />
+        <Route path="/upload-payment-receipt" element={<LazyUploadPaymentReceiptPage />}/>
+        <Route path="/uploaded-payment-receipt" element={<LazyUploadedPaymentReceiptPage />}/>
+        {/*<Route path="/product-ad-modal-list-old" element={<OldProductAdModalListPage />}/>*/}
+
+        <Route path="/product-ad-modal-list" element={<LazyProductAdModalListPage />}/>
+
         {/*NOTICE: ad 字眼容易被 Google 擋*/}
-        <Route path="/activity-list" element={<ActivityAdListPage />}/>
-        <Route path="/activity-list-dev" element={<DemoActivityAdListPage/>}/>
-        <Route path="/news-section" element={<NewsSectionPage/>}/>
+        <Route path="/activity-list" element={<LazyActivityAdListPage />}/>
+        {/*<Route path="/activity-list-dev" element={<DemoActivityAdListPage/>}/>*/}
+        <Route path="/news-section" element={<LazyNewsSectionPage/>}/>
         <Route path="*" element={<div>Not Found</div>} />
       </Routes>
       {/*{state?.name && (*/}
@@ -54,6 +81,6 @@ const Pages = () => {
       {/*    <Route path={`/loan-details`} element={<I18nRepaymentStepsModal/>}/>*/}
       {/*  </Routes>*/}
       {/*)}*/}
-    </>
+    </Suspense>
   )
 }
