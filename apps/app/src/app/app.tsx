3a1e2a7b52adfa8354b8e49db16fde33111ceb42
fronteric <fronteric@1.1>
2023-03-23 14:48:19 +0800

    fix: add global theme

--- apps/app/src/app/app.tsx
+++ apps/app/src/app/app.tsx
@@ -1,12 +1,20 @@
 import {AppRouter} from "./router";
 import {Provider} from "react-redux";
 import { appStore } from "./store";
+import {AppThemeProvider} from "@frontend/mobile/shared/ui";
+import {environment} from "../environments/environment";
+import {getThemeConfig} from "./modules/theme/getThemeConfig";
+
+// NOTICE: REFACTOR ME , 目前Modal.alert 只能從全局取得
+window.theme = getThemeConfig(environment.country);
 
 export function App() {
   return (
     <div>
       <Provider store={appStore}>
-        <AppRouter/>
+        <AppThemeProvider theme={window.theme}>
+          <AppRouter/>
+        </AppThemeProvider>
       </Provider>
     </div>
   );
