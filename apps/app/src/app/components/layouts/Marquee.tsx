b5a7312fcabf9064a4ee9d3c0ec29b8ff868f394
fronteric <fronteric@1.1>
2023-03-26 17:23:21 +0800

    build: 可以使用 data-xxx 了 fix Module build failed (from ../../node_modules/.pnpm/@nrwl+web@14.6.5_czrtp36ra5ao33uyr4koit2ot4/node_modules/@nrwl/web/src/utils/web-babel-loader.js):      SyntaxError: /Users/eric/Documents/xujie/frontend/apps/app/src/main.tsx: Duplicate __self prop found. You are most likely using the deprecated transform-react-jsx-self Babel plugin. Both __source and __self are automatically set when using the automatic runtime. Please remove transform-react-jsx-source and transform-react-jsx-self from your Babel config.

--- apps/app/src/app/components/layouts/Marquee.tsx
+++ apps/app/src/app/components/layouts/Marquee.tsx
@@ -4,6 +4,6 @@ type Props = IndexPageProps;
 
 export const Marquee = (props: Props) => {
   return (
-    <div className={"marquee h-7 p-1 bg-orange-300 px-3"}>{props.state.indexAPI?.marquee}</div>
+    <div data-td="marquee" className={"marquee h-7 p-1 bg-orange-300 px-3"}>{props.state.indexAPI?.marquee}</div>
   )
 }
