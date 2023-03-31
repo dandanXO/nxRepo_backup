0006925b5d0d69dba3ca22557f3554ad41fc6a8c
fronteric <fronteric@1.1>
2023-03-31 16:41:03 +0800

    feat: // @ts-ignore

--- apps/mobile/src/app/components/pages/ActivityAdListPage/components/AdClick.ts
+++ apps/mobile/src/app/components/pages/ActivityAdListPage/components/AdClick.ts
@@ -1,15 +1,21 @@
 const onClickToDoLoan = () => {
   // console.log("window.SyncTask.doQuickLoanApply()");
   // window.SyncTask.doQuickLoanApply();
+  // @ts-ignore
   window["SyncTask"] &&
+  // @ts-ignore
   window["SyncTask"]["doQuickLoanApply"] &&
+  // @ts-ignore
   window["SyncTask"]["doQuickLoanApply"]();
 }
 const onClickToPopup = (url: string) => {
   // console.log("onClickToPopup.url:", url);
   // window.SyncTask.thematicActivitiesPopup("https://www.google.com/");
+  // @ts-ignore
   window["SyncTask"] &&
+  // @ts-ignore
   window["SyncTask"]["thematicActivitiesPopup"] &&
+  // @ts-ignore
   window["SyncTask"]["thematicActivitiesPopup"](url);
 }
 export  const onClickToAction = (event: {action: "POP_URL" | "APPLY_LOAN" | undefined; actionUrl: string | undefined;}) => {
