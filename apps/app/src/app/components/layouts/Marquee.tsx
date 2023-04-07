4effd885583cf65fb0b709ae1364c5e114fab9ad
Emma <imgenu@gmail.com>
2023-04-07 11:09:20 +0800

    feat(app):Marquee add animation

--- apps/app/src/app/components/layouts/Marquee.tsx
+++ apps/app/src/app/components/layouts/Marquee.tsx
@@ -3,7 +3,11 @@ import {IndexPageProps} from "../../usecaseFlow/reduxStore";
 type Props = IndexPageProps;
 
 export const Marquee = (props: Props) => {
-  return (
-    <div data-testing-id="marquee" className={"marquee h-7 p-1 bg-orange-300 px-3"}>{props.state.sharedIndex?.marquee}</div>
-  )
+    return (
+        <div data-testing-id="marquee" className={"marquee h-7 p-1 bg-orange-300 px-3 "}>
+            <div className={"animate-marquee whitespace-nowrap"}>
+                {props.state.sharedIndex?.marquee}
+            </div>
+        </div>
+    )
 }
