792936c6dfb0f15272e1501aeee1d86f90366998
fronteric <fronteric@1.1>
2023-04-18 14:46:43 +0800

    fix: close is in tailwind

--- apps/app/src/app/presentation/components/layouts/CloseButton.tsx
+++ apps/app/src/app/presentation/components/layouts/CloseButton.tsx
@@ -2,7 +2,7 @@ import {AiOutlineClose} from "react-icons/all";
 
 export const CloseButton = () => {
   return (
-    <div className={"close absolute right-3 top-3"}>
+    <div className={"absolute right-3 top-3"}>
       <AiOutlineClose size={25}/>
     </div>
   )
