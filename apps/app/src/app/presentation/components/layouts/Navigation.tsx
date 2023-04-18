0b1e4325a0407d0b643652afb0115a55bfe58447
fronteric <fronteric@1.1>
2023-04-18 16:11:12 +0800

    feat: app ui

--- apps/app/src/app/presentation/components/layouts/Navigation.tsx
+++ apps/app/src/app/presentation/components/layouts/Navigation.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 export const Navigation = (props: Props) => {
   return (
-    <div className={"navigation h-14 bg-white flex flex-row justify-between items-center sticky top-0 mx-3"}>
+    <div className={"navigation h-14 bg-white flex flex-row justify-between items-center sticky top-0 px-3"}>
       {props.back && (
         <div onClick={() => {
           props.back && props.back();
