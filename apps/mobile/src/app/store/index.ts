d47bceec53f067d95f8c86121881a7be614d686a
fronteric <fronteric@1.1>
2023-04-20 19:20:26 +0800

    reactor

--- apps/mobile/src/app/store/index.ts
+++ apps/mobile/src/app/store/index.ts
@@ -1,18 +1,8 @@
-import {configureStore, createAsyncThunk} from "@reduxjs/toolkit";
-import { API } from "../api";
-import {PostLoanQuotaRefreshResponse} from "../api/PostLoanQuotaRefreshResponse";
-import queryString from "query-string";
-import {store} from "next/dist/build/output/store";
-
-// import { setupListeners } from '@reduxjs/toolkit/query'
-
+import {configureStore} from "@reduxjs/toolkit";
+import {API} from "../api";
 import createSagaMiddleware from 'redux-saga'
-import {
-  personalRecommendActionsReducer
-} from "../components/pages/ProductAdModalListPage/redux";
+import {personalRecommendActionsReducer} from "../components/pages/ProductAdModalListPage/redux";
 import AppSaga from "../components/pages/ProductAdModalListPage/saga";
-// NOTICE: create the saga middleware
-const sagaMiddleware = createSagaMiddleware()
 
 export const appStore = configureStore({
     reducer: {
@@ -24,13 +14,16 @@ export const appStore = configureStore({
         getDefaultMiddleware().concat(API.middleware).concat(sagaMiddleware),
 });
 
+// NOTICE: create the saga middleware
+const sagaMiddleware = createSagaMiddleware()
+
 // NOTICE: then run the saga
 sagaMiddleware.run(AppSaga)
 
 export type RootState = ReturnType<typeof appStore.getState>;
+export type AppDispatch = typeof appStore.dispatch;
 
 // NOTE: for testing
 // enable listener behavior for the store
 // setupListeners(appStore.dispatch)
 
-export type AppDispatch = typeof store.dispatch;
