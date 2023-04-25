14986185e027c8f02fa46d139b23826311b8d67a
fronteric <fronteric@1.1>
2023-04-25 13:20:46 +0800

    refactor

--- apps/app/src/app/presentation/router/index.tsx
+++ apps/app/src/app/presentation/router/index.tsx
@@ -1,4 +1,4 @@
-import React, {Suspense, useEffect} from "react";
+import React from "react";
 import {Route, Routes, useLocation,} from "react-router";
 
 // NOTE: Dynamic imports are only supported when the '--module' flag is
@@ -11,6 +11,14 @@ import {ErrorPage} from "../pages/__test__/ErrorPage";
 
 // const IndexPage = loadable(() => import("../pages/IndexPage"));
 import IndexPage from "../pages/IndexPage";
+import RepamentCouponModal from "../modals/RepamentCouponModal";
+import IBANFinderModal from "../modals/IBANFinderModal";
+
+import {useSelector} from "react-redux"
+import {TabBar} from "../components/layouts/TabBar";
+import {RootState} from "../../usecaseFlow/reduxStore";
+import {PagePathEnum} from "../pages/PagePathEnum";
+
 const AuthPage = loadable(() => import(/* webpackChunkName: "AuthPage" */ "../pages/AuthPage"));
 const BankCardListPage = loadable(() => import(/* webpackChunkName: "BankCardListPage" */ "../pages/BankCardListPage"));
 const BindBankCardPage = loadable(() => import(/* webpackChunkName: "BindBankCardPage" */ "../pages/BindBankCardPage"));
@@ -38,19 +46,10 @@ const RepaymentModal = loadable(() => import(/* webpackChunkName: "RepaymentModa
 const AmountRepaidModal = loadable(() => import(/* webpackChunkName: "AmountRepaidModal" */ "../modals/AmountRepaidModal/AmountRepaidModal"));
 const ExtendConfirmModal = loadable(() => import(/* webpackChunkName: "ExtendConfirmModal" */ "../modals/ExtendConfirmModal"));
 const ExtendModal = loadable(() => import(/* webpackChunkName: "ExtendModal" */ "../modals/ExtendModal/ExtendModal"));
-// const CustomerServiceModal = loadable(() => import("../modals/CustomerServiceModal"));
 const LogoutModal = loadable(() => import(/* webpackChunkName: "LogoutModal" */ "../modals/LogoutModal"));
 const APIBoundaryModal = loadable(() => import(/* webpackChunkName: "APIBoundaryModal" */ "../modals/APIBoundaryModal"));
 const PrivacyPolicyModal = loadable(() => import(/* webpackChunkName: "PrivacyPolicyModal" */ "../modals/PrivacyPolicyModal"));
 
-
-import RepamentCouponModal from "../modals/RepamentCouponModal";
-import IBANFinderModal from "../modals/IBANFinderModal";
-
-import {useDispatch, useSelector} from "react-redux"
-import {TabBar} from "../components/layouts/TabBar";
-import {RootState} from "../../usecaseFlow/reduxStore";
-import {PagePathEnum} from "../pages/PagePathEnum";
 
 export const AppRouter = () => {
   const location = useLocation();
@@ -126,270 +125,3 @@ export const AppRouter = () => {
   );
 };
 
-
-export const OuterRouter = () => {
-  const location = useLocation();
-  const apiBoundary = useSelector((state: RootState) => state.APIBoundaryModule);
-  console.log("apiBoundary", apiBoundary)
-  const payableRecords = useSelector((state: RootState) => state.indexPage.indexAPI?.payableRecords);
-
-  return (
-    <div>
-      {
-        // [
-        //   "/",
-        //   "/loan-record",
-        //   "/personal-info"
-        // ].indexOf(location.pathname) > -1 && (
-        //   <TabBar hasOrder={payableRecords ? payableRecords?.length > 0 : false}/>
-        // )
-      }
-      <TabBar hasOrder={payableRecords ? payableRecords?.length > 0 : false}/>
-
-      {apiBoundary.show && (
-        <APIBoundaryModal title={apiBoundary.title} message={apiBoundary.message}/>
-      )}
-    </div>
-  )
-}
-
-
-
-// export const appRouterV2 = createBrowserRouter([
-//   // {
-//   //   path: "*",
-//   //   element: <OuterRouter />,
-//   //   // errorElement: <ErrorPage />,
-//   // },
-//   {
-//     path: "/category",
-//     element: <CategoryPage />,
-//     // errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: "/login",
-//     element: <LoginPage />,
-//     // errorElement: <ErrorPage />,
-//     children: [
-//       {
-//         path: "log-out-modal",
-//         element: <LogoutModal />,
-//       },
-//     ],
-//   },
-//   {
-//     path: "/",
-//     element: <IndexPage />,
-//     // errorElement: <ErrorPage />,
-//     children: [
-//       {
-//         path: "",
-//         element: <OuterRouter />,
-//       },
-//     ],
-//   },
-//   {
-//     path: "/loan-record",
-//     element: <LoanRecordPage />,
-//     // errorElement: <ErrorPage />,
-//     children: [
-//       {
-//         path: "",
-//         element: <OuterRouter />,
-//       },
-//     ]
-//   },
-//   {
-//     path: "repayment-detail",
-//     element: <LoanRecordDetailPage />,
-//     // errorElement: <ErrorPage />,
-//     children: [
-//       {
-//         path: "extend-confirm-modal",
-//         element: <ExtendConfirmModal />,
-//       },
-//       {
-//         path: "extend-modal",
-//         element: <ExtendModal />,
-//       },
-//       {
-//         path: "repayment-modal",
-//         element: <RepaymentModal />,
-//       },
-//       {
-//         path: "amount-repaid-record-modal",
-//         element: <AmountRepaidModal />,
-//       },
-//     ],
-//   },
-//   {
-//     path: "/personal-info",
-//     element: <PersonalInfoPage />,
-//     // errorElement: <ErrorPage />,
-//     children: [
-//       {
-//         path: "",
-//         element: <OuterRouter />,
-//       },
-//       {
-//         path: "log-out-modal",
-//         element: <LogoutModal />,
-//       },
-//     ],
-//   },
-//   {
-//     path: "/privacy-policy-modal",
-//     element: <PrivacyPolicyModal />,
-//     // errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: "/application-progress",
-//     element: <ApplicationProgressPage />,
-//     // errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: "/auth",
-//     element: <AuthPage />,
-//     // errorElement: <ErrorPage />,
-//   },
-//
-//   {
-//     path: "/bankcard-list",
-//     element: <BankCardListPage />,
-//     // errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: "/bind-bankcard",
-//     element: <BindBankCardPage />,
-//     // errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: "/customer-service",
-//     element: <CustomerServicePage />,
-//     // errorElement: <ErrorPage />,
-//     children: [
-//       {
-//         path: "customer-service-modal",
-//         element: <CustomerServiceModal />,
-//       },
-//     ],
-//   },
-//   {
-//     path: "/disclosure-statement",
-//     element: <DisclosureStatementPage />,
-//     // errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: "/extend-details",
-//     element: <ExtendDetailsPage />,
-//     // errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: "/finished-repayment",
-//     element: <FinishedRepaymentPage />,
-//     // errorElement: <ErrorPage />,
-//   },
-//
-//
-//   {
-//     path: "/my-coupon-list",
-//     element: <MyCouponListPage />,
-//     // errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: "/mu-coupon",
-//     element: <MyCouponPage />,
-//     // errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: "/partner",
-//     element: <PartnerPage />,
-//     // errorElement: <ErrorPage />,
-//   },
-//
-//   {
-//     path: "/privacy-policy",
-//     element: <PrivacyPolicyPage />,
-//     // errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: "/quota-model",
-//     element: <QuotaModelPage />,
-//     // errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: "/uploaded-payment-receipt",
-//     element: <UploadedPaymentReceiptPage />,
-//     // errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: "/upload-payment-receipt",
-//     element: <UploadPaymentReceiptPage />,
-//     // errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: "/log-out-modal",
-//     element: <LogoutModal />,
-//     // errorElement: <ErrorPage />,
-//   },
-//   {
-//     path: "*",
-//     element: <div>Not Found</div>
-//   }
-// ]);
-
-// export const appRouterV3 = createBrowserRouter(
-//   createRoutesFromElements(
-//     <>
-//       {/*<Routes>*/}
-//         <Route path="/" element={
-//           <Page className={"flex flex-col"}>
-//             <IndexPage />
-//             {/*<OuterRouter/>*/}
-//           </Page>
-//         } children={<OuterRouter />}/>
-//         <Route path="/category" element={<CategoryPage />} />
-//
-//         <Route path="/login" element={<LoginPage />} >
-//           <Route path="log-out-modal" element={<LogoutModal />} />
-//         </Route>
-//         <Route path="/privacy-policy-modal" element={<PrivacyPolicyModal />} />
-//         <Route path="/application-progress" element={<ApplicationProgressPage />} />
-//         <Route path="/auth" element={<AuthPage />} />
-//         <Route path="/bankcard-list" element={<BankCardListPage />} />
-//         <Route path="/bind-bankcard" element={<BindBankCardPage />} />
-//         <Route path="/customer-service" element={<CustomerServicePage />} >
-//           <Route path="customer-service-modal" element={<CustomerServiceModal />} />
-//         </Route>
-//         <Route path="/disclosure-statement" element={<DisclosureStatementPage />} />
-//         <Route path="/extend-details" element={<ExtendDetailsPage />} />
-//         <Route path="/finished-repayment" element={<FinishedRepaymentPage />} />
-//
-//         <Route path="/repayment-detail" element={<LoanRecordDetailPage />}>
-//           <Route path="extend-confirm-modal" element={<ExtendConfirmModal />} />
-//           <Route path="extend-modal" element={<ExtendModal />} />
-//           <Route path="repayment-modal" element={<RepaymentModal />} />
-//           <Route path="amount-repaid-record-modal" element={<AmountRepaidModal />} />
-//         </Route>
-//
-//         <Route path="/loan-record" element={<LoanRecordPage />} />
-//         <Route path="/my-coupon-list" element={<MyCouponListPage />} />
-//         <Route path="/mu-coupon" element={<MyCouponPage />} />
-//         <Route path="/partner" element={<PartnerPage />} />
-//
-//         <Route path="/personal-info" element={<PersonalInfoPage />} >
-//           <Route path="log-out-modal" element={<LogoutModal />} />
-//         </Route>
-//
-//         <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-//         <Route path="/quota-model" element={<QuotaModelPage />} />
-//         <Route path="/uploaded-payment-receipt" element={<UploadedPaymentReceiptPage />} />
-//         <Route path="/upload-payment-receipt" element={<UploadPaymentReceiptPage />} />
-//         <Route path="/log-out-modal" element={<LogoutModal />} />
-//
-//         <Route path="*" element={<div>Not Found</div>} />
-//       {/*</Routes>*/}
-//
-//     </>
-//   )
-// );
