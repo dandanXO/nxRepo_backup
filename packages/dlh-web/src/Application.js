80fccf9dabd5c08147dad02fa3046d98877f4db8
fronteric <fronteric@1.1>
2023-04-26 19:47:43 +0800

    build(dlh-web): sentry

--- packages/dlh-web/src/Application.js
+++ packages/dlh-web/src/Application.js
@@ -32,41 +32,35 @@ export const SentryModule = {
     }
   },
   init: function () {
-    if(Application.isLocalhost()) {
-      const envName = Application.getEnvironmentName();
-      // console.log("[api-dlh-web][sentry] environment", envName);
-      // console.log("[api-dlh-web][sentry] isLocalhost", Application.isLocalhost());
+    const envName = Application.getEnvironmentName();
+    // console.log("[api-dlh-web][sentry] environment", envName);
+    // console.log("[api-dlh-web][sentry] isLocalhost", Application.isLocalhost());
 
-      const sentryConfig = {
-        dsn: "https://c10ab5774259474a9832879e2c3bfeae@web.sijneokd.com/2",
-        integrations: [new BrowserTracing(), new Sentry.Replay(this.getReplayConfig())],
-        environment: envName,
-        // Set tracesSampleRate to 1.0 to capture 100%
-        // of transactions for performance monitoring.
-        // We recommend adjusting this value in production
-        tracesSampleRate: 1.0,
-        // Session Replay
-        replaysSessionSampleRate: 1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-        replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-      }
-      if(!Application.isLocalhost()) {
-        sentryConfig.release = appInfo.COMMITHASH;
-      }
-
-      if(this.enable) {
-        Sentry.init(sentryConfig);
-        console.log("[api-dlh-web] sentry.init");
-        // this.startToMonitorClipboard();
-        this.startToMonitorContextmenu();
-      }
-      // NOTICE: Tag: can search these
-      // Tag values have a maximum length of 200 characters and they cannot contain the newline (\n) character.
-      // NOTICE: Context: cannot search these
-
-
-
+    const sentryConfig = {
+      dsn: "https://c10ab5774259474a9832879e2c3bfeae@web.sijneokd.com/2",
+      integrations: [new BrowserTracing(), new Sentry.Replay(this.getReplayConfig())],
+      environment: envName,
+      // Set tracesSampleRate to 1.0 to capture 100%
+      // of transactions for performance monitoring.
+      // We recommend adjusting this value in production
+      tracesSampleRate: 1.0,
+      // Session Replay
+      replaysSessionSampleRate: 1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+      replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    }
+    if(!Application.isLocalhost()) {
+      sentryConfig.release = appInfo.COMMITHASH;
     }
 
+    if(this.enable) {
+      Sentry.init(sentryConfig);
+      console.log("[api-dlh-web] sentry.init");
+      // this.startToMonitorClipboard();
+      this.startToMonitorContextmenu();
+    }
+    // NOTICE: Tag: can search these
+    // Tag values have a maximum length of 200 characters and they cannot contain the newline (\n) character.
+    // NOTICE: Context: cannot search these
   },
   userLogin: function () {
     if(!Cookies.get("loginInfo") || !Cookies.get("adminUser")) {
