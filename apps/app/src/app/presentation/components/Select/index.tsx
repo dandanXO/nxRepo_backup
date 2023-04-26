ebca29f00361aca60d2a03c7a99c23ce4eacdaf1
Emma <imgenu@gmail.com>
2023-04-26 18:24:53 +0800

    feat(app): add CustomSelect component

---
+++ apps/app/src/app/presentation/components/Select/index.tsx
@@ -0,0 +1,38 @@
+import Select, { StylesConfig } from 'react-select';
+
+const selectStyles: StylesConfig = {
+    control: (styles) => ({
+        ...styles,
+        backgroundColor: 'white',
+        padding: '0px 10px',
+        border: 0,
+        borderRadius: 0,
+        borderBottom: "1px solid #aaaaaa",
+        span: {
+            width: 0
+        }
+    }),
+    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+        return {
+            ...styles,
+            border: 0
+        }
+    }
+}
+
+
+
+const CustomSelect = (props: any) => {
+    const selectStylesTypes = {
+        'standard': selectStyles,
+        'outlined': '',
+        '': ''
+    }[props?.type as string]
+
+    return (<Select
+        styles={props?.type ? selectStylesTypes :''}
+        {...props}
+    />)
+}
+
+export default CustomSelect;
