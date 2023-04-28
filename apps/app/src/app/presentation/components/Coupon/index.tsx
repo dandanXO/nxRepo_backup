4f23bc3fe6c854263cc0f354059879601d228f18
Emma <imgenu@gmail.com>
2023-04-28 15:59:07 +0800

    feat(app):style_RepamentCouponModal

--- apps/app/src/app/presentation/components/Coupon/index.tsx
+++ apps/app/src/app/presentation/components/Coupon/index.tsx
@@ -22,36 +22,37 @@ const Coupon = (props: ICouponProps) => {
     const layoutTypeStyle: any = {
         1: {
             normal: {
-                font: ``,
-                darkContent: ``,
-                lightContent: ``,
-                buttonBG: ``,
+                font: `text-primary-main`,
+                darkContent: 'border-primary-main bg-primary-main',
+                lightContent: 'border-primary-main bg-primary-assistant',
+                buttonBG: 'bg-primary-variant'
             },
             unUsable: {
-                font: ``,
-                darkContent: ``,
-                lightContent: ``,
-                buttonBG: ``,
+                font: `text-primary-main`,
+                darkContent: 'border-primary-main bg-primary-main',
+                lightContent: 'border-primary-main bg-primary-assistant',
+                buttonBG: 'bg-primary-variant'
             },
             disabled: {
-                font: ``,
-                darkContent: ``,
-                lightContent: ``,
-                buttonBG: ``,
+                font: `text-disabled-main`,
+                darkContent: 'border-disabled-main bg-[#DFDFDF]',
+                lightContent: 'border-disabled-main bg-[#F9F9F9]',
+                buttonBG: 'bg-disabled-main'
             },
         },
         2: {
             normal: {
-                font: `text-primary-main`,
-                darkContent: 'border-primary-main bg-primary-main',
-                lightContent: 'border-primary-main bg-primary-assistant',
-                buttonBG: 'bg-primary-variant'
+                font: 'text-primary-main',
+                darkContent: 'border-primary-main bg-[#B2E4C6]',
+                lightContent: 'border-primary-main bg-[#F4FEF8]',
+                buttonBG: 'bg-primary-main'
+
             },
             unUsable: {
-                font: `text-primary-main`,
-                darkContent: 'border-primary-main bg-primary-main',
-                lightContent: 'border-primary-main bg-primary-assistant',
-                buttonBG: 'bg-primary-variant'
+                font: 'text-primary-main',
+                darkContent: 'border-primary-main bg-[#B2E4C6]',
+                lightContent: 'border-primary-main bg-[#F4FEF8]',
+                buttonBG: 'bg-primary-main'
             },
             disabled: {
                 font: `text-disabled-main`,
@@ -71,24 +72,26 @@ const Coupon = (props: ICouponProps) => {
             'rounded-l-lg': layoutType === 2,
         }, [typeStyle.lightContent]
         )}>
-            <div className={cx(`font-bold texxt-xs`, [typeStyle.font])}>{couponType}</div>
-            <div className={`font-bold text-sm leading-none`}>{couponName}</div>
-            <div className={`text-sm leading-none mt-2 mb-3`}>{couponContent}</div>
-            <div className={cx(`text-xs leading-none`, {
+            <div className={cx(`font-bold`,'text-xs', [typeStyle.font])}>{couponType}</div>
+            <div className={`font-bold text-sm text-black`}>{couponName}</div>
+            <div className={`text-xs mb-1.5`}>{couponContent}</div>
+            <div className={cx('text-xs flex', {
                 'text-red-500': isOverdueEqual3Days(expireTime)
             })}>Expired time {moment(expireTime).format("DD-MM-YYYY")}</div>
         </div>
-        <div className={cx(`flex flex-col border border-solid justify-center p-2 `, {
+        <div className={cx(`flex flex-col border border-solid justify-center p-2 basis-16 grow`, {
             'rounded-r-lg': layoutType === 2
         }, [typeStyle.darkContent])}
         >
-            <div className={cx(`font-bold`,{
-                "text-primary-variant": status !== 'disabled',
+            <div className={cx(`font-bold mb-1.5 text-base `, {
+                [typeStyle.font]: status !== 'disabled',
                 "text-disabled-main": status === 'disabled',
-            })}><Money money={discountAmount} /></div>
+            })}>
+                <Money money={discountAmount} isNagetive={true} moneyStyle={'text-lg'} currencyStyle={`text-xs`} />
+            </div>
             <button onClick={props.onClick} disabled={status !== 'normal'} //只有normal才能點擊
                 className={cx(`text-xs whitespace-nowrap px-2 py-1`, {
-                    'rounded-md': layoutType === 2,
+                    'rounded': layoutType === 2,
                     'text-white': layoutType === 2,
                 }, [typeStyle.buttonBG])}>USE NOW</button>
         </div>
--- apps/app/src/app/presentation/components/Money.tsx/index.tsx
+++ apps/app/src/app/presentation/components/Money.tsx/index.tsx
@@ -4,10 +4,13 @@ import { IndiaCountry } from "libs/shared/domain/src/country/IndiaCountry";
 import { PakistanCountry } from "libs/shared/domain/src/country/PakistanCountry";
 import { environment } from "apps/app/src/environments/environment";
 
-interface IMoney{
+interface IMoney {
     money: number | string;
+    currencyStyle?: string;
+    moneyStyle?: string;
+    isNagetive?: boolean;
 }
-const Money = (props:IMoney) => {
+const Money = (props: IMoney) => {
 
     const price = formatPrice(Number(props.money || 0));
     const currency = environment.currency;
@@ -15,7 +18,13 @@ const Money = (props:IMoney) => {
     return (
         renderByCountry({
             [IndiaCountry.country]: (<div className="ml-1">{`${currency} ${price}`}</div>),
-            [PakistanCountry.country]: (<div className="ml-1">{`${price} ${currency}`}</div>),
+            [PakistanCountry.country]: (
+                <div className="flex justify-center items-baseline">
+                    {props.isNagetive && <div className="mr-1">-</div>}
+                    <div className={`${props.moneyStyle} mr-1 `|| ''}>{price}</div>
+                    <div className={props.currencyStyle || ''}>{currency}</div>
+                </div>
+            ),
         }, (<div className="ml-1">{`${currency} ${price}`}</div>))
     )
 }
