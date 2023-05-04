2c8bdbfca33d46d4d6e766a4ba9b903e0ac83f6c
fronteric <fronteric@1.1>
2023-05-04 15:21:48 +0800

    feat(app):  debug

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -1,5 +1,5 @@
 import {environment} from "./environments/environment";
-import {androidAPPInfo} from "./app/modules/window/appInfo";
+// import {androidAPPInfo} from "./app/modules/window/appInfo";
 
 import {alertModal} from "./app/api/base/alertModal";
 import {StrictMode} from 'react';
@@ -14,39 +14,89 @@ import "./style.css";
 import App from './app/app';
 import {applyTheme} from "./app/modules/theme/utils";
 
-// NOTE: apply tailwind theme
-applyTheme(androidAPPInfo.environment, "v" + androidAPPInfo.uiVersion);
+
 
 // NOTICE: try for ios
 window["AppInfoTask"]["getAppInfoFromIOS"] = function (appInfo: string) {
   alertModal("received from ios.appInfo:" + appInfo);
+  let androidAPPInfo = JSON.parse(appInfo);
+  // NOTE: apply tailwind theme
+  applyTheme(androidAPPInfo.environment, "v" + androidAPPInfo.uiVersion);
+
 }
 
 window["AppInfoTask"]["getAppInfo"] = function (appInfo: string) {
   alertModal("received from android.appInfo:" + appInfo);
-}
+  let androidAPPInfo = JSON.parse(appInfo);
+
+// NOTICE: app team dev packageId 都是 com.ind.kyc.application
+// NOTE: only H5 environment
+  if(!androidAPPInfo) {
+    if(environment.country === "in") {
+      androidAPPInfo = {
+        domain: "india-api-dev.com",
+        environment: "india",
+        packageId: "com.ind.kyc.application",
+        appName: "dev_in",
+        uiVersion: "55",
+        token: "",
+      }
+    } else if(environment.country === "pk") {
+      androidAPPInfo = {
+        domain: "india-api-dev.com",
+        environment: "pakistan",
+        packageId: "com.pak.app.yesloan.android",
+        appName: "dev_in",
+        uiVersion: "15",
+        token: "",
+      }
+    } else {
+      new Error("appInfo is undefined");
+    }
+  } else {
+    // NOTICE: 印度 v55, 巴基斯坦 v56 的 uiVersion 是寫死成 1 的
+    // NOTICE: 巴基斯坦 v56, uiVersion 則是變動的
+    if(androidAPPInfo.uiVersion === "1") {
+      if(androidAPPInfo.packageId === "com.ind.kyc.application") {
+        androidAPPInfo.environment = "india";
+        androidAPPInfo.uiVersion = "55"
+
+      } else if(androidAPPInfo.packageId === "com.pak.app.yesloan.android") {
+        androidAPPInfo.environment = "pakistan";
+        androidAPPInfo.uiVersion = "15"
+      }
+    }
+  }
+  console.log("[app] androidAPPInfo", JSON.parse(JSON.stringify(androidAPPInfo)));
+
+  // NOTE: apply tailwind theme
+  applyTheme(androidAPPInfo.environment, "v" + androidAPPInfo.uiVersion);
+
 
 
 
 // NOTE: apply lib style-component theme
-import(`./environments/theme/${androidAPPInfo.environment}/v${androidAPPInfo.uiVersion}/theme`).then((content) => {
-  const themeConfig = content.themeConfig;
+  import(`./environments/theme/${androidAPPInfo.environment}/v${androidAPPInfo.uiVersion}/theme`).then((content) => {
+    const themeConfig = content.themeConfig;
 
-  window.theme = themeConfig;
+    window.theme = themeConfig;
 
-  console.log("[app] environment", environment);
-  console.log("[app] window.theme", window.theme);
-  console.log("isInAndroid", isInAndroid());
+    console.log("[app] environment", environment);
+    console.log("[app] window.theme", window.theme);
+    console.log("isInAndroid", isInAndroid());
 
-  const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-  );
+    const root = ReactDOM.createRoot(
+      document.getElementById('root') as HTMLElement
+    );
 
-  root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-})
+    root.render(
+      <StrictMode>
+        <App />
+      </StrictMode>
+    );
+  })
+
+}
+
 
 
