94c6c75b15af666733dfea40daacbe2748539014
Emma <imgenu@gmail.com>
2023-05-05 16:05:23 +0800

    feat(app):style OrderStatusPage

--- apps/app/src/app/presentation/pages/OrderStatusPage/OrderStatusItem.tsx
+++ apps/app/src/app/presentation/pages/OrderStatusPage/OrderStatusItem.tsx
@@ -11,20 +11,23 @@ export const OrderStatusItem = (props: Props) => {
             'bg-primary-assistant': props.isHightLight,
         })}>
             <div className={cx("item px-4 py-2 border-[1.5px] rounded-md flex flex-col text-sm ", {
-                'border-gray-500': !props.isHightLight,
+                'border-ctext-tertiary': !props.isHightLight,
                 'border-primary-main': props.isHightLight,
             })}>
                 <div className={cx("title font-bold ", {
-                    'text-gray-400': !props.isHightLight,
+                    'text-ctext-tertiary': !props.isHightLight,
                     'text-primary-main': props.isHightLight,
                 })}>{props.title}</div>
                 <div className={cx("content font-light leading-4 my-3 text-sm", {
-                    'text-gray-400': !props.isHightLight,
-                    'text-black': props.isHightLight,
+                    'text-ctext-tertiary': !props.isHightLight,
+                    'text-ctext-primary': props.isHightLight,
                 })}>
                     {props.content}
                 </div>
-                <div className={"date text-gray-400 text-xs font-extralight"}>{props.date}</div>
+                <div className={cx("date  text-xs font-extralight",{
+                     'text-ctext-tertiary': !props.isHightLight,
+                     'text-ctext-secondary': props.isHightLight,
+                })}>{props.date}</div>
             </div>
         </div>
     )
