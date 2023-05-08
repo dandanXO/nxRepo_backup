7c2c1ad7f2f5f2ea39f0047309fb740ef5ca09b7
Emma <imgenu@gmail.com>
2023-05-08 12:39:55 +0800

    feat(app):FRONTEND-173_[APP][色系] 印度 v57

--- apps/app/src/environments/theme/india/v57/tailwind.theme.ts
+++ apps/app/src/environments/theme/india/v57/tailwind.theme.ts
@@ -2,46 +2,46 @@ import { ICustomTailwindTheme } from '../../../../app/modules/theme/ICustomTailw
 
 const theme: ICustomTailwindTheme = {
   // NOTE: color
-  primary_main: '#57B8FF',
-  primary_variant: '#3F90DB',
-  primary_assistant: '#DDF8FF',
+  primary_main: '#ECA72C',
+  primary_variant: '#A96916',
+  primary_assistant: '#FEF5D4',
 
-  secondary_main: '#F59729',
-  secondary_variant: '#C47921',
-  secondary_assistant: '#FEF5EA',
+  secondary_main: '#FFE038',
+  secondary_variant: '#DBBC28',
+  secondary_assistant: '#FFFBD7',
 
-  tertiary_main: '#6FE486',
-  tertiary_variant: '#51C472',
+  tertiary_main: '#FFCC41',
+  tertiary_variant: '#DBA82F',
 
   // NOTE: background
-  custom_bg_primary: '#FDFFFE',
+  custom_bg_primary: '#FCFBF9',
   custom_bg_secondary: '#FFFFFF',
-  custom_bg_tertiary: '#F6FEF9',
+  custom_bg_tertiary: '#FDFDFC',
 
   // NOTE: text
-  custom_text_primary: '#282A30',
-  custom_text_secondary: '#6B738A',
-  custom_text_tertiary: '#ADB3C0',
-  custom_text_placeholder: '#CED1D9',
+  custom_text_primary: '#000000',
+  custom_text_secondary: '#9B9B9B',
+  custom_text_tertiary: '#C3C3C3',
+  custom_text_placeholder: '#D7D7D7',
 
   // NOTE: other
-  disabled_main: '#E1E1E1',
-  disabled_variant: '#868686',
+  disabled_main: '#D1D1D1',
+  disabled_variant: '#9B9B9B',
 
   // NOTE: State
-  custom_state_disable_main: '#E1E1E1',
-  custom_state_disable_variant: '#57B8FF',
+  custom_state_disable_main: '#D1D1D1',
+  custom_state_disable_variant: '#9B9B9B',
 
-  custom_state_success_main: '#4FC425',
+  custom_state_success_main: '#89E268',
 
-  custom_state_warning_main: '#FFD100',
-  custom_state_warning_variant: '#DBAF00',
+  custom_state_warning_main: '#FFE60A',
+  custom_state_warning_variant: '#DBC307',
 
-  custom_state_error_main: '#DB2C23',
-  custom_state_error_variant: '#F9DDDC',
+  custom_state_error_main: '#DB5048',
+  custom_state_error_variant: '#DB5048',
 
-  custom_state_info_main: '#1376EF',
-  custom_state_info_variant: '#E7F1FD',
+  custom_state_info_main: '#50B4FC',
+  custom_state_info_variant: '#50B4FC',
 };
 
 export default theme;
