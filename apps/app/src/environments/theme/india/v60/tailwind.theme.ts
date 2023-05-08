ffacc59673827922a794938a084f19b216f4014f
Emma <imgenu@gmail.com>
2023-05-08 12:58:11 +0800

    feat(app):FRONTEND-184_[APP][色系] 印度 v60

---
+++ apps/app/src/environments/theme/india/v60/tailwind.theme.ts
@@ -0,0 +1,47 @@
+import { ICustomTailwindTheme } from '../../../../app/modules/theme/ICustomTailwindTheme';
+
+const theme: ICustomTailwindTheme = {
+  // NOTE: Color
+  primary_main: '#EFB214',
+  primary_variant: '#CD920E',
+  primary_assistant: '#FEF6CF',
+
+  secondary_main: '#75A5ED',
+  secondary_variant: '#5580CB',
+  secondary_assistant: '#E4F2FE',
+
+  tertiary_main: '#FADB70',
+  tertiary_variant: '#D7B551',
+
+  // NOTE: State
+  custom_state_disable_main: '#BDBDBD',
+  custom_state_disable_variant: '#8F8F8F',
+
+  custom_state_success_main: '#7FDD44',
+
+  custom_state_warning_main: '#C6BDFF',
+  custom_state_warning_variant: '#948ADB',
+
+  custom_state_error_main: '#FF573D',
+  custom_state_error_variant: '#FF573D',
+
+  custom_state_info_main: '#3FA2FF',
+  custom_state_info_variant: '#3FA2FF',
+
+  // NOTE: Background
+  custom_bg_primary: '#FCFBF9',
+  custom_bg_secondary: '#FFFFFF',
+  custom_bg_tertiary: '#FFFCF2',
+
+  // NOTE: Text
+  custom_text_primary: '#2C2C2C',
+  custom_text_secondary: '#727272',
+  custom_text_tertiary: '#A1A1A1',
+  custom_text_placeholder: '#AEAEAE',
+
+  // NOTE: Other
+  disabled_main: '#BDBDBD',
+  disabled_variant: '#8F8F8F',
+};
+
+export default theme;
