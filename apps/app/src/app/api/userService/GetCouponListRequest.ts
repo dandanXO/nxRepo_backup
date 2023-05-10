567718bb391a2d7ffc1ad0f2dd112f2058775e64
Emma <imgenu@gmail.com>
2023-05-10 12:02:05 +0800

    feat(app):FRONTEND-119_[APP][IN] MyCouponListPage

---
+++ apps/app/src/app/api/userService/GetCouponListRequest.ts
@@ -0,0 +1,5 @@
+export interface GetCouponListRequest {
+    pageNumber: number; // 頁碼, default: 0
+    pageSize: number;   // 一頁幾筆紀錄, default: 10
+    status: 'UNUSED' | 'USED' | 'EXPIRED' | 'USED_EXPIRE'; // 優惠卷狀態
+}
--- apps/app/src/app/presentation/pages/MyCouponListPage/index.tsx
+++ apps/app/src/app/presentation/pages/MyCouponListPage/index.tsx
@@ -1,27 +1,69 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Tags } from '../../components/Tag';
 import { Page } from '../../components/layouts/Page';
+import { useLazyGetCouponListQuery } from '../../../api/rtk';
+import { GetCouponListRequest } from '../../../api/userService/GetCouponListRequest';
+import Coupon from '../../components/Coupon';
+import { environment } from 'apps/app/src/environments/environment';
+import { IndiaCountry } from 'libs/shared/domain/src/country/IndiaCountry';
 
 const MyCouponListPage = () => {
-  const [listStatus, setListStatus] = useState(0);
-  return (
-    <Page className="flex flex-col">
-      <div
-        className={`flex flex-row py-3 px-5 justify-between sticky top-[0px] bg-white`}
-      >
-        <Tags
-          items={['Usable', 'Used', 'Expired']}
-          layoutType={2}
-          style={` text-sm mx-1`}
-        />
+    const [listStatus, setListStatus] = useState('Usable');
 
-        {/* {['Usable', 'Used', 'Expired'].map((i,index) => <Tag layoutType={2}
-                    key={i} onClick={() => setListStatus(index)} text={i}
-                    active={index === listStatus} style={` text-sm mx-1`} />)} */}
-      </div>
-      123
-    </Page>
-  );
+    const [ triggerGetList, { currentData, isLoading, isFetching, isSuccess, isError, isUninitialized }] = useLazyGetCouponListQuery({
+        pollingInterval: 0,
+        refetchOnFocus: false,
+        refetchOnReconnect: false,
+    });
+
+    const statusEnum = {
+        Usable: 'UNUSED',
+        Used: 'USED',
+        Expired: 'EXPIRED',
+    } as { [key: string]: string };
+
+    useEffect(() => {
+        triggerGetList({
+            pageNumber: 0,
+            pageSize: 500,
+            status: statusEnum[listStatus] as GetCouponListRequest['status'],
+        });
+    }, [listStatus]);
+
+    return (
+        <Page className="flex flex-col">
+            <div className={`flex flex-row py-3 px-5 justify-between sticky top-[0px] bg-white`}>
+                <Tags
+                    items={['Usable', 'Used', 'Expired']}
+                    layoutType={2}
+                    style={` text-sm mx-1`}
+                    onClick={(i: any) => setListStatus(['Usable', 'Used', 'Expired'][i])}
+                />
+            </div>
+            <div className='mx-4'>
+                {currentData && currentData.records && currentData.records.length > 0 ? (
+                    currentData?.records?.map((i) => {
+                        return <Coupon
+                            expireTime={i.expiredTime}
+                            discountAmount={i.discountAmount}
+                            couponType={i.couponType}
+                            couponName={i.couponName}
+                            couponContent={i.couponContent}
+                            status={listStatus === 'Usable' ? "normal" : "disabled"}
+                            key={i.couponId}
+                            buttonText={listStatus === 'Usable' ? 'USE NOW' : i.redeemed ? 'USED' : 'EXPIRED'}
+                            layoutType={ environment.country === IndiaCountry.country ? 1 : 2}
+                        />;
+                    })
+                ) : (
+                    <div className="flex justify-center items-center p-3 grow">
+                        There are no orders currently
+                    </div>
+                )}
+            </div>
+
+        </Page>
+    );
 };
 
 export default MyCouponListPage;
