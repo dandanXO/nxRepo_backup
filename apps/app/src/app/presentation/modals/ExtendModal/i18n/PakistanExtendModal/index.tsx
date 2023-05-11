cdd7fefd46f8016cec7d9e923ffea9f3da6b5472
Emma <imgenu@gmail.com>
2023-05-11 16:29:35 +0800

    feat(app):PakistanExtendModal adjust text color with status

--- apps/app/src/app/presentation/modals/ExtendModal/i18n/PakistanExtendModal/index.tsx
+++ apps/app/src/app/presentation/modals/ExtendModal/i18n/PakistanExtendModal/index.tsx
@@ -6,105 +6,133 @@ import moment from 'moment';
 import { Button } from '../../../../components/layouts/Button';
 import CustomSelect from '../../../../components/Select';
 import Money from '../../../../components/Money.tsx';
-
+import { Status } from '../../../../../modules/statusEnum';
 type paymentMethodValueType = {
-  value: string;
-  label: string;
+    value: string;
+    label: string;
 };
 const PakistanExtendModal = (props: any) => {
-  const navigate = useNavigate();
-  const {
-    repayConfirmDetail: { extendDate, extensionFee, extensionPayAmount = '' },
-    orderNo = '',
-    productName = '',
-    dueDate = '',
-    overdueDays = '',
-    penaltyInterest = '',
-    reductionAmount,
-    paidAmount,
-  } = props.currentData ?? {};
+    const navigate = useNavigate();
+    const {
+        repayConfirmDetail: { extendDate, extensionFee, extensionPayAmount = '' },
+        orderNo = '',
+        productName = '',
+        dueDate = '',
+        overdueDays = '',
+        penaltyInterest = '',
+        reductionAmount,
+        paidAmount,
+        status = ''
+    } = props.currentData ?? {};
 
-  return (
-    <div className={`p-2`}>
-      <div className="text-xl font-bold mb-4">Extend</div>
-      <ListItem title={'Product'} text={productName ?? ''} />
-      <ListItem title={'No.'} text={orderNo ?? ''} />
-      <ListItem
-        title={'Due Date'}
-        text={dueDate ? moment(dueDate).format('DD-MM-YYYY') : ''}
-      />
-      <ListItem title={'Overdue Days'} text={overdueDays ?? ''} />
-      {/*NOTE: 展期費用*/}
-      <ListItem title={'Extension Fee'} text={<Money money={extensionFee} />} />
-      {/*NOTE: 展期罰金*/}
-      <ListItem
-        title={'Overdue Fee'}
-        text={<Money money={penaltyInterest} />}
-        textColor={'text-red-500'}
-      />
+    return (
+        <div className={`p-2`}>
+            <div className="text-xl font-bold mb-4 text-ctext-primary">Extend</div>
+            <ListItem
+                title={'Product'}
+                text={productName ?? ''}
+                titleColor="text-ctext-secondary"
+                textColor="text-ctext-primary"
+            />
+            <ListItem
+                title={'No.'}
+                text={orderNo ?? ''}
+                titleColor="text-ctext-secondary"
+                textColor="text-ctext-primary"
+            />
+            <ListItem
+                title={'Due Date'}
+                text={dueDate ? moment(dueDate).format('DD-MM-YYYY') : ''}
+                titleColor="text-ctext-secondary"
+                textColor="text-ctext-primary"
+            />
+            {/*NOTE: 展期費用*/}
+            <ListItem
+                title={'Extension Fee'}
+                text={<Money money={extensionFee} />}
+                titleColor="text-ctext-secondary"
+                textColor="text-ctext-primary"
+            />
+            <ListItem
+                title={'Overdue Days'}
+                text={overdueDays ?? ''}
+                titleColor="text-ctext-secondary"
+                textColor={status === 'OVERDUE' ? Status(status).color : "text-ctext-primary"}
+            />
+            {/*NOTE: 展期罰金*/}
+            <ListItem
+                title={'Overdue Fee'}
+                text={<Money money={penaltyInterest} />}
+                titleColor="text-ctext-secondary"
+                textColor={status === 'OVERDUE' ? Status(status).color : ''}
+            />
 
-      {/*NOTE: 減免金額*/}
-      <ListItem
-        title={'Reduction Amount'}
-        text={<Money money={reductionAmount} isNagetive={true} />}
-        textColor={'text-red-500'}
-      />
+            {/*NOTE: 減免金額*/}
+            <ListItem
+                title={'Reduction Amount'}
+                text={<Money money={reductionAmount} isNagetive={true} />}
+                titleColor="text-ctext-secondary"
+                textColor="text-ctext-primary"
+            />
 
-      {/*NOTE: 已還金額*/}
-      <ListItem
-        title={'Amount Repaid'}
-        text={<Money money={paidAmount} isNagetive={true} />}
-        textColor={'text-red-500'}
-      />
+            {/*NOTE: 已還金額*/}
+            <ListItem
+                title={'Amount Repaid'}
+                text={<Money money={paidAmount} isNagetive={true} />}
+                titleColor="text-ctext-secondary"
+                textColor="text-ctext-primary"
+            />
 
-      <ListItem
-        title={'Extension Due Date'}
-        text={extendDate ? moment(extendDate).format('DD-MM-YYYY') : ''}
-        textColor={overdueDays > 0 ? 'text-red-500' : ''}
-      />
+            <ListItem
+                title={'Extension Due Date'}
+                text={extendDate ? moment(extendDate).format('DD-MM-YYYY') : ''}
+                titleColor="text-ctext-secondary"
+                textColor="text-primary-main"
+            />
 
-      <Divider />
+            <Divider />
 
-      {/*NOTE: 總金額*/}
-      <ListItem
-        className="font-bold"
-        title={'Total Extension Fee' as string}
-        text={<Money money={extensionPayAmount} />}
-      />
+            {/*NOTE: 總金額*/}
+            <ListItem
+                className="font-bold mt-3"
+                title={'Total Extension Fee' as string}
+                text={<Money money={extensionPayAmount} />}
+                titleColor="text-ctext-primary"
+            />
 
-      <div className="mt-6  mb-5 bg-gray-200 h-2 mx-[-20px]"></div>
-      <div className="text-black text-xs font-bold text-left">
-        {'Payment Method'}
-      </div>
-      <CustomSelect
-        type={'standard'}
-        options={props.repayTypesList || []}
-        value={props?.repayType}
-        onChange={(item: any) => {
-          props.setRepayType(item as paymentMethodValueType);
-        }}
-        isSearchable={false}
-      />
+            <div className="mt-6  mb-5 bg-cstate-disable-main h-2 mx-[-20px]"></div>
+            <div className="text-black text-xs font-bold text-left">
+                {'Payment Method'}
+            </div>
+            <CustomSelect
+                type={'standard'}
+                options={props.repayTypesList || []}
+                value={props?.repayType}
+                onChange={(item: any) => {
+                    props.setRepayType(item as paymentMethodValueType);
+                }}
+                isSearchable={false}
+            />
 
-      <div className={`flex flex-row mt-6 text-white`}>
-        <div className={`grow mr-1.5`}>
-          <Button
-            type={'ghost'}
-            onClick={() => navigate(-2)}
-            text={'Cancel'}
-            className={`w-full`}
-          />
+            <div className={`flex flex-row mt-6 text-white`}>
+                <div className={`grow mr-1.5`}>
+                    <Button
+                        type={'ghost'}
+                        onClick={() => navigate(-2)}
+                        text={'Cancel'}
+                        className={`w-full`}
+                    />
+                </div>
+                <div className={`grow ml-1.5`}>
+                    <Button
+                        onClick={props.handleConfirm}
+                        text={'Confirm'}
+                        className={`w-full`}
+                    />
+                </div>
+            </div>
         </div>
-        <div className={`grow ml-1.5`}>
-          <Button
-            onClick={props.handleConfirm}
-            text={'Confirm'}
-            className={`w-full`}
-          />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default PakistanExtendModal;
