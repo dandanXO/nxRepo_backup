0ab69e823b7212e4b493cfa7196fa827b5380891
Emma <imgenu@gmail.com>
2023-05-12 17:51:47 +0800

    feat(app):FRONTEND-193-194_[APP][IN][色系] 印度 v61 v62

---
+++ apps/app/src/environments/theme/india/v61/tailwind.theme.ts
@@ -0,0 +1,51 @@
+import { ICustomTailwindTheme } from '../../../../app/modules/theme/ICustomTailwindTheme';
+
+const theme: ICustomTailwindTheme = {
+    // NOTE: Color
+    primary_main: '#5374BD',
+    primary_variant: '#3C59A2',
+    primary_assistant: '#DFEBFB',
+
+    secondary_main: '#D4A418',
+    secondary_variant: '#B68711',
+    secondary_assistant: '#FCF6D0',
+
+    tertiary_main: '#9CB9EB',
+    tertiary_variant: '#728FCA',
+
+    // NOTE: State
+    custom_state_disable_main: '#BEBEBE',
+    custom_state_disable_variant: '#919191',
+    custom_state_disable_assistant: '#ECECEC',
+
+    custom_state_success_main: '#74D260',
+
+    custom_state_warning_main: '#F9A959',
+    custom_state_warning_variant: '#D68441',
+
+    custom_state_error_main: '#FF4444',
+    custom_state_error_variant: '#FFE6D9',
+
+    custom_state_info_main: '#4699D6',
+    custom_state_info_variant: '#DAF6FC',
+
+    // NOTE: Background
+    custom_bg_primary: '#F9FAFC',
+    custom_bg_secondary: '#FFFFFF',
+    custom_bg_tertiary: '#F1F7FF',
+    custom_bg_homepage_button: '#5374BD', // 首頁按鈕 Get my limit
+
+    // NOTE: Text
+    custom_text_primary: '#2B2B2B',
+    custom_text_secondary: '#747474',
+    custom_text_tertiary: '#A0A0A0',
+    custom_text_divider: '#E8E8E8',
+
+    // NOTE: Text Fields
+    custom_text_fields_background_main: '#FFFFFF',
+    custom_text_fields_background_variant: '#EEEEEE',
+    custom_text_fields_placeholder_main: '#AEAEAE',
+    custom_text_fields_outline_main: '#AAAAAA',
+};
+
+export default theme;
