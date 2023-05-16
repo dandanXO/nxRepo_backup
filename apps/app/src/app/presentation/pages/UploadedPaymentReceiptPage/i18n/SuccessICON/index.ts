7f81c76737d045e3686f0f1fc8503bffa2f1924d
Emma <imgenu@gmail.com>
2023-05-16 11:18:41 +0800

    feat(app):FRONTEND-210_[APP][IN][Theme] 還款證明上傳完成頁

--- apps/app/src/app/presentation/pages/UploadedPaymentReceiptPage/i18n/SuccessICON/index.ts
+++ apps/app/src/app/presentation/pages/UploadedPaymentReceiptPage/i18n/SuccessICON/index.ts
@@ -1,13 +1,14 @@
 import {
   GeneralSuccessICON,
-  OrangeSuccessICON,
+//   OrangeSuccessICON,
 } from '@frontend/mobile/shared/ui';
+import IndiaSuccessIcon from '../india/IndiaSuccessIcon'
 import PakistanSuccessIcon from '../pakistan/PakistanSuccessIcon';
 import { environment } from '../../../../../../environments/environment';
 
 export const SuccessICON =
   environment.country === 'in'
-    ? OrangeSuccessICON
+    ? IndiaSuccessIcon
     : environment.country === 'pk'
     ? PakistanSuccessIcon
     : GeneralSuccessICON;
