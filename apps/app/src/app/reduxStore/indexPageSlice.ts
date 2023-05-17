3c6fa0112d97bc062f252259a5304ac0436c9915
fronteric <fronteric@1.1>
2023-05-17 16:49:44 +0800

    feat(APP): FRONTEND-196 posthog

--- apps/app/src/app/reduxStore/indexPageSlice.ts
+++ apps/app/src/app/reduxStore/indexPageSlice.ts
@@ -120,6 +120,7 @@ export const indexPageSlice = createSlice({
     },
     updateIndexAPI: (state, action: PayloadAction<GetIndexResponse>) => {
       // console.log("updateIndexAPI", state, action)
+
       state.indexAPI = action.payload;
       state.sharedIndex.marquee = action.payload.marquee;
 
