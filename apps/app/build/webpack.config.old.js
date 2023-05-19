1b80beec4305e6dda5ff81f34431779d65a038ec
fronteric <fronteric@1.1>
2023-05-19 22:04:01 +0800

    build: eval-cheap-source-map

--- apps/app/build/webpack.config.old.js
+++ apps/app/build/webpack.config.old.js
@@ -59,7 +59,9 @@ module.exports = (config, context) => {
     // devtool: "eval-source-map",
     // NOTE: Android 9 成功
     // devtool: "eval-source-map",
-    devtool: "eval-cheap-module-source-map",
+    // NOTE: Android 9 成功
+    // devtool: "eval-cheap-module-source-map",
+    devtool: "eval-cheap-source-map",
     output: {
       // filename: '[name].[contenthash].js',
       // sourceMapFilename: 'maps/[name].[contenthash].map.js'
