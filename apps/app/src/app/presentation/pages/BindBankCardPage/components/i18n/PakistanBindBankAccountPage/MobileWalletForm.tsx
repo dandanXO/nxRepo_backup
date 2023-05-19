11227cbf96b52af10c5771363b65d219ccc4227d
fronteric <fronteric@1.1>
2023-05-19 17:18:45 +0800

    feat(FRONTEND-221): [APP][PK] 電子錢包綁卡調整

--- apps/app/src/app/presentation/pages/BindBankCardPage/components/i18n/PakistanBindBankAccountPage/MobileWalletForm.tsx
+++ apps/app/src/app/presentation/pages/BindBankCardPage/components/i18n/PakistanBindBankAccountPage/MobileWalletForm.tsx
@@ -104,7 +104,6 @@ export const MobileWalletForm = (props: IMobileWalletForm) => {
           textAlign={'left'}
           labelType={'left'}
           outlineType={'standard'}
-          label={'+92'}
           placeholder={'Confirm Mobile Wallet Account'}
           value={props.confirmMobileData.data}
           onChange={props.onConfirmMobileDataChange}
