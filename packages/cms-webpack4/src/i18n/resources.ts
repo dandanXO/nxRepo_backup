631bf1943a6c014aaff73a48442c9a692d88373f
eddie <a589658gur@gmail.com>
2023-06-19 16:56:21 +0800

    feat(cms):FRONTEND-241 i18n翻譯檔改為json file

--- packages/cms-webpack4/src/i18n/resources.ts
+++ packages/cms-webpack4/src/i18n/resources.ts
@@ -1,12 +1,12 @@
-import commonZHCN from './locales/zh-CN/common'
-import userZHCN from './locales/zh-CN/user'
-import orderZHCN from './locales/zh-CN/order'
-import urgeCollectionZHCN from "./locales/zh-CN/urgeCollection";
+import commonZHCN from './locales/zh-CN/common.json'
+import userZHCN from './locales/zh-CN/user.json'
+import orderZHCN from './locales/zh-CN/order.json'
+import urgeCollectionZHCN from "./locales/zh-CN/urgeCollection.json";
 
-import commonENUS from './locales/en-US/common'
-import userENUS from './locales/en-US/user'
-import orderENUS from './locales/en-US/order'
-import urgeCollectionENUS from "./locales/en-US/urgeCollection";
+import commonENUS from './locales/en-US/common.json'
+import userENUS from './locales/en-US/user.json'
+import orderENUS from './locales/en-US/order.json'
+import urgeCollectionENUS from "./locales/en-US/urgeCollection.json";
 
 export default {
     "zh-CN": {
