52ed197977d24cd0ceafa0cc99ea5b5b6a56cf0d
Emma <imgenu@gmail.com>
2023-08-07 16:36:51 +0800

    feata(app):FRONTEND-318_[APP][IN] APP 測試修正 (L3首頁文案調整)

--- apps/app/src/app/presentation/pages/IndexPage/noticeSections/NoticeOrderOrQuotaRejectedSection.tsx
+++ apps/app/src/app/presentation/pages/IndexPage/noticeSections/NoticeOrderOrQuotaRejectedSection.tsx
@@ -1,19 +1,28 @@
+import { IndexPageProps } from 'apps/app/src/app/reduxStore';
 import { NoticeSectionContainer } from './index';
 
-type Props = {};
+type Props = IndexPageProps;
+
 export const NoticeOrderOrQuotaRejectedSection = (props: Props) => {
+    const withinOneDay = Number(props.state.timeout.refreshableDate.days) < 1;
     return (
         <NoticeSectionContainer dataTestingID="noticeOrderOrQuotaRejected">
-            <div className={'mb-2 text-primary-main font-bold'}>We apologize for the inconvenience</div>
-            <div className={'paragraph mb-4 font-normal leading-none text-sm'}>
-                We are currently unable to process your loan application. This does not mean that your credit is bad; it is simply due to a high number of current applicants, making it difficult for us to meet everyone's needs immediately.
-            </div>
-            <div className={'paragraph mb-4 font-normal leading-none text-sm'}>
-                Tip: Repaying loans on time can help prioritize your loan application.
-            </div>
-            <div className={'paragraph mb-5 font-normal leading-none text-sm'}>
-                You are welcome to try applying again after the countdown is complete.
-            </div>
+            <div className={'mb-2 text-primary-main font-bold'}>Application Denied</div>
+            {withinOneDay ?
+             (<div className={'paragraph mb-4 font-normal leading-none text-sm'}>
+                Your application has been declined due to insufficient credit score. You are welcome to try applying again in 24 hours.
+             </div>) : 
+             (<div>
+                <div className={'paragraph mb-4 font-normal leading-none text-sm'}>
+                    We regret to inform you that your application has been denied due to your current credit score not meeting the requirements.
+                </div>
+                <div className={'paragraph mb-5 font-normal leading-none text-sm'}>
+                    You
+                    We kindly request you to try again later.
+                </div>
+             </div>
+            )}
+            
         </NoticeSectionContainer>
     );
 };
