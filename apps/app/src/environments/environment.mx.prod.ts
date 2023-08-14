0dfcb6023c59a70222b4a1fd73be8a051b8b1911
fronteric <fronteric@1.1>
2023-08-14 14:24:48 +0800

    build(mx/production): nx-project.json, app-environment.ts

---
+++ apps/app/src/environments/environment.mx.prod.ts
@@ -0,0 +1,7 @@
+import { IndiaCountry } from '../../../../libs/shared/domain/src/country/IndiaCountry';
+import { IEnvironment } from '../app/modules/appEnvironment/IEnvironment';
+
+export const environment: IEnvironment = {
+  production: true,
+  ...IndiaCountry,
+};
