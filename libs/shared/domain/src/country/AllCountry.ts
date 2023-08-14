451680a422fd262ca574518ac10b424bd3720395
fronteric <fronteric@1.1>
2023-08-14 15:27:42 +0800

    refactor(shared/domain): note

--- libs/shared/domain/src/country/AllCountry.ts
+++ libs/shared/domain/src/country/AllCountry.ts
@@ -3,6 +3,7 @@ import {PakistanCountry} from "./PakistanCountry";
 import {ICountry} from "./types/ICountry";
 import {IndiaCountry} from "./IndiaCountry";
 
+// REFACTOR: mobile 那邊有自定義
 export const AllCountry: ICountry[] = [
   IndiaCountry,
   PakistanCountry,
--- libs/shared/domain/src/country/AllCountryInstance.ts
+++ libs/shared/domain/src/country/AllCountryInstance.ts
@@ -2,6 +2,7 @@ import {IndiaCountry} from "./IndiaCountry";
 import {PakistanCountry} from "./PakistanCountry";
 import {BangladeshCountry} from "./BangladeshCountry";
 
+// REFACTOR: mobile 那邊有自定義
 export const AllCountryInstance = {
   IndiaCountry,
   PakistanCountry,
