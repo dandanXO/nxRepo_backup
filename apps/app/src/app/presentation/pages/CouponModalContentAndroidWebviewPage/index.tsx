94138fe5003716e9ac15606825cc102c09e47f23
Emma <imgenu@gmail.com>
2023-09-08 16:08:00 +0800

    feat(app):FRONTEND-325 [APP][PH] 菲律賓開發故事 (CouponModalContentAndroidWebviewPage add coupon picture by country)

--- apps/app/src/app/presentation/pages/CouponModalContentAndroidWebviewPage/index.tsx
+++ apps/app/src/app/presentation/pages/CouponModalContentAndroidWebviewPage/index.tsx
@@ -1,7 +1,20 @@
+import { environment } from 'apps/app/src/environments/environmentModule/environment';
 import CouponImageSource from '../../../../assets/coupon.png';
+import { AllCountriesEnum } from 'libs/shared/domain/src/country/AllCountry';
+import { AllCountryIdentityName } from 'libs/shared/domain/src/country/enum/AllCountryIdentityName';
+import MexicoCoupon from './i18n/MexicoCoupon.svg';
+import PhilippinesCoupon from './i18n/PhilippinesCoupon.png';
 
 const CouponModalContentAndroidWebviewPage = () => {
-  return <img src={CouponImageSource} />;
+  const couponImage = {
+    [AllCountryIdentityName.IN]: CouponImageSource,
+    [AllCountryIdentityName.PK]: CouponImageSource,
+    [AllCountryIdentityName.BN]: CouponImageSource,
+    [AllCountryIdentityName.MX]: MexicoCoupon,
+    [AllCountryIdentityName.PH]: PhilippinesCoupon,
+  }[environment.country]
+
+  return <img src={couponImage ? couponImage : CouponImageSource} />;
 };
 
 export default CouponModalContentAndroidWebviewPage;
