8167b0b6623eae70b9dd84c7eed4e2f78f7283a7
eddie <a589658gur@gmail.com>
2023-09-11 16:38:16 +0800

    feat(app):FRONTEND-326 [APP][PH] 菲律賓還款流程
    fix

--- apps/app/src/app/presentation/pages/BindBankCardPage/i18nPage/PhilippinesBindBankAccountPage/validation.ts
+++ apps/app/src/app/presentation/pages/BindBankCardPage/i18nPage/PhilippinesBindBankAccountPage/validation.ts
@@ -3,15 +3,20 @@ import { z } from 'zod';
 export const validationPHMobileNumber = (data: string) => {
   const mobileNumberSchema = z
     .string()
-    .min(11, 'Account number should be 11 digits starting with 0')
-    .max(11, 'Account number should be 11 digits starting with 0')
+    .refine(
+      (value) => value.length === 11,
+      'Account number should be 11 digits'
+    )
     .refine(
       (value) => value.startsWith('0'),
       'Account number should be 11 digits starting with 0'
-    );
+    )
+    .refine((value) => value !== '', 'This field cannot be left blank.');
   const result = mobileNumberSchema.safeParse(data);
   const isValidation = result.success;
-  const errorMessage = !isValidation ? result.error.format()._errors[0] : '';
+  const errorMessage = !isValidation
+    ? result.error.errors[result.error.errors.length - 1].message
+    : '';
   return {
     data,
     isValidation,
