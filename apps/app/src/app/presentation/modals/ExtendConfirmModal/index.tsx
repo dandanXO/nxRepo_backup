0214c3c0e289031425a1e4533763cb16530e4393
eddie <a589658gur@gmail.com>
2023-09-11 10:39:15 +0800

    feat(app):FRONTEND-326 [APP][PH] 菲律賓還款流程
    實作展期確認Modal

--- apps/app/src/app/presentation/modals/ExtendConfirmModal/index.tsx
+++ apps/app/src/app/presentation/modals/ExtendConfirmModal/index.tsx
@@ -1,27 +1,32 @@
+import { MexicoCountry } from 'libs/shared/domain/src/country/MexicoCountry';
 import { useLocation, useNavigate } from 'react-router';
+
 import { IndiaCountry } from '../../../../../../../libs/shared/domain/src/country/IndiaCountry';
 import { PakistanCountry } from '../../../../../../../libs/shared/domain/src/country/PakistanCountry';
+import { PhilippinesCountry } from '../../../../../../../libs/shared/domain/src/country/PhilippinesCountry';
 import { renderByCountry } from '../../../modules/i18n';
 import IndiaExtendConfirmModal from './i18n/IndiaExtendConfirmModal';
-import PakistanExtendConfirmModal from './i18n/PakistanExtendConfirmModal';
-import { MexicoCountry } from 'libs/shared/domain/src/country/MexicoCountry';
 import MexicoExtendConfirmModal from './i18n/MexicoExtendConfirmModal';
+import PakistanExtendConfirmModal from './i18n/PakistanExtendConfirmModal';
+import PhilippinesExtendConfirmModal from './i18n/PhilippinesExtendConfirmModal';
 
 const ExtendConfirmModal = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
-    return (
-        <div>
-            {renderByCountry(
-                {
-                    [IndiaCountry.country]: <IndiaExtendConfirmModal />,
-                    [PakistanCountry.country]: <PakistanExtendConfirmModal />,
-                    [MexicoCountry.country]: <MexicoExtendConfirmModal />,
-                },
-                <IndiaExtendConfirmModal />
-            )};
-        </div>
-    );
+  const navigate = useNavigate();
+  const location = useLocation();
+  return (
+    <div>
+      {renderByCountry(
+        {
+          [IndiaCountry.country]: <IndiaExtendConfirmModal />,
+          [PakistanCountry.country]: <PakistanExtendConfirmModal />,
+          [MexicoCountry.country]: <MexicoExtendConfirmModal />,
+          [PhilippinesCountry.country]: <PhilippinesExtendConfirmModal />,
+        },
+        <IndiaExtendConfirmModal />
+      )}
+      ;
+    </div>
+  );
 };
 
 export default ExtendConfirmModal;
