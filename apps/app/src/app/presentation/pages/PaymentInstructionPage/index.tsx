a3e7dd100758b71ae2308327952d21510d878b1c
eddie <a589658gur@gmail.com>
2023-09-13 15:16:14 +0800

    feat(app):FRONTEND-326 [APP][PH] 菲律賓還款流程
    PaymentInstructionPage改成renderByCountry

--- apps/app/src/app/presentation/pages/PaymentInstructionPage/index.tsx
+++ apps/app/src/app/presentation/pages/PaymentInstructionPage/index.tsx
@@ -1,129 +1,60 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router';
 
-import { PostRepayCreateResponse } from '../../../api/loanService/PostRepayCreateResponse';
+import { MexicoCountry } from '../../../../../../../libs/shared/domain/src/country/MexicoCountry';
+import { PhilippinesCountry } from '../../../../../../../libs/shared/domain/src/country/PhilippinesCountry';
+import { environment } from '../../../../environments/environmentModule/environment';
+import { renderByCountry } from '../../../modules/i18n';
 import { getOrderNo } from '../../../modules/querystring/getOrderNo';
 import { getToken } from '../../../modules/querystring/getToken';
 import { isShowNavigation } from '../../../modules/window/isShowNavigation';
-import { CopyButton } from '../../components/Buttons';
-import { Label } from '../../components/Labels';
-import Money from '../../components/Money.tsx';
 import { Navigation } from '../../components/layouts/Navigation';
 import { Page } from '../../components/layouts/Page';
 import { PagePathEnum } from '../PagePathEnum';
-import { RepaymentDetailPageUseCaseActions } from '../RepaymentDetailPage/userUsecaseSaga';
+import MexicoPaymentInstructionPage from './i18nPage/MexicoPaymentInstructionPage';
+
+const navigatorMap = {
+  [MexicoCountry.country]: '#E70020',
+  [PhilippinesCountry.country]: '#E85D75',
+};
 
 const PaymentInstructionPage = () => {
   const navigate = useNavigate();
   const { state } = useLocation();
 
-  const dispatch = useDispatch();
-
-  const { payType, orderNo, payload } = state as PostRepayCreateResponse;
-
   return (
     <Page className="flex flex-col">
       {isShowNavigation() && (
         <Navigation
-          title={payType}
+          title={
+            environment.country === MexicoCountry.country
+              ? state.payType
+              : 'Payment Instructions'
+          }
           back={() => {
             navigate(
               `${
                 PagePathEnum.RepaymentDetailPage
               }/repayment-modal?token=${getToken()}&orderNo=${
-                orderNo ?? getOrderNo()
+                state.orderNo ?? getOrderNo()
               }`,
               {
                 state: {},
               }
             );
           }}
-          backgroundColor="#E70020"
+          backgroundColor={navigatorMap[environment.country]}
         />
       )}
-      <div className="flex-grow">
-        <div className="mx-6 mt-9 flex flex-col items-center">
-          <div className="text-ctext-secondary">Total de la Factura</div>
-          <div className="mt-2 mb-4 flex text-4xl font-bold">
-            <Money money={payload?.orderAmount || 0} />
-          </div>
-          {payload?.beneficiario && (
-            <Label
-              className="mb-2"
-              title="Beneficiario"
-              value={payload.beneficiario}
-            />
-          )}
-          {payload?.clabe && (
-            <Label
-              className="mb-2"
-              title="CLABE"
-              value={payload.clabe}
-              extra={
-                <CopyButton
-                  className="rounded-full bg-[#E70020]"
-                  text="Copiar"
-                  value={payload.clabe}
-                />
-              }
-            />
-          )}
-          {payload.referencia && (
-            <Label
-              className="mb-2"
-              title="Referencia"
-              value={payload.referencia}
-              extra={
-                <CopyButton
-                  className="rounded-full bg-[#E70020]"
-                  text="Copiar"
-                  value={payload.referencia}
-                />
-              }
-            />
-          )}
-          {payload.barcodeUrl && (
-            <div className="mt-4 mb-2">
-              <div className="text-center text-sm">
-                Código de barras de pago
-              </div>
-              <img alt="barcodeUrl" src={payload.barcodeUrl} />
-            </div>
-          )}
-        </div>
-        <div className="text-ctext-secondary mx-6 mt-2 items-start text-xs">
-          <div>Instrucciones de pago:</div>
-          <ul className="list-outside list-decimal pl-4 pt-1">
-            <li>
-              Para completar el pago, por favor realiza la transferencia del
-              monto indicado a la cuenta proporcionada.
-            </li>
-            <li>
-              El código de pago es válido durante 24 horas. Por favor, realiza
-              el pago de manera oportuna.
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="mt-auto px-6 py-3">
-        <button
-          className="w-full rounded-full bg-[#E70020] px-2.5 py-4 text-center text-sm font-bold text-white"
-          onClick={() => {
-            navigate(
-              `${
-                PagePathEnum.RepaymentDetailPage
-              }?token=${getToken()}&orderNo=${orderNo}`,
-              { replace: true }
-            );
-            dispatch(
-              RepaymentDetailPageUseCaseActions.system.showReservation()
-            );
-          }}
-        >
-          Pago completado, siguiente paso.
-        </button>
-      </div>
+      {state &&
+        renderByCountry(
+          {
+            [MexicoCountry.country]: (
+              <MexicoPaymentInstructionPage {...state} />
+            ),
+          },
+          <MexicoPaymentInstructionPage {...state} />
+        )}
     </Page>
   );
 };
