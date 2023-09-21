0680d1fda9c6b828bbba5254baf1b61eb5ee8b3f
Emma <imgenu@gmail.com>
2023-09-21 15:12:20 +0800

    feat(app):FRONTEND-358_[APP][IN] APP 測試修正 w38 (adBanner click to auth)

--- apps/app/src/app/presentation/pages/IndexPage/sections/ADBannerSection.tsx
+++ apps/app/src/app/presentation/pages/IndexPage/sections/ADBannerSection.tsx
@@ -1,11 +1,19 @@
+import { useDispatch } from 'react-redux';
 import { IndexPageProps } from '../../../../reduxStore';
+import { IndexPageSagaAction } from '../userUsecaseSaga/indexPageActions';
 
 type Props = IndexPageProps;
 
 export const ADBannerSection = (props: Props) => {
+
+  const dispatch = useDispatch();
   // NOTE: User Event
   const onUserClickToOpenBrowser = (url: string) => {
     window.location.href = url;
+  };
+
+  const onUserClickToVerify = () => {
+    dispatch(IndexPageSagaAction.user.authenticateSaga());
   };
 
   return (
@@ -16,6 +24,7 @@ export const ADBannerSection = (props: Props) => {
             <img
               className={'rounded-lg text-center text-white'}
               src={banner.imageUrl}
+              onClick={onUserClickToVerify}
               // onClick={() => onUserClickToOpenBrowser(banner.jumpUrl || '')}
             />
           </div>
