0bfe49ee43794bb20d11702d40f7ebbb6c45468e
fronteric <fronteric@1.1>
2023-09-21 14:29:45 +0800

    refactor(app): refactor

---
+++ apps/app/src/app/persistant/nativeAppInfo/types/IAppEnvironment.ts
@@ -0,0 +1,15 @@
+import {IAndroidAppInfo} from "./IAndroidAppInfo";
+import {AllCountriesEnum} from "../../../../../../../libs/shared/domain/src/country/AllCountry";
+
+export type IAppEnvironment = {
+  localhost: IAndroidAppInfo;
+  dev: IAndroidAppInfo;
+  prod: IAndroidAppInfo;
+};
+
+export type INullAppInfoTaskDefault = {
+  [AllCountriesEnum.india]: IAppEnvironment;
+  [AllCountriesEnum.pakistan]: IAppEnvironment;
+  [AllCountriesEnum.mexico]: IAppEnvironment;
+  [AllCountriesEnum.philippines]: IAppEnvironment;
+};
