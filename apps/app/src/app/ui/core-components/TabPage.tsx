d7b0aa58d3f4d5bda32e6bf46e4f36693d535c82
Emma <imgenu@gmail.com>
2023-10-11 15:41:10 +0800

    feat(app):FRONTEND-382_[APP] APP測試修正 w40 (PersonalInfo Page height)

--- apps/app/src/app/ui/core-components/TabPage.tsx
+++ apps/app/src/app/ui/core-components/TabPage.tsx
@@ -3,9 +3,9 @@ import cx from 'classnames';
 import { Props } from './Page';
 
 export const TabPage = (props: Props) => {
-  //NOTE: Tab Height: 56px
+  //NOTE: Tab Height: 63px
   return (
-    <div className={cx('h-[calc(100% - 56px)]', props.className)}>
+    <div className={cx('h-[calc(100% - 63px)]', props.className)}>
       {props.children}
     </div>
   );
