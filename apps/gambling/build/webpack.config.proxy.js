783ec3bc5746bc44c92db7338f878cc95c33a38d
fronteric <fronteric@1.1>
2023-11-02 11:54:51 +0800

    build: refactor proxy

--- apps/gambling/build/webpack.config.proxy.js
+++ apps/gambling/build/webpack.config.proxy.js
@@ -1,25 +1,30 @@
-const { APP_IDENTIFICATION } = require('./webpack.config.common');
+const { APP_IDENTIFICATION , isProduction} = require('./webpack.config.common');
 
-// NOTE: Proxy URL
 let proxyURL = null;
-switch (process.env.NODE_COUNTRY) {
-  case 'brazil': {
-    proxyURL = 'https://ds.bmw777slots.com';
-    break;
+
+if(!isProduction) {
+  // NOTE: Proxy URL
+
+  switch (process.env.NODE_COUNTRY) {
+    case 'brazil': {
+      proxyURL = 'https://ds.bmw777slots.com';
+      break;
+    }
+    case 'bmw': {
+      proxyURL = 'https://ds.bmw777slots.com';
+      break;
+    }
+    case 'legend': {
+      proxyURL = 'https://ds.legend777slots.com';
+      break;
+    }
+    default: {
+      throw new Error(APP_IDENTIFICATION + 'please setting proxy url');
+    }
   }
-  case 'bmw': {
-    proxyURL = 'https://ds.bmw777slots.com';
-    break;
-  }
-  case 'legend': {
-    proxyURL = 'https://ds.legend777slots.com';
-    break;
-  }
-  default: {
-    throw new Error(APP_IDENTIFICATION + 'please setting proxy url');
-  }
+
+  console.log(`${APP_IDENTIFICATION} proxyURL: ${proxyURL}`);
 }
-console.log(`${APP_IDENTIFICATION} proxyURL: ${proxyURL}`);
 
 module.exports = {
   proxyURL,
