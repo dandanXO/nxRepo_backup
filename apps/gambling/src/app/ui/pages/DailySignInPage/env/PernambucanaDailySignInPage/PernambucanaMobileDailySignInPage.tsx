fd230c35be9a3529963bdb717ed968f796ce1bd0
eddie <a589658gur@gmail.com>
2023-11-13 22:25:46 +0800

    refactor: dailySignInpage

--- apps/gambling/src/app/ui/pages/DailySignInPage/env/PernambucanaDailySignInPage/PernambucanaMobileDailySignInPage.tsx
+++ apps/gambling/src/app/ui/pages/DailySignInPage/env/PernambucanaDailySignInPage/PernambucanaMobileDailySignInPage.tsx
@@ -1,24 +1,23 @@
 import { LeftOutlined } from '@ant-design/icons';
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router';
 import styled from 'styled-components';
 
-import { PageOrModalPathEnum } from '../../PageOrModalPathEnum';
+import { PageOrModalPathEnum } from '../../../../PageOrModalPathEnum';
 import { Footer } from './Footer';
 import { Notice } from './Notice';
-import { DayList, LevelList } from './index';
-import {Bonus} from "../../components/Bonus";
-import {AppLocalStorage} from "../../../persistant/localstorage";
-import {GetSignInConfigResponse} from "../../../external";
-import { RootState } from '../../../reduxStore';
+import { DayList } from './index';
+import {GetSignInConfigResponse} from "../../../../../external";
+import { RootState } from '../../../../../reduxStore';
 import { useSelector } from 'react-redux';
+import { LevelList } from "../../index";
 
 const Container = styled.div`
   //background: #287052;
   background-color: rgba(40, 112, 82, 0.1);
 
 `;
-export const MobileTemplate = ({
+export const PernambucanMobileDailySignInPage = ({
   currentLevel,
   signInAllConfig,
   onClickToSignIn,
@@ -47,55 +46,55 @@ export const MobileTemplate = ({
   const vip_level = useSelector((state: RootState) => state.app?.vip_level)
 
   return (
-      <Container className={'100vh relative'}>
+    <Container className={'100vh relative'}>
       {/*// <Container className={'h-full relative'}>*/}
       {/*  <section className={"sm:rounded-3xl text-white font-bold overflow-hidden relative mb-8"}>*/}
       {/*    <img className="w-full" src={`assets/${environment.assetPrefix}/bannerVIP.png`}/>*/}
       {/*  </section>*/}
 
-        <div className={'p-2'}>
-          <LeftOutlined
-            style={{ fontSize: '24px' }} // 調整箭頭圖示的大小
-            className={'absolute left-[20px] top-[20px] text-[#ffffff] font-bold'}
-            onClick={() => {
-              navigate(PageOrModalPathEnum.IndexPage);
-            }}
-          />
+      <div className={'p-2'}>
+        <LeftOutlined
+          style={{ fontSize: '24px' }} // 調整箭頭圖示的大小
+          className={'absolute left-[20px] top-[20px] text-[#ffffff] font-bold'}
+          onClick={() => {
+            navigate(PageOrModalPathEnum.IndexPage);
+          }}
+        />
 
 
         <div className={'mt-2.5 mb-2 text-lg font-bold text-white'}>
-            Meu nível atual VIP {currentLevel}
-          </div>
+          Meu nível atual VIP {currentLevel}
+        </div>
 
-          <div className={'mt-9'}>
+        <div className={'mt-9'}>
           <LevelList
             startLevel={1}
             currentLevel={currentLevel}
             currentSelectedLevel={currentSelectedLevel}
             setCurrentSelectedLevel={setCurrentSelectedLevel}
           />
-          </div>
-
-          <DayList
-            className={'mx-4 flex-wrap justify-start'}
-            itemClassName={'shrink-0 grow-0 basis-[20%] mb-4'}
-            currentSelectedLevel={currentSelectedLevel}
-            signInAllConfig={signInAllConfig}
-            signInConfig={signInConfig}
-            signInTotalDays={signInTotalDays}
-            todayIsSignIn={todayIsSignIn}
-            vipLevel={vipLevel}
-          />
-
-          <Notice />
-
-          <Footer
-            onClickToSignIn={onClickToSignIn}
-            todayIsSignIn={todayIsSignIn}
-            vipLevel={vipLevel}
-            setCurrentSelectedLevel={setCurrentSelectedLevel}
-          />
         </div>
-      </Container>
+
+        <DayList
+          className={'mx-4 flex-wrap justify-start'}
+          itemClassName={'shrink-0 grow-0 basis-[20%] mb-4'}
+          currentSelectedLevel={currentSelectedLevel}
+          signInAllConfig={signInAllConfig}
+          signInConfig={signInConfig}
+          signInTotalDays={signInTotalDays}
+          todayIsSignIn={todayIsSignIn}
+          vipLevel={vipLevel}
+        />
+
+        <Notice />
+
+        <Footer
+          onClickToSignIn={onClickToSignIn}
+          todayIsSignIn={todayIsSignIn}
+          vipLevel={vipLevel}
+          setCurrentSelectedLevel={setCurrentSelectedLevel}
+        />
+      </div>
+    </Container>
   );
 };
