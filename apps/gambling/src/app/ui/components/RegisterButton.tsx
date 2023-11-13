ac56207c96ec48c876eff7ee62361ee75093267e
fronteric <fronteric@1.1>
2023-11-13 14:47:36 +0800

    feat(gambling): registerbutton

--- apps/gambling/src/app/ui/components/RegisterButton.tsx
+++ apps/gambling/src/app/ui/components/RegisterButton.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import {UserMoneyStatusSection} from "../header/UserMoneyStatusSection";
+import React from "react";
 
 export const RegisterButton = styled.button`
   width: 150px;
@@ -6,3 +8,15 @@ export const RegisterButton = styled.button`
   border-radius: 25px;
   background: linear-gradient(180deg,#56F0D6 0%,#8499FD 99%);
 `
+
+type IProps = {
+  children: React.ReactNode;
+  onClick: () => void;
+}
+export const RegisterButton2 = (props: IProps) => {
+  return (
+    <div className={"rounded-[5px] !bg-[#1A3084] py-[5px] px-[50px] text-white text-bold shadow-[0_1px_#1f6dc8]"} onClick={() => props.onClick()}>
+      {props.children}
+    </div>
+  )
+}
