bf461b350d6369f40aa7b338f2005248c6bce69b
Emma <imgenu@gmail.com>
2023-11-17 17:16:08 +0800

    feat:InviteCopySection

--- apps/gambling/src/app/ui/pages/InvitePage/HowToInviteTabSection/env/coco/InviteCopySection.tsx
+++ apps/gambling/src/app/ui/pages/InvitePage/HowToInviteTabSection/env/coco/InviteCopySection.tsx
@@ -15,14 +15,14 @@ export const InviteCopySection = () => {
 
   useEffect(()=>{
     if(isLogin) triggerGetInviteReward({});
-  },[])
+  },[isLogin])
 
   const inviteUrl = inviteInfo?.data?.inviteUrl || '';
 
   return (
     <div className="w-full text-center">
       <p className="mt-4 text-[#ffd624] text-xs leading-none mb-2">Copie o link e cole-o no navegador do seu computador para abri-lo em seu computador</p>
-      <CopyInputUrlItem url={inviteUrl} className={'rounded'} urlClassName="py-2 text-sm" buttonClassName={'rounded'} />
+      <CopyInputUrlItem url={isLogin ? inviteUrl : 'https://www.hxj-dev.com'} className={'rounded break-all text-left'} urlClassName="py-2 text-sm" buttonClassName={'rounded'} />
     </div>
   )
 }
