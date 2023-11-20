2bcad1c1f2b13ca9ec1dddc1a1aff85df0fc5d5c
fronteric <fronteric@1.1>
2023-11-20 15:26:31 +0800

    feat: add assetMapping

--- apps/gambling/src/app/ui/footers/coco/Logo.tsx
+++ apps/gambling/src/app/ui/footers/coco/Logo.tsx
@@ -1,8 +1,10 @@
-import {environment} from "../../../../environments/environment";
+// import {environment} from "../../../../environments/environment";
 import React from "react";
-
+// import LOGO from "../../../../assets/coco777bet/LOGO.png";
+import {AssetMappingCoco} from "../../../../assets/assetMapping.coco";
 export const Logo = () => {
   return (
-    <img className={"w-[50px] h-[50px]"} src={`assets/${environment.assetPrefix}/LOGO.png`}/>
+    // <img className={"w-[50px] h-[50px]"} src={`assets/${environment.assetPrefix}/LOGO.png`}/>
+    <img className={"w-[50px] h-[50px]"} src={AssetMappingCoco["logo"]}/>
   )
 }
