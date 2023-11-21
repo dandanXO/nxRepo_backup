0b1e4bf73c60811f5358a47d03882457ba526927
Emma <imgenu@gmail.com>
2023-11-21 19:51:30 +0800

    feat:HowToInviteTabSection

--- apps/gambling/src/app/ui/pages/InvitePage/HowToInviteTabSection/env/coco/InviteCopySection.tsx
+++ apps/gambling/src/app/ui/pages/InvitePage/HowToInviteTabSection/env/coco/InviteCopySection.tsx
@@ -1,11 +1,14 @@
-import {CopyInputUrlItem} from "../../../../../components/CopyInputUrlItem";
-import {useLazyGetInviteRewardDataQuery} from "../../../../../../external";
-import {useEffect} from "react";
-import {useSelector} from "react-redux";
-import {RootState} from "../../../../../../reduxStore";
+import { CopyInputUrlItem } from "../../../../../components/CopyInputUrlItem";
+import { useLazyGetInviteRewardDataQuery } from "../../../../../../external";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../../../reduxStore";
+import useBreakpoint from "apps/gambling/src/app/ui/hooks/useBreakpoint";
 
 export const InviteCopySection = () => {
-  const {isLogin} = useSelector((state: RootState) => state.app)
+  const { isLogin } = useSelector((state: RootState) => state.app);
+  const { isMobile } = useBreakpoint();
+
   const [triggerGetInviteReward, { currentData: inviteInfo, isFetching: isInviteInfoFetching }] =
     useLazyGetInviteRewardDataQuery({
       pollingInterval: 0,
@@ -13,15 +16,15 @@ export const InviteCopySection = () => {
       refetchOnReconnect: false,
     });
 
-  useEffect(()=>{
-    if(isLogin) triggerGetInviteReward({});
-  },[isLogin])
+  useEffect(() => {
+    if (isLogin) triggerGetInviteReward({});
+  }, [isLogin])
 
   const inviteUrl = inviteInfo?.data?.inviteUrl || '';
 
   return (
-    <div className="w-full text-center">
-      <p className="mt-5 text-[var(--secondary-assistant)] text-xs leading-4 mb-4">Copie o link e cole-o no navegador do seu computador para abri-lo em seu computador</p>
+    <div className="w-full text-center sm:text-left">
+      <p className="my-3.5 sm:mb-2 text-[var(--secondary-assistant)] sm:text-white text-sm md:text:xl">{`${!isMobile ? 'Passo 1:' : ''}Clique no botão para copiar o link do convite`}</p>
       <CopyInputUrlItem url={isLogin ? inviteUrl : location.href} className={'rounded break-all text-left'} urlClassName="py-2 text-sm" buttonClassName={'rounded'} />
     </div>
   )
