259e15a2c4fa5272e7f5b9b0afaff8e5df4b119f
Emma <imgenu@gmail.com>
2023-11-22 17:15:10 +0800

    feat:telegramPage

--- apps/gambling/src/app/ui/components/DatePickers/DatePicker.tsx
+++ apps/gambling/src/app/ui/components/DatePickers/DatePicker.tsx
@@ -12,7 +12,7 @@ export const DatePickerBlock = styled.div`
   padding: 4px 8px;
   width: fit-content;
   color: white;
-  background-color: var(--main-primary-varient);
+  background-color: var(--primary-variant);
   border: 1px solid rgba(255,255,255,30%);
   border-radius: 4px;
   display: flex;
