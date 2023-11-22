3b5d26f1648714b951db7779ea4235ea808e8946
fronteric <fronteric@1.1>
2023-11-22 15:55:42 +0800

    feat: sticky header and move user info status popover

--- apps/gambling/src/app/ui/popover/UserInfoStatusPopover/env/coco/UserInfoStatusPopoverContainer.tsx
+++ apps/gambling/src/app/ui/popover/UserInfoStatusPopover/env/coco/UserInfoStatusPopoverContainer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const UserInfoStatusPopoverContainer = ({ children, onMouseLeave }: { children: React.ReactNode; onMouseLeave: () => void;}) => (
   <div
     className={`
-      fixed right-[144px] top-[66px] z-10 w-[396px] flex flex-col flex-between gap-3
+      fixed right-[84px] top-[66px] z-10 w-[396px] flex flex-col flex-between gap-3
       rounded-2xl px-4 py-6 text-sm bg-[var(--primary-variant)] border-[2px] border-[var(--primary-assistant)]
     `}
      onMouseLeave={onMouseLeave}
