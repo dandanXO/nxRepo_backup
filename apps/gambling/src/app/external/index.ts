03125de3c8eab29c7479ba9854d57fa85d700614
Emma <imgenu@gmail.com>
2023-11-24 15:25:43 +0800

    feat:InviteSettlementRecordPage datepicker

--- apps/gambling/src/app/external/index.ts
+++ apps/gambling/src/app/external/index.ts
@@ -788,7 +788,8 @@ export const API = createApi({
       >({
         query: (query: GetRewardRecordRequest) => ({
           method: 'get',
-          url: `/japi/invite/userInvite/getRewardRecordList?userId=${query.userId}&pageNum=${query.pageNum}&pageSize=${query.pageSize}&startTime=${query.startTime}&endTime=${query.endTime}`,
+          // url: `/japi/invite/userInvite/getRewardRecordList?userId=${query.userId}&pageNum=${query.pageNum}&pageSize=${query.pageSize}&startTime=${query.startTime}&endTime=${query.endTime}`,
+          url: `/japi/invite/userInvite/getRewardRecordList`,
           params: query,
         }),
       }),
--- apps/gambling/src/app/ui/pages/InviteSettlementRecordPage/index.tsx
+++ apps/gambling/src/app/ui/pages/InviteSettlementRecordPage/index.tsx
@@ -1,17 +1,20 @@
-import {SectionContainer} from "../../components/container/SectionContainer";
-import moment, {Moment} from "moment/moment";
-import {DatePicker} from "antd";
-import {Button} from "../../components/Buttons/Button";
-import {useAllowLoginRouterRules} from "../../router/useAllowLoginRouterRules";
-import {PageOrModalPathEnum} from "../../PageOrModalPathEnum";
-import {useNavigate} from "react-router";
-import {useLazyGetUserInviteRewardRecordQuery} from "../../../external";
-import React, {useCallback, useEffect, useState} from "react";
-import {AppLocalStorage} from "../../../persistant/localstorage";
-import {environment} from "../../../../environments/environment";
-import {BackNavigation} from "../../components/BackNavigation/BackNavigation";
-import {AppLocalStorageKey} from "../../../persistant/AppLocalStorageKey";
+import { SectionContainer } from "../../components/container/SectionContainer";
+import moment, { Moment } from "moment/moment";
+import { DatePicker } from "antd";
+import { Button } from "../../components/Buttons/Button";
+import { useAllowLoginRouterRules } from "../../router/useAllowLoginRouterRules";
+import { PageOrModalPathEnum } from "../../PageOrModalPathEnum";
+import { useNavigate } from "react-router";
+import { useLazyGetUserInviteRewardRecordQuery } from "../../../external";
+import React, { useCallback, useEffect, useState } from "react";
+import { AppLocalStorage } from "../../../persistant/localstorage";
+import { environment } from "../../../../environments/environment";
+import { BackNavigation } from "../../components/BackNavigation/BackNavigation";
+import { AppLocalStorageKey } from "../../../persistant/AppLocalStorageKey";
 import { formatMoney } from "../../utils/formatMoney";
+import useBreakpoint from "../../hooks/useBreakpoint";
+import RangeDatePicker from "../../components/DatePickers/RangeDatePicker";
+import { datePickerStyle } from "../../components/DatePickers/DatePicker";
 
 const { RangePicker } = DatePicker;
 
@@ -19,7 +22,7 @@ const NoData = () => {
   return (
     <td colSpan={2} className=''>
       <div className='p-12'>
-        <img style={{display: 'unset'}} className={'h-[100px] margin-auto'} src={`assets/${environment.assetPrefix}/noData.png`} />
+        <img style={{ display: 'unset' }} className={'h-[100px] margin-auto'} src={`assets/${environment.assetPrefix}/noData.png`} />
         <div>Nada aqui</div>
       </div>
     </td>
@@ -30,7 +33,8 @@ export const InviteSettlementRecordPage = () => {
   useAllowLoginRouterRules();
 
   const navigate = useNavigate();
-  const [triggerGetUserInviteReward, { currentData}] = useLazyGetUserInviteRewardRecordQuery();
+  const { isMobile } = useBreakpoint();
+  const [triggerGetUserInviteReward, { currentData }] = useLazyGetUserInviteRewardRecordQuery();
 
   const min = moment().subtract(7, 'days');
   const max = moment();
@@ -38,7 +42,7 @@ export const InviteSettlementRecordPage = () => {
   const [dates, setDates] = useState([min, max]);
 
   const refresh = useCallback((startTime: string, endTime: string) => {
-    if(!AppLocalStorage.getItem(AppLocalStorageKey.userId)) return;
+    if (!AppLocalStorage.getItem(AppLocalStorageKey.userId)) return;
     triggerGetUserInviteReward({
       userId: AppLocalStorage.getItem(AppLocalStorageKey.userId) || "",
       pageNum: "1",
@@ -53,73 +57,76 @@ export const InviteSettlementRecordPage = () => {
     refresh(dates[0].format(dateFormat), dates[1].format(dateFormat));
   }, [dates])
 
-  const datePickerStyle = {
-    backgroundColor: 'var(--table-main)',
-    borderRadius: '10px',
-    border: 'none',
-    color: '#ffffff',
-  };
 
   return (
-    <>
-      <div className={"p-8"}>
+    <div className={"p-4 sm:p-8"}>
 
-        <SectionContainer id={"game-record-section"}>
+      <SectionContainer id={"game-record-section"}>
 
-          <BackNavigation onClick={() => navigate(PageOrModalPathEnum.InvitePage)}/>
+        <BackNavigation onClick={() => navigate(PageOrModalPathEnum.InvitePage)} />
 
-          <section className={"text-left mb-8 flex flex-row justify-between items-center"}>
+        <section className={"text-left mb-3 sm:mb-8 flex flex-row justify-between items-center"}>
 
-            <section>
-              <Button onClick={() => {
-                refresh("2023-10-22 00:00:00", "2023-10-29 23:59:59");
-              }}>Registros de liquidação</Button>
-            </section>
+          {!isMobile &&
+            (<section>
+              <button className="invisible sm:visible rounded-lg w-full text-xl py-3 px-16 bg-gradient-to-b from-[var(--primary-main-from)] to-[var(--primary-main-to)]" onClick={() => {
+                refresh(moment().subtract(7, 'days').format('YYYY-MM-DD 00:00:00'), moment().format('YYYY-MM-DD 23:59:59'));
+              }}>Registros de liquidação</button>
+            </section>)
+          }
+          {
+            isMobile ?
+              (<RangeDatePicker
+                min='2023-01-01'
+                max={max.format('YYYY-MM-DD')}
+                onConfirm={(values: any) => setDates([moment(values[0], 'YYYY-MM-DD'), moment(values[1], 'YYYY-MM-DD')])}
+                value={[dates[0].format('YYYY-MM-DD'), dates[1].format('YYYY-MM-DD')]}
+              />) :
+              (
+                <RangePicker
+                  value={[dates[0], dates[1]]}
+                  allowClear={false}
+                  format="YYYY-MM-DD"
+                  onChange={(dates) => {
+                    if (dates) {
+                      setDates(dates as Moment[]);
+                    }
+                  }}
+                  style={datePickerStyle}
+                  disabledDate={(current) => current > max}
+                />
+              )
+          }
+        </section>
 
-            <RangePicker
-              value={[dates[0], dates[1]]}
-              allowClear={false}
-              onChange={(dates) => {
-                if (dates) {
-                  setDates(dates as Moment[]);
-                }
-              }}
-              format={dateFormat}
-              style={datePickerStyle}
-            />
-          </section>
+        <div className="overflow-x-auto rounded-xl">
+          <table className="table table-zebra w-full text-center">
+            {/* head */}
+            <thead>
+              <tr>
+                <th className='p-4 border-r border-[rgba(255,255,255,0.2)]'>Hora De Entrada</th>
+                <th className='p-4'>Bônus</th>
+              </tr>
+            </thead>
 
-          <div className="overflow-x-auto rounded-xl">
-            <table className="table table-zebra w-full text-center">
-              {/* head */}
-              <thead>
-                <tr>
-                  <th className='p-4 border-r border-[rgba(255,255,255,0.2)]'>Hora De Entrada</th>
-                  <th className='p-4'>Bônus</th>
-                </tr>
-              </thead>
-
-              <tbody>
-              {currentData?.rows.length === 0 ? <tr><NoData/></tr> : (
+            <tbody>
+              {currentData?.rows.length === 0 ? <tr><NoData /></tr> : (
                 currentData?.rows.map((itme, index) => {
                   return (
                     <tr>
                       <td className='p-12 border-r border-[rgba(255,255,255,0.2)]'>{itme.updateTime}</td>
-                      <td className='p-12'>R$: {formatMoney(itme.reward/100)}</td>
+                      <td className='p-12'>R$: {formatMoney(itme.reward / 100)}</td>
                     </tr>
                   )
                 })
               )}
-              </tbody>
+            </tbody>
 
-            </table>
-          </div>
+          </table>
+        </div>
+      </SectionContainer>
 
+    </div>
 
-        </SectionContainer>
-
-      </div>
-
-    </>
   )
 }
