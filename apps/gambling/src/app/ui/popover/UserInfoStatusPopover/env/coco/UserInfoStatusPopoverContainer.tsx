bda44cfdd22e6ed428f8a72fd3ed48872d8139cf
fronteric <fronteric@1.1>
2023-11-24 18:53:15 +0800

    feat: user info status popover max height
    (cherry picked from commit 71e3ef17cb44fa8a53a9a326ff0c55e8c276f902)

--- apps/gambling/src/app/ui/popover/UserInfoStatusPopover/env/coco/UserInfoStatusPopoverContainer.tsx
+++ apps/gambling/src/app/ui/popover/UserInfoStatusPopover/env/coco/UserInfoStatusPopoverContainer.tsx
@@ -5,7 +5,7 @@ export const UserInfoStatusPopoverContainer = ({ children, onMouseLeave }: { chi
     className={`
       fixed right-[0] top-[66px] z-10 w-[396px] flex flex-col flex-between gap-2 rounded-2xl px-4 py-6 text-sm
       bg-[var(--primary-variant)] border-[2px] border-[var(--primary-assistant)]
-      h-[calc(100vh-66px)] overflow-auto
+      max-h-[calc(100vh-66px)] overflow-auto
     `}
      onMouseLeave={onMouseLeave}
   >
