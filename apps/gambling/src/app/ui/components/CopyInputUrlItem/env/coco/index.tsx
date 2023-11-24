c466724b1e6a8339a7a08fcc0fc75c3bb699b2e8
fronteric <fronteric@1.1>
2023-11-24 19:23:19 +0800

    fix: 螢幕寬度窄首頁會有copy按鈕

--- apps/gambling/src/app/ui/components/CopyInputUrlItem/env/coco/index.tsx
+++ apps/gambling/src/app/ui/components/CopyInputUrlItem/env/coco/index.tsx
@@ -20,7 +20,7 @@ export const CopyInputUrlItem = (props: ICopyInputUrlItem) => {
       message: 'Copiado!',
     });
   };
-  
+
   return (
     <div className={cx(`
       w-full flex rounded-xl text-[var(--text-primary)]
@@ -28,13 +28,13 @@ export const CopyInputUrlItem = (props: ICopyInputUrlItem) => {
       className
     )}>
       {contextHolder}
-      <div className={cx("py-1 px-2.5 text-xs", urlClassName)}>{url}</div>
+      <div className={cx("py-1 px-2 text-xs", urlClassName)}>{url}</div>
       <button onClick={onClickToCopy}
         className={cx(`
-        px-4
-        rounded-tr-xl rounded-br-xl 
+        px-2
+        rounded-tr-xl rounded-br-xl
         text-base text-white whitespace-nowrap
-        bg-gradient-to-b from-[var(--secondary-main-from)] to-[var(--secondary-main-to)] `, 
+        bg-gradient-to-b from-[var(--secondary-main-from)] to-[var(--secondary-main-to)] `,
         buttonClassName)}>Cópia</button>
     </div>
   )
--- apps/gambling/src/app/ui/pages/InvitePage/HowToInviteTabSection/env/coco/InviteCopySection.tsx
+++ apps/gambling/src/app/ui/pages/InvitePage/HowToInviteTabSection/env/coco/InviteCopySection.tsx
@@ -24,7 +24,7 @@ export const InviteCopySection = () => {
 
   return (
     <div className="w-full text-center sm:text-left">
-      <p className="my-3.5 sm:mb-2 text-[var(--secondary-assistant)] sm:text-white text-sm md:text:xl">{`${!isMobile ? 'Passo 1:' : ''}Clique no botão para copiar o link do convite`}</p>
+      <p className="my-3.5 sm:mb-2 text-[var(--secondary-assistant)] sm:text-white text-sm md:text-xl">{`${!isMobile ? 'Passo 1:' : ''}Clique no botão para copiar o link do convite`}</p>
       <CopyInputUrlItem url={isLogin ? inviteUrl : location.href} className={'rounded break-all text-left'} urlClassName="py-2 text-sm" buttonClassName={'rounded'} />
     </div>
   )
