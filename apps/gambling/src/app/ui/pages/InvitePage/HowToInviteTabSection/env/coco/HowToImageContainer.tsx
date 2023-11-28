50649790e251b58417b3924e85560ab2850c7a21
Emma <imgenu@gmail.com>
2023-11-28 14:58:52 +0800

    feat:InvitePage

--- apps/gambling/src/app/ui/pages/InvitePage/HowToInviteTabSection/env/coco/HowToImageContainer.tsx
+++ apps/gambling/src/app/ui/pages/InvitePage/HowToInviteTabSection/env/coco/HowToImageContainer.tsx
@@ -28,12 +28,12 @@ export const HowToImage = (props: any) => {
       'border border-solid border-[var(--stroke-block)] bg-gradient-to-b from-[var(--primary-main-from)] to-[var(--primary-main-to)]': isMobile,
       'shadow-[-4px_-4px_4px_0px_rgba(255,255,255,0.25)_inset,4px_4px_4px_0px_rgba(255,255,255,0.25)_inset]': isMobile
     })}>
-      <div className="mb-2 sm:mb-8">
-        <div className={`text-3xl font-extrabold text-[var(--text-popup)] mb-2 sm:text-4xl sm:font-bold sm:text-white sm:mb-2.5`}>Como convidar usuários?</div>
-        <HowToImageText />
+      <div className="mb-3.5 sm:mb-8 w-full">
+        <div className={`text-center sm:text-left text-2xl sm:text-4xl font-bold text-[var(--secondary-assistant)] mb-2 sm:mb-2.5`}>Como convidar usuários?</div>
+        <div className="text-sm sm:text-xl">Convide usuários válidos para recarga, o bônus pode chegar a até R$ 20 ! O que você está esperando, convide seus amigos para participar!</div>
       </div>
 
-      <div className={cx("w-full flex flex-col text-center sm:text-left sm:flex-row sm:rounded-2xl sm:pb-4 sm:pt-0.5 sm:px-8", {
+      <div className={cx("w-full flex flex-col text-center sm:text-left sm:flex-row sm:rounded-2xl sm:pb-4 sm:pt-5 sm:px-8", {
         'border border-solid border-[var(--stroke-block)] bg-gradient-to-b from-[var(--primary-main-from)] to-[var(--primary-main-to)]': !isMobile
       })}>
         <div className={'flex-1'}>
@@ -41,7 +41,7 @@ export const HowToImage = (props: any) => {
         </div>
         {!isMobile && <div className="border-r border-r-solid border-white mx-11 sm:mx-5"></div>}
         <div className={" flex-1 flex flex-col items-center sm:items-start"}>
-          <div className={"text-[var(--secondary-assistant)] sm:text-white my-3.5 sm:mb-2 text-sm md:text:xl"}>{`${!isMobile ? 'Passo 2 : ' : ''}Partilhar ligações através de software social`}</div>
+          <div className={"text-white my-3.5 sm:mt-0 sm:mb-2 text-sm md:text-xl"}>{`Passo 2 : Partilhar ligações através de software social`}</div>
           <img className={"w-[400px]"} src={`assets/${environment.assetPrefix}/pic_social_media_logo.png`} />
         </div>
       </div>
