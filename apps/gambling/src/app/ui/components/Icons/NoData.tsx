8bb8e55aeebeda68ab29104eeb78384c0705ff81
eddie <a589658gur@gmail.com>
2023-11-28 11:30:56 +0800

    fix: ui daily sign in record page add no data

--- apps/gambling/src/app/ui/components/Icons/NoData.tsx
+++ apps/gambling/src/app/ui/components/Icons/NoData.tsx
@@ -1,12 +1,13 @@
 
 import cx from "classnames";
 import { environment } from "../../../../environments/environment";
+import { tcx } from "../../utils/tcx";
 
 type IICON = {
   className?: string;
 }
 export const NoData = (props: IICON) => {
   return (
-    <img className={'h-[100px]'} alt="NoData" src={`assets/${environment.assetPrefix}/noData.png`} />
+    <img className={tcx('h-[100px]', props.className)} alt="NoData" src={`assets/${environment.assetPrefix}/noData.png`} />
   )
 }
