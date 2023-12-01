ba4eaa1c870fc4008df46ab416245b90907557c3
eddie <a589658gur@gmail.com>
2023-12-01 11:00:08 +0800

    fix: ui wallet page title

--- apps/gambling/src/app/ui/components/BackNavigation/env/coco/BackNavigation.tsx
+++ apps/gambling/src/app/ui/components/BackNavigation/env/coco/BackNavigation.tsx
@@ -1,15 +1,17 @@
 import {LeftOutlined} from "@ant-design/icons";
 import React from "react";
 import styled from "styled-components";
+import { tcx } from "../../../../utils/tcx";
 
 type IProps = {
   onClick?: () => void;
   title?: React.ReactNode;
+  className?: string;
 }
 
 export const BackNavigation = (props: IProps) => {
   return (
-    <div className={"pl-3 pt-3 pb-3 text-xl text-left text-white"}>
+    <div className={tcx("relative pl-3 pt-3 pb-3 text-xl text-left text-white", props.className)}>
       <div
         // className='flex items-center text-2xl text-[#ff97ef] ml-[6vw]'
         className={'mb-2 flex flex-row items-center justify-start'}
