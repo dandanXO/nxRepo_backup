5e02b9131a09815473aaabd9b737a99de3b93355
eddie <a589658gur@gmail.com>
2023-12-05 12:39:10 +0800

    fix(cms): frontend-413 [Admin][催收] 催收明細新增催收員當日跟進件數欄位

--- packages/cms-webpack4/src/modules/afterLoanManage/api/types/getCollectOverdueCollectDetail.ts
+++ packages/cms-webpack4/src/modules/afterLoanManage/api/types/getCollectOverdueCollectDetail.ts
@@ -32,6 +32,7 @@ export interface GetCollectOverdueCollectDetail {
     merchantName?: string; // 商戶名
     numberOfExtensionOrders?: number; // 展期筆數
     numberOfFollowUps?: number; // 跟進筆數
+    numberOfOrderCount?: number; // 催收案件筆數
     orderPaymentRate?: string; // 單數回收率
     paymentAmountRatio?: string; // 金額回收率
     receiptAmount?: number; // 回收金額
