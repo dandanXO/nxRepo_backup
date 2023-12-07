d6e42579c757f389ce4cf00bd42b6a2a23fa6d91
Emma <imgenu@gmail.com>
2023-12-07 14:44:16 +0800

    feat:RechargeActivityPage style

--- apps/gambling/src/app/ui/pages/RechargeActivityPage/env/coco/BenefitSection.tsx
+++ apps/gambling/src/app/ui/pages/RechargeActivityPage/env/coco/BenefitSection.tsx
@@ -2,8 +2,8 @@ export const BenefitSection=()=>{
   return(
     <div className="md:px-8 text-white my-4 md:my-8">
       <div className="text-2xl font-bold mb-4">Recarga benefícios</div>
-      <div className="mb-3 md:mb-1">A partir de agora, a recarga pode obter recompensas extras em dinheiro.</div>
-      <div>Quanto mais você recarregar, maior será a taxa de recompensa, até 10%. Após a recarga, o dinheiro extra também será transferido diretamente para a sua conta.</div>
+      <div className="mb-1 text-base md:text-2xl leading-6 md:leading-8">A partir de agora, a recarga pode obter recompensas extras em dinheiro.</div>
+      <div className="text-base md:text-2xl leading-6 md:leading-8">Quanto mais você recarregar, maior será a taxa de recompensa, até 10%. Após a recarga, o dinheiro extra também será transferido diretamente para a sua conta.</div>
     </div>
   )
 }
