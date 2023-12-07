d494137cccc28985ed39e6331ff749adc9c01bfa
Emma <imgenu@gmail.com>
2023-12-07 12:56:27 +0800

    feat:InitialChargePage style

--- apps/gambling/src/app/ui/pages/InitialChargePage/env/components/BenefitSection.tsx
+++ apps/gambling/src/app/ui/pages/InitialChargePage/env/components/BenefitSection.tsx
@@ -2,7 +2,7 @@ import cx from 'classnames';
 export const BenefitSection = ({ className }: { className?: string }) => {
   return (
     <>
-      <div className={cx("text-left w-full",className)}>Bônus de 20% para o primeiro depósito.</div>
+      <div className={cx("text-left w-full ",className)}>Bônus de 20% para o primeiro depósito.</div>
       <div className={cx("text-left w-full",className)}>Obrigado pela confiança e apoio. Para sua primeira recarga, oferecemos um bônus de recarga de até 20%! As recompensas serão transferidas diretamente para sua conta após a recarga.</div>
     </>
   )
