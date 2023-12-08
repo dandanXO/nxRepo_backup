c09540e91601a7d1865ccd9f3817ac1c65b6ccd5
fronteric <fronteric@1.1>
2023-12-08 18:01:50 +0800

    feat: add bbq777slot v4

---
+++ apps/gambling/src/environments/coco777bet/v4/environment.dev.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  assetPrefix: "coco777bet",
+  assetVersionPrefix: "v4",
+  mockBackend: false,
+  platformName: "Dev BBQ777Slots",
+  platformGroup: "O JS Group",
+  baseUrl: "",
+  appPackageName: "com.ttgroup.phxj",
+  captcha: "/japi/user/captcha/image",
+  appVersion: "1.0.0",
+  s3URLImages: "https://resources.ttgroup.vip/icon",
+};
