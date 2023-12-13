6941a1b06adc779c011ce26b4c63fa643b557f1c
fronteric <fronteric@1.1>
2023-12-13 16:50:12 +0800

    feat: add v5 purasorte777bet

---
+++ apps/gambling/src/environments/coco777bet/v5/environment.dev.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  assetPrefix: "coco777bet",
+  assetVersionPrefix: "v5",
+  mockBackend: false,
+  platformName: "Dev PuraSorte777Bet",
+  platformGroup: "O TT Group",
+  baseUrl: "",
+  appPackageName: "com.ttgroup.phxj",
+  captcha: "/japi/user/captcha/image",
+  appVersion: "1.0.0",
+  s3URLImages: "https://resources.ttgroup.vip/icon",
+};
