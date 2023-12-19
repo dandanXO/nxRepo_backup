9eb13a62c59f190380c1e43e3236c6adeb322e5c
fronteric <fronteric@1.1>
2023-12-19 17:18:16 +0800

    feat: change carousel1

--- apps/gambling/src/app/ui/pages/IndexPage/Carousel/env/coco/AppCarouselContent.tsx
+++ apps/gambling/src/app/ui/pages/IndexPage/Carousel/env/coco/AppCarouselContent.tsx
@@ -17,7 +17,7 @@ import {CarouselTitleSection} from "./CarouselTitleSection";
 
 
 export const AppCarouselContent = (props: IAppCarouselContent) => {
-  const {isMobile} = useBreakpoint();
+  const {isMobile, } = useBreakpoint();
   const {onClickToFirstDeposit} = usePageNavigate();
 
   return (
@@ -29,14 +29,28 @@ export const AppCarouselContent = (props: IAppCarouselContent) => {
       }}
     >
       <div className={""}>
-        <CarouselTitleSection>
-          Primeiro depósito <br/> bônus de 20%
+        <CarouselTitleSection className={"sm:top-[45%] md:top-[35%]"}>
+          Prêmio upgrade VIP
+          <br/>
+          Só esperando você coletar!
         </CarouselTitleSection>
+
         {isMobile ? (
           <CarouselImage alt={"h5_banner_2"} src={`assets/${environment.assetPrefix}/${environment.assetVersionPrefix}/h5_banner_2.png`}/>
         ): (
           <CarouselImage alt={"banner_2"} src={`assets/${environment.assetPrefix}/${environment.assetVersionPrefix}/banner_2.png`}/>
         )}
+
+        <Container
+          className={cx("absolute top-[74%] transform -translate-y-1/2",
+          )}
+        >
+          <div className="w-full bg-[rgba(255,255,255,0.5)] px-3 md:px-3 md:py-2 xl:px-4 xl:py-3 flex flex-row justify-center items-start rounded-lg">
+            <div className="text-lg md:text-3xl xl:text-6xl font-bold leading-7 md:leading-9 xl:leading-none text-white">
+              VIP0 Pode Retirar
+            </div>
+          </div>
+        </Container>
       </div>
     </CarouselContainer>
   )
