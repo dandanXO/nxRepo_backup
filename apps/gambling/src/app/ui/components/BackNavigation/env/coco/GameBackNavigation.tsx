1cbf168749aaab4796da01c315271704351f1fda
fronteric <fronteric@1.1>
2023-12-21 10:32:53 +0800

    feat: change game page back navigation

--- apps/gambling/src/app/ui/components/BackNavigation/env/coco/GameBackNavigation.tsx
+++ apps/gambling/src/app/ui/components/BackNavigation/env/coco/GameBackNavigation.tsx
@@ -8,11 +8,11 @@ type IGameBackNavigation = {
 
 export const GameBackNavigation = (props: IGameBackNavigation) => {
   return (
-    <div className="bg-[#1a1a1a] flex flex-row w-full items-center h-[56px] py-3 px-6 fixed">
+    <div className="bg-[#1a1a1a] flex flex-row w-full items-center h-[40px] md:h-[52px] lg:h-[56px] py-3 px-6 fixed">
       <span onClick={() => props.onClick && props.onClick()}>
         <ArrowLeft className='relative z-10 text-white mr-1'/>
       </span>
-      <div className="text-2xl font-medium leading-[32px] text-white">Retornar</div>
+      <div className="text-base md:text-lg lg:text-2xl font-medium leading-6 md:leading-7 lg:leading-8 text-white">Retornar</div>
     </div>
   )
 }
