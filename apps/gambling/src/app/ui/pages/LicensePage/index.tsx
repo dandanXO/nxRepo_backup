463e94be0dcbdb9e2a3d66b380d251c9911ef572
DAN <dandanxo.1996@gmail.com>
2023-12-26 15:18:57 +0800

    fix: title

--- apps/gambling/src/app/ui/pages/LicensePage/index.tsx
+++ apps/gambling/src/app/ui/pages/LicensePage/index.tsx
@@ -1,67 +1,11 @@
-import { useNavigate } from "react-router";
-
-import { AppLocalStorage } from "../../../persistant/localstorage";
-import { useAllowLoginRouterRules } from "../../router/useAllowLoginRouterRules";
 import { environment } from "../../../../environments/environment";
 import { BackNavigation } from "../../components/BackNavigation/BackNavigation";
 import { usePageNavigate } from "../../hooks/usePageNavigate";
-import { AppLocalStorageKey } from "../../../persistant/AppLocalStorageKey";
-import {useEffect} from "react";
-import {useDispatch} from "react-redux";
-import useBreakpoint from "../../hooks/useBreakpoint";
-
-// const GoToTelegram = styled.div`
-//   cursor: pointer;
-//   background: linear-gradient(180deg, var(--primary-main-from) 0%, var(--primary-main-to) 100%); 
-//   border-radius: 8px;
-//   /* width: 300px; */
-//   /* height: 60px; */
-//   padding: 14px 96px;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   margin: 32px auto 64px;
-// `
-
-// const ListItem = (props: { count: string; text: string; }) => {
-//   return (
-//     <li className="mb-4 sm:mb-3 space-x-2 flex">
-//       <div className="text-[var(--primary-assistant)] text-xl">{props.count}</div>
-//       <div className="">{props.text}</div>
-//     </li>
-//   )
-// }
 
 export const LicensePage = () => {
 
 
-  const navigate = useNavigate();
-  const { isMobile } = useBreakpoint();
-
-  const userInfoString = AppLocalStorage.getItem(AppLocalStorageKey.userInfo);
-  const userInfo = userInfoString ? JSON.parse(userInfoString) : null;
-
-
   const { onClickToIndex } = usePageNavigate();
-
-  // const TelegramContentContainer = renderByPlatform({
-  //   "wild777bet": VIPBorderStyleContainer,
-  //   "coco777bet": FragmentContainer,
-  // }, VIPBorderStyleContainer);
-
-  // const isCoco777bet = environment.assetPrefix === 'coco777bet';
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    // const timer = setTimeout(() => {
-    //   dispatch(appSlice.actions.setShowTelegramModal(true))
-    // }, 5000);
-
-    // return () => {
-    //   clearTimeout(timer);
-    // }
-  }, []);
 
   return (
 
@@ -69,7 +13,6 @@ export const LicensePage = () => {
 
       <BackNavigation
         onClick={() => onClickToIndex()}
-        title={isMobile && <div className={"w-full text-center font-bold"}>Canal De Telegram</div>}
       />
       <section>
       <div className="flex flex-col justify-center items-center">
