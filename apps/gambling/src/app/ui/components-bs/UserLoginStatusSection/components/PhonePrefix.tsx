0588dd4528f1b1f9e390ead327f259ae015dc24f
fronteric <fronteric@1.1>
2023-12-26 13:07:06 +0800

    fix: phone text size

--- apps/gambling/src/app/ui/components-bs/UserLoginStatusSection/components/PhonePrefix.tsx
+++ apps/gambling/src/app/ui/components-bs/UserLoginStatusSection/components/PhonePrefix.tsx
@@ -1,5 +1,5 @@
 export const PhonePrefix = () => {
   return (
-    <span className="text-[var(--input-text-color)] mr-2 text-xs md:text-sm leading-[24px]">+55</span>
+    <span className="text-[var(--input-text-color)] mr-2 leading-[24px] text-sm md:text-xl">+55</span>
   )
 }
