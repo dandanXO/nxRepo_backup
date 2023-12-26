a6ed55d5f304efd1165a17e25b422233ec578a44
DAN <dandanxo.1996@gmail.com>
2023-12-26 17:08:31 +0800

    feat: mainten modal  for  riojungle

--- apps/gambling/build/tailwind.config.js
+++ apps/gambling/build/tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      bgGradientDeg: {
+        75: '75deg',
+      },
       keyframes: {
         marquee: {
           '0%': { transform: 'translateX(100%)' },
@@ -122,8 +125,33 @@ module.exports = {
   },
   plugins: [
     // require("daisyui"),
-    plugin(function ({ addUtilities, addComponents, addVariant, e }) {
-
+    plugin(function ({ matchUtilities, theme, addUtilities, addComponents, addVariant, e }) {
+      matchUtilities(
+        {
+            'bg-gradient': (angle) => ({
+                'background-image': `linear-gradient(${angle}, var(--tw-gradient-stops))`,
+            }),
+        },
+        {
+            // values from config and defaults you wish to use most
+            values: Object.assign(
+                theme('bgGradientDeg', {}), // name of config key. Must be unique
+                {
+                    10: '10deg', // bg-gradient-10
+                    15: '15deg',
+                    20: '20deg',
+                    25: '25deg',
+                    30: '30deg',
+                    45: '45deg',
+                    60: '60deg',
+                    90: '90deg',
+                    120: '120deg',
+                    144: '144deg',
+                    135: '135deg',
+                }
+            )
+        }
+     )
     }),
   ],
   safelist: ['border-l-2', 'border-dashed'],
