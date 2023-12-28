8a022975062a612fdfbb6e780832c6ba36ea2783
fronteric <fronteric@1.1>
2023-12-28 11:27:50 +0800

    build: add u1v8 SambaFortuneBet

---
+++ apps/gambling/src/environments/coco777bet/v8/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  assetPrefix: "coco777bet",
+  assetVersionPrefix: "v8",
+  mockBackend: false,
+  platformName: "SambaFortuneBet",
+  platformGroup: "O E STAR Group",
+  baseUrl: "",
+  appPackageName: "com.ttgroup.phxj",
+  captcha: "/japi/user/captcha/image",
+  appVersion: "1.0.0",
+  s3URLImages: "https://resources.ttgroup.vip/icon",
+};
