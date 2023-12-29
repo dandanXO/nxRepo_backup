24dd380f52597d1475c739d6aee139c705228d4e
eddie <a589658gur@gmail.com>
2023-12-29 15:21:53 +0800

    feat: depositAdvertisementModal

---
+++ apps/gambling/src/app/ui/modals/DepositAdvertisementModal/env/riojungle/index.tsx
@@ -0,0 +1,49 @@
+import { environment } from "../../../../../../environments/environment";
+import { CloseICON } from "../../../../components-bs/Icons/CloseICON";
+import React from "react";
+import useBreakpoint from "../../../../hooks/useBreakpoint";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../../reduxStore";
+import { IDepositAdvertisementModalProps } from "../../index";
+
+export const DepositAdvertisementModal = ({
+  onConfirm,
+  close
+}: IDepositAdvertisementModalProps) => {
+  const { isMobile } = useBreakpoint();
+  const recharge_first_cashback_rate = useSelector((rootState: RootState) => rootState.app.config.recharge_first_cashback_rate);
+
+  return (
+    <div
+      className='relative flex flex-col items-center text-white rounded-2xl bg-gradient-to-br from-[#8547EB] to-[#10B98F] w-4/5 sm:w-[320px] lg:w-[480px] p-6 text-center'
+      onClick={(event) => {
+        event.stopPropagation();
+      }}
+    >
+      <img
+        alt='close'
+        className='absolute cursor-pointer top-2 right-2 w-12' src={`assets/${environment.assetPrefix}/WXCircle.png`}
+        onClick={()=>close()}
+      />
+      <img
+        alt='genie'
+        src={`assets/${environment.assetPrefix}/${environment.assetVersionPrefix}/deposit-genie.png`}
+        className='w-3/5 -mt-[50px] sm:-mt-[50px]'
+      />
+
+      <div className='mt-2 sm:mt-3 lg:mt-5 font-medium text-base sm:text-xl lg:text-3xl'>Equilíbrio insuficiente!</div>
+
+      <div className='mt-2 lg:mt-3 text-sm lg:text-lg'>
+        Caros clientes VIP, você pode obter até {recharge_first_cashback_rate} de recompensa ao recarregar. Quanto mais você recarrega, mais bônus você recebe! Sem limite de tempo!
+      </div>
+
+      <button
+        className='mt-5 py-[10px] sm:py-[12px] lg:py-[14px] text-sm sm:text-base lg:text-xl bg-[#8547EB] rounded-lg shadow-[inset_0px_-4px_4px_0px_rgba(0,_0,_0,_0.25),_inset_0px_4px_4px_0px_rgba(255,_255,_255,_0.25)] w-full'
+        onClick={onConfirm}
+      >
+        Depósito
+      </button>
+
+    </div>
+  )
+}
