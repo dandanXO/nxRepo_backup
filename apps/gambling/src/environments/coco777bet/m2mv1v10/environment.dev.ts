0beeed32a35389521ef7d7f245961b5b5e54f177
fronteric <fronteric@1.1>
2024-01-04 16:08:58 +0800

    feat: add v10u1m2v1

---
+++ apps/gambling/src/environments/coco777bet/m2mv1v10/environment.dev.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  assetPrefix: "coco777bet",
+  assetVersionPrefix: "m2mv1v10",
+  mockBackend: false,
+  platformName: "Dev BrasilSlotsBet",
+  platformGroup: "O E STAR Group",
+  baseUrl: "",
+  appPackageName: "com.ttgroup.phxj",
+  captcha: "/japi/user/captcha/image",
+  appVersion: "1.0.0",
+  s3URLImages: "https://resources.ttgroup.vip/icon",
+};
