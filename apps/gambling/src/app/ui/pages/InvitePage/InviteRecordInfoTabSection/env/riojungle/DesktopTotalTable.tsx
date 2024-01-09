524b5a90c00b84e9b3929599ece047ffe0ba2814
Emma <imgenu@gmail.com>
2024-01-09 11:44:29 +0800

    feat:InviteRecordInfoTabSection add currency

--- apps/gambling/src/app/ui/pages/InvitePage/InviteRecordInfoTabSection/env/riojungle/DesktopTotalTable.tsx
+++ apps/gambling/src/app/ui/pages/InvitePage/InviteRecordInfoTabSection/env/riojungle/DesktopTotalTable.tsx
@@ -5,12 +5,12 @@ import useBreakpoint from "../../../../../pageTemplate/hooks/useBreakpoint";
 
 export const DesktopTotalTable = (props: ITotal & { type: string }) => {
 
-  const thStyle = "text-sm lg:text-base px-2 lg:px-3 border-r border-[rgba(255,255,255,0.2)] text-[#b3b3b3] font-normal lg:font-bold";
+  const thStyle = "text-sm lg:text-base px-2 lg:px-3 border-r border-[rgba(255,255,255,0.2)] text-[var(--grayscale-70)] font-normal lg:font-bold";
   const tdStyle = 'text-sm lg:text-lg px-2 lg:px-3 text-center pt-5 border-r border-[rgba(255,255,255,0.2)] text-white font-bold'
   const { isTablet } = useBreakpoint()
 
   return (
-    <div className="border border-solid border-[#666] overflow-x-auto px-3 lg:p-5 shadow-[inset_0px_-4px_4px_0px_rgba(0,_0,_0,_0.25),_inset_0px_4px_4px_0px_rgba(255,_255,_255,_0.25)] bg-[#333333] rounded-lg">
+    <div className="border border-solid border-[var(--grayscale-40)] overflow-x-auto px-3 md:p-5 shadow-[inset_0px_-4px_4px_0px_rgba(0,_0,_0,_0.25),_inset_0px_4px_4px_0px_rgba(255,_255,_255,_0.25)] bg-[var(--grayscale-20)] rounded-lg">
       <table className="w-full">
         <thead>
           <tr>
@@ -31,7 +31,7 @@ export const DesktopTotalTable = (props: ITotal & { type: string }) => {
                 </span>
               </div>
             </th>
-            <th className='text-sm lg:text-base p-4 text-[#b3b3b3]'>Recompensa Total</th>
+            <th className='text-sm lg:text-base px-2 lg:px-3 text-[var(--grayscale-70)] font-normal lg:font-bold'>Recompensa Total</th>
           </tr>
         </thead>
 
@@ -39,11 +39,11 @@ export const DesktopTotalTable = (props: ITotal & { type: string }) => {
           {/* row 1 */}
           <tr>
             <td className={tdStyle}>{props?.data.numRecharge || 0}</td>
-            {props.type === "1" && <td className={tdStyle}>{props?.data.firstRecharge}</td>}
+            {props.type === "1" && <td className={tdStyle}>R$ {props?.data.firstRecharge || '0,00'}</td>}
             {/* {props.isProxy && <td className={tdStyle}>R${props?.data.dividendos}</td>} */}
-            <td className={tdStyle}>{props?.data.gameRecharge}</td>
-            <td className={tdStyle}>{props?.data.gameRechargeReward}</td>
-            <td className='text-sm lg:text-lg text-center pt-5 text-white'>{props?.data.totalReward}</td>
+            <td className={tdStyle}>R$ {props?.data.gameRecharge || '0,00'}</td>
+            <td className={tdStyle}>R$ {props?.data.gameRechargeReward || '0,00'}</td>
+            <td className='text-sm lg:text-lg px-2 lg:px-3 text-center pt-5 text-white font-bold'>R$ {props?.data.totalReward || '0,00'}</td>
           </tr>
         </tbody>
       </table>
