b1b33a60499469f3b4662cb24150cbd711001c2f
eddie <a589658gur@gmail.com>
2024-01-09 16:57:56 +0800

    feat: add u1m1mv9v12

---
+++ apps/gambling/src/environments/coco777bet/m1mv9v12/environment.dev.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  assetPrefix: "coco777bet",
+  assetVersionPrefix: "m1mv9v12",
+  mockBackend: false,
+  platformName: "Dev BreezeSlotsBet",
+  platformGroup: "O LGD Group",
+  baseUrl: "",
+  appPackageName: "com.ttgroup.phxj",
+  captcha: "/japi/user/captcha/image",
+  appVersion: "1.0.0",
+  s3URLImages: "https://resources.ttgroup.vip/icon",
+  snowEffects: "sakura",
+};
---
+++ apps/gambling/src/environments/coco777bet/m1mv9v12/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  assetPrefix: "coco777bet",
+  assetVersionPrefix: "m1mv9v12",
+  mockBackend: false,
+  platformName: "BrasilSlotsBet",
+  platformGroup: "O LGD Group",
+  baseUrl: "",
+  appPackageName: "com.ttgroup.phxj",
+  captcha: "/japi/user/captcha/image",
+  appVersion: "1.0.0",
+  s3URLImages: "https://resources.ttgroup.vip/icon",
+  snowEffects: "sakura",
+};
