33fafb4709d6cf3c970853cc3c86f4dfe6ce91d9
Emma <imgenu@gmail.com>
2024-01-11 12:12:06 +0800

    feat:v14-u1m1v10

---
+++ apps/gambling/src/environments/u1/m1v10/environment.dev.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  uVersion: "u1",
+  mvVersion: "m1v10",
+  mockBackend: false,
+  platformName: "Dev TropicaSlotsBet",
+  platformGroup: "O LGD Group",
+  baseUrl: "",
+  appPackageName: "com.ttgroup.phxj",
+  captcha: "/japi/user/captcha/image",
+  appVersion: "1.0.0",
+  s3URLImages: "https://resources.ttgroup.vip/icon",
+  snowEffects: "snowDot",
+};
