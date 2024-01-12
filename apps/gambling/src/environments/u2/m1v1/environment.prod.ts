299ca398932065b7d20c1fc37b6529e12169e39e
DAN <dandanxo.1996@gmail.com>
2024-01-12 15:42:44 +0800

    feat: delete videlo log

--- apps/gambling/src/environments/u2/m1v1/environment.prod.ts
+++ apps/gambling/src/environments/u2/m1v1/environment.prod.ts
@@ -10,5 +10,5 @@ export const environment = {
   captcha: "/japi/user/captcha/image",
   appVersion: "1.0.0",
   s3URLImages: "https://resources.ttgroup.vip/icon",
-  packgroundVideoUrl: "https://xb-web.s3.sa-east-1.amazonaws.com/video/calica777slots_video.mp4"
+  packgroundVideoUrl: ""
 };
