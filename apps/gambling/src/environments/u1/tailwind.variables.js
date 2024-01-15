a97751a49a05a6118f5a82abc7fe4c58a87c7569
Emma <imgenu@gmail.com>
2024-01-15 14:37:08 +0800

    feat:U1 responsive breakpoint

--- apps/gambling/src/environments/u1/tailwind.variables.js
+++ apps/gambling/src/environments/u1/tailwind.variables.js
@@ -2,9 +2,12 @@ module.exports = {
   tailwindVariables: {
     theme: {
       screens: {
-        mobile: '640px',
-        tablet: '768px',
-        desktop: '1024px',
+        // mobile: '640px',
+        // tablet: '768px',
+        // desktop: '1024px',
+        mobile: '1439px',
+        tablet: '1439px',
+        desktop: '7680px',
       }
     },
   }
