2856ac354a9af9f9213fcabab3687561af522280
eddie <a589658gur@gmail.com>
2024-01-15 12:13:51 +0800

    feat: add v15

---
+++ apps/gambling/src/environments/u1/m1v11/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  uVersion: "u1",
+  mvVersion: "m1v11",
+  mockBackend: false,
+  platformName: "Bingo777Bet",
+  platformGroup: "O FUN Group",
+  baseUrl: "",
+  appPackageName: "com.ttgroup.phxj",
+  captcha: "/japi/user/captcha/image",
+  appVersion: "1.0.0",
+  s3URLImages: "https://resources.ttgroup.vip/icon",
+  snowEffects: "",
+};
