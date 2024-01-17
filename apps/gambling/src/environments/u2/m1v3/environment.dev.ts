a9ce2d693834a05a98571de42bbbe5c1f2bc6d15
Evan <a.a@a.a>
2024-01-17 14:43:10 +0800

    feat: cariocas777bet_v13_u2miv3

---
+++ apps/gambling/src/environments/u2/m1v3/environment.dev.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  uVersion: "u2",
+  mvVersion: "m1v3",
+  mockBackend: false,
+  platformName: "Dev Cariocas777Bet",
+  platformGroup: "O TT Group",
+  baseUrl: "",
+  appPackageName: "com.ttgroup.phxj",
+  captcha: "/japi/user/captcha/image",
+  appVersion: "1.0.0",
+  s3URLImages: "https://resources.ttgroup.vip/icon",
+};
