653977990b2d92257408d8e3d0b198545c013f82
Evan <a.a@a.a>
2024-01-18 12:26:03 +0800

    feat: add v17_u1m1v12

---
+++ apps/gambling/src/environments/u1/m1v12/environment.dev.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  uVersion: "u1",
+  mvVersion: "m1v12",
+  mockBackend: false,
+  platformName: "Dev OuroSlotsBet",
+  platformGroup: "O LGD Group",
+  baseUrl: "",
+  appPackageName: "com.ttgroup.phxj",
+  captcha: "/japi/user/captcha/image",
+  appVersion: "1.0.0",
+  s3URLImages: "https://resources.ttgroup.vip/icon",
+  snowEffects: "snowDot",
+  backgroundVideoUrl: "https://resources.ttgroup.vip/video/dragon.mp4"
+};
