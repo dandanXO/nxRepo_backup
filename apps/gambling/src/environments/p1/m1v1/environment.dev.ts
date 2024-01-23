93cc7ed7d3b4a9f525221e773516b500f8b6b321
eddie <a589658gur@gmail.com>
2024-01-23 12:08:02 +0800

    feat: 新增pbet

---
+++ apps/gambling/src/environments/p1/m1v1/environment.dev.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  uVersion: "p1",
+  mvVersion: "m1v1",
+  mockBackend: false,
+  platformName: "Dev BreezeSlotsBet",
+  platformGroup: "O LGD Group",
+  baseUrl: "",
+  appPackageName: "com.ttgroup.phxj",
+  captcha: "/japi/user/captcha/image",
+  appVersion: "1.0.0",
+  s3URLImages: "https://resources.ttgroup.vip/icon",
+  snowEffects: "sakura",
+};
