61149edf2ed59392c1bd5d333b4fe0614d8e5871
Evan <a.a@a.a>
2024-01-23 16:47:01 +0800

    feat: add v20 u1 m1 v13 luxoslotsbet

---
+++ apps/gambling/src/environments/u1/m1v13/environment.dev.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  uVersion: "u1",
+  mvVersion: "m1v13",
+  mockBackend: false,
+  platformName: "Dev LuxoSlotsBet",
+  platformGroup: "O NEW Group ",
+  baseUrl: "",
+  appPackageName: "com.ttgroup.phxj",
+  captcha: "/japi/user/captcha/image",
+  appVersion: "1.0.0",
+  s3URLImages: "https://resources.ttgroup.vip/icon",
+  snowEffects: "none",
+  backgroundVideoUrl: ""
+};
