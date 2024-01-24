9f1cace871da501ab920d5c443f04ad4358648ef
Evan <a.a@a.a>
2024-01-24 14:33:03 +0800

    feat: add v19 u1m3v1 MikaSlotsBet

---
+++ apps/gambling/src/environments/u1/m3v1/environment.dev.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  uVersion: "u1",
+  mvVersion: "m3v1",
+  mockBackend: false,
+  platformName: "Dev MikaSlotsBet",
+  platformGroup: "O JS Group",
+  baseUrl: "",
+  appPackageName: "com.ttgroup.phxj",
+  captcha: "/japi/user/captcha/image",
+  appVersion: "1.0.0",
+  s3URLImages: "https://resources.ttgroup.vip/icon",
+  snowEffects: "sakura",
+  backgroundVideoUrl: "https://resources.ttgroup.vip/video/castle.mp4"
+};
